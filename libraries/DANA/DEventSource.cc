874a48f3e5d31e9ad18cdac34e6413517e7c3ebf
David Lawrence <davidl@jlab.org>
2004-06-28 15:17:54 +0000

    Added mechanisim to loop over event sources. Added source argument to Open().

--- libraries/DANA/DEventSource.cc
+++ libraries/DANA/DEventSource.cc
@@ -22,6 +22,8 @@ DEventSource::DEventSource(int narg, char *argv[])
 	sources = new char*[narg];
 	Nsources = 0;
 	source_type = EVENT_SOURCE_NONE;
+	source_index = 0;
+	source_is_open = 0;
 	for(int i=1;i<narg;i++)if(argv[i][0]!='-')sources[Nsources++] = strdup(argv[i]);
 	
 	Nevents_read = 0;
@@ -51,6 +53,30 @@ derror_t DEventSource::NextEvent(void)
 {
 	/// Increment event counters and update event rate calculation
 
+	// If an event source isn't open, then open the next one and recall ourself
+	derror_t err;
+	if(!source_is_open){
+		if(source_index>=Nsources)return NO_MORE_EVENT_SOURCES;
+		cout<<endl<<"Opening \""<<sources[source_index]<<"\""<<endl;
+		err = Open(sources[source_index++]);
+		if(err)return err;
+		source_is_open = 1;
+		return NextEvent();
+	}
+
+	// Read next event from source. If none, then close source and recall ourself
+	switch(GetEvent()){
+		case NO_MORE_EVENTS_IN_SOURCE:
+			cout<<endl<<"Closing \""<<sources[source_index]<<"\""<<endl;
+			err = Close();
+			if(err)return err;
+			source_is_open = 0;
+			return NextEvent();
+			break;
+		default:
+			break;
+	}
+
 	// Event counters
 	Nevents_read++;
 	Nevents_read_total++;
@@ -62,13 +88,13 @@ derror_t DEventSource::NextEvent(void)
 	}else{
 		time_t now = time(NULL);
 		if(now-prate_last_time >= prate_period){
-			prate_last_rate = (float)(Nevents_read_total-prate_last_events)/(float)prate_period;
+			prate_last_rate = (float)(Nevents_read_total-prate_last_events)/(float)(now-prate_last_time);
 			prate_last_events = Nevents_read_total;
 			prate_last_time = now;
 		}
 	}
 	
-	return GetEvent();
+	return NOERROR;
 }
 
 //----------------
@@ -86,15 +112,15 @@ float DEventSource::GetRate(void)
 //----------------
 DEventSource::EVENT_SOURCE_TYPE DEventSource::GuessSourceType(int narg, char *argv[])
 {
-	/// Returns the rate at which the events are being processed.
-	/// This could lag by as much as 1 prate_period from instantaneous.
-	return EVENT_SOURCE_NONE;
+	/// Guesses type of event source
+	/// Only hddm is supported for now.
+	return EVENT_SOURCE_FILE;
 }
 
 //----------------
 // Open
 //----------------
-derror_t DEventSource::Open(void)
+derror_t DEventSource::Open(char *source)
 {
 	/// virtual function
 	return NOERROR;
