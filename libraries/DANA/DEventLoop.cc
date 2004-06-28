17b30c5720c205e90a1180ae1bac3f2e7be92a55
David Lawrence <davidl@jlab.org>
2004-06-28 15:16:45 +0000

    Added GetRate and PrintRate methods. Added loop over events.

--- libraries/DANA/DEventLoop.cc
+++ libraries/DANA/DEventLoop.cc
@@ -3,12 +3,13 @@
 //
 // DEventLoop methods
 //
-
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 #include "DEventLoop.h"
 #include "DEventSourceET.h"
+#include "DEventSourceFile.h"
 
 #include "../BCAL/DBCAL.h"
 #include "../CDC/DCDC.h"
@@ -21,6 +22,8 @@ using namespace std;
 #include "../FDC/DFDC.h"
 #include "../CHERENKOV/DCHERENKOV.h"
 
+int DONE = 0;
+
 //----------------
 // Constructor
 //----------------
@@ -31,6 +34,7 @@ DEventLoop::DEventLoop(int narg, char *argv[])
 	// Initialize variables
 	proc_mask = EVENT_PROCESSOR_ALL;
 	Nprocessors = 0;
+	last_print_rate_time = 0;
 	
 	// Determine event source type
 	DEventSource::EVENT_SOURCE_TYPE source_type;
@@ -39,7 +43,7 @@ DEventLoop::DEventLoop(int narg, char *argv[])
 			source = new DEventSourceET(narg, argv);
 			break;
 		case DEventSource::EVENT_SOURCE_FILE:
-			source = new DEventSourceFILE(narg, argv);
+			source = new DEventSourceFile(narg, argv);
 			break;
 		default:
 			source = NULL;
@@ -66,6 +70,8 @@ derror_t DEventLoop::AddProcessor(DEventProcessor *processor)
 	}
 	
 	processors[Nprocessors++] = processor;
+	
+	return NOERROR;
 }
 
 //----------------
@@ -110,7 +116,29 @@ derror_t DEventLoop::Run(void)
 	// Re-append "user" processors
 	for(int i=0;i<Nprocessors_copy;i++)AddProcessor(processors_copy[i++]);
 	
+	// Set the event_loop field for all processors
+	for(int i=0;i<Nprocessors;i++)processors[i]->event_loop = this;
+	
 	// Loop over events
+	do{
+		derror_t err = source->NextEvent();
+		switch(err){
+			case NOERROR:
+				break;
+			case NO_MORE_EVENT_SOURCES:
+				cout<<endl<<"No more event sources"<<endl;
+				break;
+			default:
+				break;
+		}
+		if(err != NOERROR)break;
+		
+		// Call Event Processors
+		for(int i=0;i<Nprocessors;i++){
+			processors[i]->evnt(0);
+		}
+		
+	}while(!DONE);
 }
 
 //----------------
@@ -145,5 +173,61 @@ derror_t DEventLoop::Run(DEventProcessor *proc, EVENT_PROCESSORS mask)
 	if(err=SetStandardProcessors(mask))return err;
 	
 	return Run();
+}
+
+//----------------
+// GetRate
+//----------------
+float DEventLoop::GetRate(void)
+{
+	/// This just returns GetRate from the private DEventSource member
+	if(!source){
+		cerr<<__FILE__<<":"<<__LINE__<<" No event source set (source=NULL)!"<<endl;
+		return -1.0;
+	}
+	
+	return source->prate_last_rate;
+}
+
+//----------------
+// PrintRate
+//----------------
+derror_t DEventLoop::PrintRate(void)
+{
+	/// Update the screen with the current event processing rate.
+	/// This governs itself to only update as often as the rate
+	/// is recalculated by DEventSource so it is safe to call
+	/// it every event.
+	if(!source){
+		cerr<<__FILE__<<":"<<__LINE__<<" No event source set (source=NULL)!"<<endl;
+		return RESOURCE_UNAVAILABLE;
+	}
+
+	//cout<<__FILE__<<":"<<__LINE__<<" last_print_rate_time="<<last_print_rate_time;
+	//cout<<" source->prate_last_time="<<source->prate_last_time<<endl;
+
+	if(last_print_rate_time != source->prate_last_time){
+		float rate = source->prate_last_rate;
+		char *rateunits = "Hz";
+		if(rate>1500000.){
+			rate/=1000000.;
+			rateunits = "kHz";
+		}else if(rate>1500.){
+			rate/=1000;
+			rateunits = "kHz";
+		}
+		float Nevents = (float)source->Nevents_read;
+		char *eventunits = "";
+		if(Nevents>10000.){
+			Nevents/=1000.;
+			eventunits = "k";
+		}
+		
+		printf("  %3.1f%s events  (%3.1f%s)          \r", Nevents,eventunits, rate, rateunits);
+		fflush(stdout);
+		last_print_rate_time = source->prate_last_time;
+	}
+	
+	return NOERROR;
 }
 
