2d51ad558fb7027aeee2f155004928c86f994189
David Lawrence <davidl@jlab.org>
2004-08-30 14:00:47 +0000

    Added x0,y0 to argument list for first guess methods

--- libraries/CDC/DCDC.h
+++ libraries/CDC/DCDC.h
@@ -19,6 +19,6 @@ class DCDC:public DEventProcessor
 		derror_t fini(void);					///< Called after last event of last event source has been processed.
 
 		derror_t	copy_to_cdchits(void);
-		derror_t firstguess(CDChit_t *hits, int Nhits, float &theta, float &phi, float &p, float &q);
-		derror_t firstguess_curtis(CDChit_t *hits, int Nhits, float &theta, float &phi, float &p, float &q);
+		derror_t firstguess(CDChit_t *hits, int Nhits, float &theta, float &phi, float &p, float &q, float &x0, float &y0);
+		derror_t firstguess_curtis(CDChit_t *hits, int Nhits, float &theta, float &phi, float &p, float &q, float &x0, float &y0);
 };
