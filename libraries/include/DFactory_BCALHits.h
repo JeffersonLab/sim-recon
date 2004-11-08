92230c4ed44acf7f2424ea69c29188aef4fb78f5
David Lawrence <davidl@jlab.org>
2004-11-08 18:10:44 +0000

    Define BCALHit_t. Add Print() method

--- libraries/include/DFactory_BCALHits.h
+++ libraries/include/DFactory_BCALHits.h
@@ -8,13 +8,17 @@
 #include "DFactory.h"
 
 typedef struct{
-	// place holder for now;
+	float phim;
+	int end; ///< 0=upstream  1=downstream
+	float E;
+	float t;
 }BCALHit_t;
 
 class DFactory_BCALHits:public DFactory{
 	public:
 		DFactory_BCALHits(DEvent *event):DFactory(event, "BCALHits", sizeof(BCALHit_t)){};
 		~DFactory_BCALHits(){};
+		derror_t Print(void);
 	
 	private:
 		derror_t evnt(int eventnumber);		///< Called every event.
