7596d2f2907676dee9ba175b4e617aaa84d51697
David Lawrence <davidl@jlab.org>
2004-11-10 19:13:10 +0000

    Add FDCClusters factory

--- libraries/FDC/FDC_init.cc
+++ libraries/FDC/FDC_init.cc
@@ -2,11 +2,13 @@
 
 #include "DEvent.h"
 #include "DFactory_FDCHits.h"
+#include "DFactory_FDCClusters.h"
 
 derror_t FDC_init(DEvent *event)
 {
 	/// Create and register FDC data factories
 	event->AddFactory(new DFactory_FDCHits(event));
+	event->AddFactory(new DFactory_FDCClusters(event));
 
 	return NOERROR;
 }
