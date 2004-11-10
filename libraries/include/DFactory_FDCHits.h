24c5224cc6fb340585e975f8a2b421e88b4841a3
David Lawrence <davidl@jlab.org>
2004-11-10 17:50:34 +0000

    Defined TOFHits structure.

--- libraries/include/DFactory_FDCHits.h
+++ libraries/include/DFactory_FDCHits.h
@@ -8,13 +8,21 @@
 #include "DFactory.h"
 
 typedef struct{
-	// place holder for now;
+	int layer;
+	int module;
+	float tau;
+	float z;
+	float u;
+	float dE;
+	float t;
+	int type; ///< 0=anode, 1=cathode
 }FDCHit_t;
 
 class DFactory_FDCHits:public DFactory{
 	public:
 		DFactory_FDCHits(DEvent *event):DFactory(event, "FDCHits", sizeof(FDCHit_t)){};
 		~DFactory_FDCHits(){};
+		derror_t Print(void);
 	
 	private:
 		derror_t evnt(int eventnumber);		///< Called every event.
