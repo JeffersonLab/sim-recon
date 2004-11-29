683264810eb3c1902993bb32bdf434b409268ad5
David Lawrence <davidl@jlab.org>
2004-11-29 19:16:50 +0000

    Prettied up documentation. Added Print() method

--- libraries/include/DQuickFit.h
+++ libraries/include/DQuickFit.h
@@ -2,47 +2,46 @@
 //
 /// Do a very fast, linear-regression type fit on a set of hits.
 ///
-/// This class allows one to define a set of 2D or 3D points
+/// <p>This class allows one to define a set of 2D or 3D points
 /// which can then be fit to a circle (2D) or a helical track (3D)
 /// via the FitCircle() and FitTrack() methods. This is written
 /// in a generic way such that either CDC or FDC data or any
-/// combination of the two can be used.
+/// combination of the two can be used.</p>
 ///
-/// DQuickFit, as the name implies, is intended to be very fast.
-/// it will <bNOT</b> produce a terribly accurate result.
+/// <p>DQuickFit, as the name implies, is intended to be very fast.
+/// it will <b>NOT</b> produce a terribly accurate result.</p>
 ///
-/// This will hopefully be useful in a couple of places:
+/// <p>This will hopefully be useful in a couple of places:
 ///
-/// 1. When trying to find clusters, it can be used to help
+/// -# When trying to find clusters, it can be used to help
 ///    reject outlying hits.
-///
-/// 2. In the Level-3 or filtering application, it can be used
+/// -# In the Level-3 or filtering application, it can be used
 ///    to quickly identify whether a cluster has a reasonable
 ///    chance of becoming a "track"
-///
-/// 3. To find first-guess parameters for tracks which can
+/// -# To find first-guess parameters for tracks which can
 ///    be used as the starting point for real track fitting.
+/// </p>
 ///
-/// To use this class, simply instantiate it and then repeatedly
+/// <p>To use this class, simply instantiate it and then repeatedly
 /// call one of the <i>AddHit</i> methods to add points you want to
 /// fit. When all of the points have been added, invoke either
-/// the FitCircle(2D) or FitTrack(3D) method to perform the fit.
+/// the FitCircle() (2D) or FitTrack() (3D) method to perform the fit.
 /// Note that since the fits are done using a linear regression
 /// style and other "one-pass" calculations, there is no iteration. 
-/// It also assumes the same error for each point.
+/// It also assumes the same error for each point.</p>
 ///
-/// To fit results are stored in public members of the class.
+/// <p>The fit results are stored in public members of the class.
 /// The x0,y0 members represent the coordinates of the center of
 /// the 2D circle in whatever units the hits had when added. The
 /// chisq value is just the sum of the squares of the differences
-/// between each hit's distance from x0,y0 and r0=sqrt(x0^2 + y0^2).
+/// between each hit's distance from x0,y0 and \f$ r_0=\sqrt{x_0^2 + y_0^2} \f$.
 /// p_trans will have the transverse component of the particle's
-/// momentum.
+/// momentum.</p>
 ///
-/// A few methods are available to remove hits which do not
+/// <p>A few methods are available to remove hits which do not
 /// match certain criteria. These include PruneHits() and
 /// PruneWorst() (both of with call PruneHit()). See the notes
-/// in each for more info.
+/// in each for more info.</p>
 
 #ifndef _DQUICK_FIT_H_
 #define _DQUICK_FIT_H_
@@ -74,6 +73,7 @@ class DQuickFit{
 		inline int GetNhits(){return hits->nrows;};
 		derror_t PrintChiSqVector(void);
 		derror_t CopyToFitParms(FitParms_t *fit);
+		derror_t Print(void);
 
 		enum ChiSqSourceType_t{
 			NOFIT,
