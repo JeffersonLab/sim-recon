6ed44e8d0649649a850a37067b8017d7acebba3f
Brad Futch <futch@jlab.org>
2004-12-02 16:55:15 +0000

    DMagneticFiledMap files.

---
+++ libraries/TRACKING/DMagneticFieldMap.cpp
@@ -0,0 +1,266 @@
+//-------------------------------------------------------------------------
+//
+//      Author:     Brad Futch 
+//      Date:       Wed 01 Dec 2004 04:37:54 
+//
+//      Copyright (C) 2004 Brad Futch 
+//      
+//      This program is free software; you can redistribute it and/or
+//      modify it under the terms of the GNU General Public License
+//      as published by the Free Software Foundation; either version 2
+//      of the License, or (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, 
+//      MA  02111-1307, USA.
+//
+//-------------------------------------------------------------------------
+
+#ifndef __DMAGNETICFIELDMAP_CPP__
+#define __DMAGNETICFIELDMAP_CPP__
+
+/*! \file DMagneticFieldMap.cpp
+ *  \brief An implementation of a Magnetic Field Map.
+ *  @see DMagnetifField.h
+ */
+
+#include "DMagneticFieldMap.h"
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::cerr;
+using std::ifstream;
+using std::endl;
+
+
+//------------------------------------------------------------------
+// NOTE:
+//   
+//   I make the constructor take input dimensions because 
+//   as far as I can tell this information is not in the document.
+//   I would think that it would be better to have this information 
+//   in the file itself.
+//-------------------------------------------------------------------
+
+DMagneticFieldMap::DMagneticFieldMap(int rDim, int zDim)
+{
+  this->rDim = rDim;
+  this->zDim = zDim;
+  this->rMin = 0.0;
+  this->rMax = 0.0;
+  this->zMin = 0.0;
+  this->zMax = 0.0;
+  DEBUG = false;
+}
+
+DMagneticFieldMap::~DMagneticFieldMap()
+{
+  if (map)
+    delete map;
+}
+
+void DMagneticFieldMap::setDebug(const bool val)
+{
+  DEBUG = val;
+}
+
+void DMagneticFieldMap::readMap(ifstream &in)
+{
+  
+  if (!in.is_open() )
+  {
+    cout << "DMagneticFieldMap:readMap: Error reading solenoid map!" 
+         << endl;
+    exit(0);
+  }
+
+  //------------------------------------------------------------------
+  // NOTE: Data Format
+  //
+  // Based on the solenoid table format, read data (comments) until 
+  // the first occurance of the characters '=='.  At this point grab 
+  // the next 15 lines and throw them away.  The data begins on the
+  // next line.
+  //
+  // The formatted input is of the form:
+  //
+  // <xcoord> <ycoord> <zcoord>  <Bx> <By> <Bz>
+  //
+  // Again see the note in the header file about the xy symmetry.
+  //
+  //------------------------------------------------------------------
+  
+  int numSkip = 9;
+  char c;
+  char buf[1000];
+
+  //---------------------------
+  // Skip all of the comments
+  //---------------------------
+  
+  while (in.good())
+  {
+    c = in.get();
+    if (c == '=')
+    {
+      c = in.get();
+      if (c == '=')
+        break;
+    }
+  }
+
+  for (int i=0; i < numSkip; i++)
+  {
+    in.getline(buf, 1000);
+  }
+
+  //------------------------------------------------------------
+  // At this point we need the dimensions, one way or the other
+  //------------------------------------------------------------
+  
+  if (rDim == 0 || zDim == 0)
+  {
+    cerr << "DMagneticFieldMap:readMap: Data Dimensions not set, map "
+         << "NOT set" << endl;
+    return;
+  }
+
+  //-------------------------------------------
+  // Allocate some space and set up the loop
+  //-------------------------------------------
+
+  D3Vector_t pos;
+  D3Vector_t mag;
+  int        countR;
+  int        countZ;
+
+  countR     = 0;
+  countZ     = 0;
+  map        = new double [rDim*zDim*3];
+
+  while (!in.eof())
+  {
+
+    if (countZ >= zDim)
+    {
+      countZ = 0;
+      countR++;
+    }
+    if (countR >= rDim)
+    { 
+      //cerr << "DMagneticFieldMap:readMap:Warning  Dimension overrun" 
+      //     << endl;
+      return;
+    }
+
+    if (DEBUG)
+      printf("%d:%d: ", countR, countZ);
+    
+    in >> pos.x;
+    in >> pos.y;
+    in >> pos.z;
+
+    in >> mag.x;
+    in >> mag.y;
+    in >> mag.z;
+
+    if (DEBUG)
+      printf("POS(%.2f, %.2f, %.2f) B(%.2f, %.2f, %.2f)\n", 
+              pos.x,pos.y,pos.z,mag.x,mag.y,mag.z);
+
+    int ind; 
+    ind = serialize(countR, countZ);
+    
+    map[ind]     = mag.x;
+    map[ind + 1] = mag.y;
+    map[ind + 2] = mag.z;
+
+    countZ++;
+
+    //---------------------
+    // Set the min and max
+    //---------------------
+
+    this->rMin = pos.x < this->rMin ? pos.x : this->rMin;
+    this->rMax = pos.x > this->rMax ? pos.x : this->rMax;
+    
+    this->zMin = pos.z < this->zMin ? pos.z : this->zMin;
+    this->zMax = pos.z > this->zMax ? pos.z : this->zMax;
+  }
+}
+
+//------------------------------------
+// Serialize coordinates into the map
+//------------------------------------
+
+int DMagneticFieldMap::serialize(int r, int z)
+{
+  return (r*zDim*3) + z;
+}
+
+//----------------------------------------------------------------------
+// This is going to return the base indices from which to interpolate 
+// as well as the percentage of the distance to the next index.
+//
+// This should make the task of actuall interpolating much easier.
+//----------------------------------------------------------------------
+
+void DMagneticFieldMap::getInds(const double &r, const double &z, int ind[2],
+                                double &rho, double &zeta)
+{
+
+//----------------------------------------------------------------
+// First we need to map the the real-space coordinates into [0,1]
+//----------------------------------------------------------------
+
+  double rN, zN;
+  int baseR, baseZ;
+
+  rN = (r - rMin)/(rMax - rMin);
+  zN = (z - zMin)/(zMax - zMin);
+
+  baseR = (int)(rN * (rDim-1));
+  baseZ = (int)(zN * (zDim-1));
+
+  rho  = ((rN * (rDim-1)) - baseR)/(rDim-1);
+  zeta = ((zN * (zDim-1)) - baseZ)/(zDim-1);
+
+/*
+  printf("(rMin, rMax)(zMin, zMax):  (%.2f, %.2f)(%.2f, %.2f)\n", 
+          rMin, rMax, zMin, zMax);
+  printf("(r,z)->(rN, zN):           (%.2f, %.2f) -> (%f, %f)\n", r, z, rN, zN); 
+  printf("(baseR, baseZ)(rho, zeta): (%d, %d)(%f, %f)\n", 
+         baseR, baseZ, rho, zeta);
+*/
+}
+
+
+
+
+
+
+#endif
+
+    
+
+
+
+
+
+  
+
+
+
+
+
+  
+
+
+
---
+++ libraries/TRACKING/DMagneticFieldMap.h
@@ -0,0 +1,150 @@
+//-------------------------------------------------------------------------
+//
+//      Author:     Brad Futch 
+//      Date:       Wed 01 Dec 2004 02:04:52 
+//
+//      Copyright (C) 2004 Brad Futch 
+//      
+//      This program is free software; you can redistribute it and/or
+//      modify it under the terms of the GNU General Public License
+//      as published by the Free Software Foundation; either version 2
+//      of the License, or (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 59 Temple Place - Suite 330, Boston, 
+//      MA  02111-1307, USA.
+//
+//
+//-------------------------------------------------------------------------
+
+#ifndef __DMAGNETICFIELDMAP_H__
+#define __DMAGNETICFIELDMAP_H__
+
+/*! \file DMagneticFieldMap.h
+ *  \brief A class that holds the magnetic field map for the detector.
+ *
+ *  This class provides an interface to the map that is created for 
+ *  the Solenoid used in this project.  The current implementation reads
+ *  a solenoid table in the format described in the dsolenoid.table file.
+ *
+ *  NOTE:  There is an assumed radial symmetry, so there is actually only 
+ *         an x and z entry in the table.  The x coordinate is assumed 
+ *         to be equivalent to the radius.
+ */
+
+#include <fstream>
+
+using std::ifstream;
+
+/*! \var typedef DVEC D3Vector_t
+ *  \brief A simple magnetic field vector.
+ *
+ *  This is just an implementation of a 3-vector of doubles.
+ */
+
+typedef struct DVEC
+{
+  double x;
+  double y;
+  double z;
+} D3Vector_t;
+
+
+
+class DMagneticFieldMap
+{
+
+  public:
+
+    DMagneticFieldMap(int rDim, int zDim);   /*!< Constructor */
+    ~DMagneticFieldMap();                    /*!< Destructor  */
+
+    /*!
+     * A method to read a solenoid map from a file.
+     * @param in - ifstream input
+    */
+
+    void          readMap(ifstream &in);
+
+    //@{
+    /*! These are overloads of the getMagVec method */
+    D3Vector_t    getMagVec(const D3Vector_t *vec);
+    D3Vector_t    getMagVec(double x, double y, double z);
+    D3Vector_t    getMagVec(double r, double z);
+    //@}
+
+    /*! Set the Debug state
+     * @param val - state to set
+     */
+    void          setDebug(const bool val);
+
+    /*!
+     * A utility function to return nearest indices.
+     * @param r - a double radius
+     * @param z - a double z value
+     * @param ind - a 2D int vector of return indices
+     * @param rho - amount to interpolate in the radial direction
+     * @param zeta - amount to interpolte in the z direction
+     *
+     *  This will be used for the trilinear interpolation in order 
+     *  to fetch the closest indices from which to interpolate.
+     */
+    
+    void          getInds(const double &r, const double &z, int ind[2],
+                          double &rho, double &zeta);
+
+
+  private:
+
+    double *map;   /*!< serialized array for Mag. Field values  */
+    double rMin;   /*!< minimum double radius coordinate  */
+    double rMax;   /*!< maximum double radius coordinate  */
+    double zMin;   /*!< minimum double z coordinate  */
+    double zMax;   /*!< maximum double z coordinate  */
+    int    rDim;   /*!< radial dimension of internal data structure  */
+    int    zDim;   /*!< z dimension of internal data structure  */
+    bool   DEBUG;  /*!< if true output debug informations  */
+
+    //-----------------
+    // Utility Methods
+    //-----------------
+    
+   
+    /*!
+     *  This utility funciton based on the assumption of radial symmetry.
+     *  @param r - integer radius index
+     *  @param z - integer z index
+     *
+     *  This will return the base index for the value.  The actual xyz values
+     *  will be:
+     *
+     *  x = map[ind]
+     *  y = map[ind+1]
+     *  z = map[ind+2]
+     */
+     
+    int           serialize(int r, int z);
+
+
+};
+
+#endif
+    
+
+                          
+                                  
+
+
+    
+
+
+
+
+
+
