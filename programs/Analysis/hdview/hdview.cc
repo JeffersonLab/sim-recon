6efea9933b64c23f42382399c576516bcb7b703d
David Lawrence <davidl@jlab.org>
2004-12-09 21:18:34 +0000

    Change the order things are instantiated so we can have the detectors drawn before the first event. Otherwise, the window is just blank

--- programs/Analysis/hdview/hdview.cc
+++ programs/Analysis/hdview/hdview.cc
@@ -15,16 +15,21 @@ hdv_mainframe *hdvmf=NULL;
 //-------------------
 int main(int narg, char *argv[])
 {
-	// Instantiate an event loop object and initialize it
+	// Instantiate an event loop object this has to be done BEFORE
+	// creating the TApplication object since that modifes the argument list.
 	eventloop = new DEventLoop(narg, argv);
-	myproc = new MyProcessor();
-	eventloop->AddProcessor(myproc);
-	eventloop->Init();
 
 	// Open Window
 	TApplication app("HDView", &narg, argv);
 	hdvmf = new hdv_mainframe(gClient->GetRoot(), 1000, 600);
 	
+	// This is done AFTER creating the TApplication object so when the
+	// init routine is called, the window will be mapped and it can
+	// draw the detectors.
+	myproc = new MyProcessor();
+	eventloop->AddProcessor(myproc);
+	eventloop->Init();
+
 	// Hand control to ROOT event loop
 	app.Run();
 	
