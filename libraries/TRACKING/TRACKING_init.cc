c1fa5058631fc5a6f252b08370ae0912879918d3
David Lawrence <davidl@jlab.org>
2005-02-11 12:22:36 +0000

    Add MCTrackCandidates factory

--- libraries/TRACKING/TRACKING_init.cc
+++ libraries/TRACKING/TRACKING_init.cc
@@ -3,12 +3,14 @@
 #include "DEvent.h"
 #include "DFactory_TRACKINGHits.h"
 #include "DFactory_MCCheatHits.h"
+#include "DFactory_MCTrackCandidates.h"
 
 derror_t TRACKING_init(DEvent *event)
 {
 	/// Create and register TRACKING data factories
 	event->AddFactory(new DFactory_TRACKINGHits(event));
 	event->AddFactory(new DFactory_MCCheatHits(event));
+	event->AddFactory(new DFactory_MCTrackCandidates(event));
 
 	return NOERROR;
 }
