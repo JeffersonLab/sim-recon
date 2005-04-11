273308074968527c0568e36eea6fbfebfbcdaf2d
David Lawrence <davidl@jlab.org>
2005-04-11 18:55:56 +0000

    These files are generated via rootcint. The Makefile includes a rule to make them, but if they don't previously exist, it won't work. Hence a copy is included here. A make special "clean" rule may also be needed.

---
+++ programs/Analysis/hdview/hdv_mainframe_Dict.h
@@ -0,0 +1,48 @@
+/********************************************************************
+* hdv_mainframe_Dict.h
+********************************************************************/
+#ifdef __CINT__
+#error hdv_mainframe_Dict.h/C is only for compilation. Abort cint.
+#endif
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define G__ANSIHEADER
+#define G__DICTIONARY
+#include "G__ci.h"
+extern "C" {
+extern void G__cpp_setup_tagtablehdv_mainframe_Dict();
+extern void G__cpp_setup_inheritancehdv_mainframe_Dict();
+extern void G__cpp_setup_typetablehdv_mainframe_Dict();
+extern void G__cpp_setup_memvarhdv_mainframe_Dict();
+extern void G__cpp_setup_globalhdv_mainframe_Dict();
+extern void G__cpp_setup_memfunchdv_mainframe_Dict();
+extern void G__cpp_setup_funchdv_mainframe_Dict();
+extern void G__set_cpp_environmenthdv_mainframe_Dict();
+}
+
+
+#include "TROOT.h"
+#include "TMemberInspector.h"
+#include "hdv_mainframe.h"
+
+#ifndef G__MEMFUNCBODY
+#endif
+
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TClass;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TBuffer;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TMemberInspector;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TObject;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TGWindow;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TGObject;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TQObject;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TGFrame;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TGCompositeFrame;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TGMainFrame;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TText;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_TRootEmbeddedCanvas;
+extern G__linked_taginfo G__hdv_mainframe_DictLN_hdv_mainframe;
+
+/* STUB derived class for protected member access */
