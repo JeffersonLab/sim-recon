4e2cd56e5a38620137c85dbe6ce81057ef354b94
Ryan Mitchell <remitche@jlab.org>
2005-05-17 15:58:17 +0000

    Added the DFCALShowers factory to the list of factories.

--- libraries/FCAL/FCAL_init.cc
+++ libraries/FCAL/FCAL_init.cc
@@ -2,11 +2,13 @@
 
 #include "DEvent.h"
 #include "DFactory_DFCALHit.h"
+#include "DFactory_DFCALShowers.h"
 
 derror_t FCAL_init(DEvent *event)
 {
 	/// Create and register FCAL data factories
 	event->AddFactory(new DFactory_DFCALHit());
+	event->AddFactory(new DFactory_DFCALShowers());
 
 	return NOERROR;
 }
