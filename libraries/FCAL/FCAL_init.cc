c686042b5960abb49b71d88ecb9e14c629d7bf69
Ryan Mitchell <remitche@jlab.org>
2005-05-17 17:00:21 +0000

    Changed the name of the factory DFCALShowers to DFCALShower for consistency with other factories.

--- libraries/FCAL/FCAL_init.cc
+++ libraries/FCAL/FCAL_init.cc
@@ -2,13 +2,13 @@
 
 #include "DEvent.h"
 #include "DFactory_DFCALHit.h"
-#include "DFactory_DFCALShowers.h"
+#include "DFactory_DFCALShower.h"
 
 derror_t FCAL_init(DEvent *event)
 {
 	/// Create and register FCAL data factories
 	event->AddFactory(new DFactory_DFCALHit());
-	event->AddFactory(new DFactory_DFCALShowers());
+	event->AddFactory(new DFactory_DFCALShower());
 
 	return NOERROR;
 }
