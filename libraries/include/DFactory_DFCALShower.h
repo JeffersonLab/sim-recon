b7b88ee2fe330d76c1ed48fec4ab50d4d921a50e
Ryan Mitchell <remitche@jlab.org>
2005-05-17 17:10:16 +0000

    A factory to create FCAL showers based on FCAL hits from the DFCALHit factory.

---
+++ libraries/include/DFactory_DFCALShower.h
@@ -0,0 +1,25 @@
+// $Id$
+//
+//    File: DFactory_DFCALShower.h
+// Created: Tue May 17 11:57:50 EST 2005
+// Creator: remitche (on Linux mantrid00 2.4.20-18.8smp i686)
+//
+
+#ifndef _DFactory_DFCALShower_
+#define _DFactory_DFCALShower_
+
+#include "DFactory.h"
+#include "DFCALShower.h"
+
+class DFactory_DFCALShower:public DFactory<DFCALShower>{
+	public:
+		DFactory_DFCALShower(){};
+		~DFactory_DFCALShower(){};
+		const string toString(void);
+	
+	private:
+		derror_t evnt(int eventnumber);	///< Invoked via DEventProcessor virtual method
+};
+
+#endif // _DFactory_DFCALShower_
+
