8cbf7b3f9e436735d478c37b9ef1d3eb39f0a8a2
Ryan Mitchell <remitche@jlab.org>
2005-07-18 16:58:32 +0000

    Added a geometry factory (DTOFGeometry).

--- libraries/TOF/TOF_init.cc
+++ libraries/TOF/TOF_init.cc
@@ -2,11 +2,13 @@
 
 #include "DEventLoop.h"
 #include "DFactory_DTOFHit.h"
+#include "DFactory_DTOFGeometry.h"
 
 derror_t TOF_init(DEventLoop *loop)
 {
 	/// Create and register TOF data factories
 	loop->AddFactory(new DFactory_DTOFHit());
+	loop->AddFactory(new DFactory_DTOFGeometry());
 
 	return NOERROR;
 }
