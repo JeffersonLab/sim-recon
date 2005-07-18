ad700db3bdf1c421bcf3e069520d80c1577da573
Ryan Mitchell <remitche@jlab.org>
2005-07-18 21:04:29 +0000

    Added the bar2y and y2bar methods to convert between bar number and bar position in the local coordinate system.

--- libraries/include/DTOFGeometry.h
+++ libraries/include/DTOFGeometry.h
@@ -20,7 +20,40 @@ class DTOFGeometry{
                 float SHORTBARLENGTH; ///> length of the short scintillators
                 float BARWIDTH;       ///> width of the scintillator bars
 
-		
+
+                float bar2y(int bar, int orientation)   ///> convert bar number to the
+                                                        ///> position of the center of the
+                                                        ///> bar in local coordinations
+                  {
+                    float y;
+                    if (bar <= NLONGBARS){
+                      y = ((NLONGBARS/2.0)+(NSHORTBARS/4.0)-(bar-0.5))*BARWIDTH;
+                      if (bar > (NLONGBARS/2)) y -= (NSHORTBARS/2.0)*BARWIDTH;
+                    }
+                    else{
+                      y = ((NSHORTBARS/4.0)-(((bar-NLONGBARS+1)/2)-0.5))*BARWIDTH;
+                    }
+                    if (orientation == 0) y *= -1.0;
+                    return y;
+                  }
+
+
+               int y2bar(float x, float y, int orientation)   ///> convert local x,y to 
+                                                              ///> bar number
+                 {
+                   int bar;
+                   if (orientation == 0) {float temp = y; y = -1.0*x; x = temp;}
+                   if (fabs(y) < (NSHORTBARS/4.0)*BARWIDTH){
+                     bar = NLONGBARS + (int)((NSHORTBARS/4.0*BARWIDTH-y)/BARWIDTH)*2 + 1;
+                     if (x > 0) bar++;
+                   }
+                   else{
+                     bar = (int)(((NLONGBARS/2.0+NSHORTBARS/4.0)*BARWIDTH-y)/BARWIDTH) + 1;
+                     if (bar > NLONGBARS/2) bar -= NSHORTBARS/2;
+                   }
+                   return bar;
+                 }
+
 };
 
 #endif // _DTOFGeometry_
