8a9b2c537231c8e3cc14a48a7e6c28ae72faa925
David Lawrence <davidl@jlab.org>
2005-08-17 15:27:56 +0000

    Base class for all data classes

---
+++ libraries/DANA/DObject.cc
@@ -0,0 +1,24 @@
+// $Id$
+//
+//    File: DObject.cc
+// Created: Wed Aug 17 10:57:09 EDT 2005
+// Creator: davidl (on Darwin wire129.jlab.org 7.8.0 powerpc)
+//
+
+#include "DObject.h"
+
+//---------------------------------
+// DObject    (Constructor)
+//---------------------------------
+DObject::DObject()
+{
+
+}
+
+//---------------------------------
+// ~DObject    (Destructor)
+//---------------------------------
+DObject::~DObject()
+{
+
+}
---
+++ libraries/include/DObject.h
@@ -0,0 +1,32 @@
+// $Id$
+//
+//    File: DObject.h
+// Created: Wed Aug 17 10:57:09 EDT 2005
+// Creator: davidl (on Darwin wire129.jlab.org 7.8.0 powerpc)
+//
+
+#ifndef _DObject_
+#define _DObject_
+
+#include "derror.h"
+
+
+typedef int identifier_t;
+
+
+class DObject{
+
+	/// The DObject class is a base class for all data classes.
+	/// In other words, all classes which are produced by DANA
+	/// factories.
+
+	public:
+		DObject();
+		virtual ~DObject();
+
+		identifier_t id;
+		
+};
+
+#endif // _DObject_
+
