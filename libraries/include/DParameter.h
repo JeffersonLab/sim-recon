d5355441aecdaae78c6ec0c033c24fd0b742e5fd
David Lawrence <davidl@jlab.org>
2005-08-22 12:17:11 +0000

    Added enum for data types. Added various flavors of DataType methods.  Added decalaration of Dump and DataName methods.

--- libraries/include/DParameter.h
+++ libraries/include/DParameter.h
@@ -30,6 +30,45 @@ class DParameter{
 		inline float f(void){return (float)atof(value.c_str());}		///< Return value as a float
 		inline double d(void){return (double)atof(value.c_str());}	///< Return value as a double
 		inline int i(void){return (int)atoi(value.c_str());}			///< Return value as an int
+		void Dump(void);
+		
+		enum dataType_t{
+			UNKNOWN,
+			BOOL,
+			CHAR,
+			CHAR_PTR,
+			CONST_CHAR_PTR,
+			STRING,
+			SHORT,
+			INT,
+			LONG,
+			LONGLONG,
+			UCHAR,
+			USHORT,
+			UINT,
+			ULONG,
+			ULONGLONG,
+			FLOAT,
+			DOUBLE
+		};
+		static inline dataType_t DataType(bool &v){return BOOL;}
+		static inline dataType_t DataType(char &v){return CHAR;}
+		static inline dataType_t DataType(char* &v){return CHAR_PTR;}
+		static inline dataType_t DataType(const char* &v){return CONST_CHAR_PTR;}
+		static inline dataType_t DataType(string &v){return STRING;}
+		static inline dataType_t DataType(short &v){return SHORT;}
+		static inline dataType_t DataType(int &v){return INT;}
+		static inline dataType_t DataType(long &v){return LONG;}
+		static inline dataType_t DataType(long long &v){return LONGLONG;}
+		static inline dataType_t DataType(unsigned char &v){return UCHAR;}
+		static inline dataType_t DataType(unsigned short &v){return USHORT;}
+		static inline dataType_t DataType(unsigned int &v){return UINT;}
+		static inline dataType_t DataType(unsigned long &v){return ULONG;}
+		static inline dataType_t DataType(unsigned long long &v){return ULONGLONG;}
+		static inline dataType_t DataType(float &v){return FLOAT;}
+		static inline dataType_t DataType(double &v){return DOUBLE;}
+		
+		static const char* DataName(dataType_t type);
 		
 	protected:
 		string key;
@@ -37,6 +76,7 @@ class DParameter{
 		bool isdefault;	///< is the current value set by SetDefaultParameter ?
 		bool hasdefault;	///< was SetDefaultParameter ever called for this key ?
 		bool printme;		///< used by ParameterManager::PrintParameters()
+		dataType_t type;	///< data type used in last set
 	private:
 
 };
