802baebd633a3d8587c68317db11316b4fb42d35
David Lawrence <davidl@jlab.org>
2005-08-22 12:18:40 +0000

    Added Dump and DataName methods.

--- libraries/DANA/DParameter.cc
+++ libraries/DANA/DParameter.cc
@@ -5,6 +5,8 @@
 // Creator: davidl (on Darwin wire129.jlab.org 7.8.0 powerpc)
 //
 
+#include <iostream>
+
 #include "DParameter.h"
 
 //---------------------------------
@@ -25,3 +27,47 @@ DParameter::~DParameter()
 {
 
 }
+
+//---------------------------------
+// Dump
+//---------------------------------
+void DParameter::Dump(void)
+{
+	cout<<" -----------------------------"<<endl;
+	cout<<"  className: "<<className()<<endl;
+	cout<<"        key: "<<key<<endl;
+	cout<<"      value: "<<value<<endl;
+	cout<<"  isdefault: "<<isdefault<<endl;
+	cout<<" hasdefault: "<<hasdefault<<endl;
+	cout<<"    printme: "<<printme<<endl;
+	cout<<"       type: "<<DataName(type)<<endl;
+}
+
+//---------------------------------
+// DataName
+//---------------------------------
+const char* DParameter::DataName(dataType_t type)
+{
+	switch(type){
+		case UNKNOWN:			return "unknown";
+		case BOOL:				return "bool";
+		case CHAR:				return "char";
+		case CHAR_PTR:			return "char*";
+		case CONST_CHAR_PTR:	return "const char*";
+		case STRING:			return "string";
+		case SHORT:				return "short";
+		case INT:				return "int";
+		case LONG:				return "long";
+		case LONGLONG:			return "long long";
+		case UCHAR:				return "unsigned char";
+		case USHORT:			return "unsigned short";
+		case UINT:				return "unsigned int";
+		case ULONG:				return "unsigned long";
+		case ULONGLONG:		return "unsigned long long";
+		case FLOAT:				return "float";
+		case DOUBLE:			return "double";
+	}
+	return "unknown";
+}
+
+
