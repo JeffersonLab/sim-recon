34bf6ab06b15205d97158f0348ec6c7e2ae9e63c
David Lawrence <davidl@jlab.org>
2005-08-25 18:50:50 +0000

    Renamed DMCTrackCandidate to DTrackCandidate

--- libraries/include/DFactory_DTrackCandidate.h
+++ libraries/include/DFactory_DTrackCandidate.h
@@ -1,35 +1,33 @@
 // $Id$
 //
-//    File: DFactory_DMCTrackCandidate_B.h
+//    File: DFactory_DTrackCandidate.h
 // Created: Mon Jul 18 15:23:04 EDT 2005
 // Creator: davidl (on Darwin wire129.jlab.org 7.8.0 powerpc)
 //
 
-#ifndef _DFactory_DMCTrackCandidate_B_
-#define _DFactory_DMCTrackCandidate_B_
+#ifndef _DFactory_DTrackCandidate_
+#define _DFactory_DTrackCandidate_
 
 #include <TH1.h>
 
 #include "DFactory.h"
 #include "DQuickFit.h"
-#include "DMCTrackCandidate.h"
-#include "DTrkHit.h"
+#include "DTrackCandidate.h"
+#include "../TRACKING/Dtrkhit.h"
 
 
-class DFactory_DMCTrackCandidate_B:public DFactory<DMCTrackCandidate>{
+class DFactory_DTrackCandidate:public DFactory<DTrackCandidate>{
 	public:
-		DFactory_DMCTrackCandidate_B();
-		~DFactory_DMCTrackCandidate_B(){};
+		DFactory_DTrackCandidate();
+		~DFactory_DTrackCandidate(){};
 		const string toString(void);
-		const char* Tag(void){return "B";}
-		void SetMaxDebugBuffers(int N){MAX_DEBUG_BUFFERS = N;}
-		static void Fill_phi_circle(vector<DTrkHit*> hits, float x0, float y0);
+		static void Fill_phi_circle(vector<Dtrkhit*> hits, float x0, float y0);
 		
-		vector<DTrkHit*>& Get_trkhits(void){return trkhits;}
-		vector<vector<DTrkHit*> >& Get_dbg_in_seed(void){return dbg_in_seed;}
-		vector<vector<DTrkHit*> >& Get_dbg_hoc(void){return dbg_hoc;}
-		vector<vector<DTrkHit*> >& Get_dbg_hol(void){return dbg_hol;}
-		vector<vector<DTrkHit*> >& Get_dbg_hot(void){return dbg_hot;}
+		vector<Dtrkhit*>& Get_trkhits(void){return trkhits;}
+		vector<vector<Dtrkhit*> >& Get_dbg_in_seed(void){return dbg_in_seed;}
+		vector<vector<Dtrkhit*> >& Get_dbg_hoc(void){return dbg_hoc;}
+		vector<vector<Dtrkhit*> >& Get_dbg_hol(void){return dbg_hol;}
+		vector<vector<Dtrkhit*> >& Get_dbg_hot(void){return dbg_hot;}
 		vector<DQuickFit*>& Get_dbg_seed_fit(void){return dbg_seed_fit;}
 		vector<DQuickFit*>& Get_dbg_track_fit(void){return dbg_track_fit;}
 		vector<int>& Get_dbg_seed_index(void){return dbg_seed_index;}
@@ -46,30 +44,30 @@ class DFactory_DMCTrackCandidate_B:public DFactory<DMCTrackCandidate>{
 		void ClearEvent(void);
 		void GetTrkHits(DEventLoop *loop);
 		int FindSeed(void);
-		int TraceSeed(DTrkHit *hit);
-		DTrkHit* FindClosestXY(DTrkHit *hit);
+		int TraceSeed(Dtrkhit *hit);
+		Dtrkhit* FindClosestXY(Dtrkhit *hit);
 		int FitSeed(void);
 		int FindLineHits(void);
 		int FindPhiZAngle(void);
 		int FindZvertex(void);
 		int FitTrack(void);
-		int MarkTrackHits(DMCTrackCandidate* mctrackcandidate, DQuickFit *fit);
-		inline void ChopSeed(void){if(hits_in_seed.size()>0)hits_in_seed[0]->flags |= DTrkHit::IGNORE;}
+		int MarkTrackHits(DTrackCandidate* trackcandidate, DQuickFit *fit);
+		inline void ChopSeed(void){if(hits_in_seed.size()>0)hits_in_seed[0]->flags |= Dtrkhit::IGNORE;}
 
 
 		void DebugMessage(int line);
 		int SeedTrack(void);
 
-		vector<DTrkHit*> trkhits; // sorted by z
-		vector<DTrkHit*> trkhits_r_sorted; // sorted by dist. from beam line
-		vector<DTrkHit*> hits_in_seed;
-		vector<DTrkHit*> hits_on_circle;
-		vector<DTrkHit*> hits_on_line;
-		vector<DTrkHit*> hits_on_track;
-		vector<vector<DTrkHit*> > dbg_in_seed;
-		vector<vector<DTrkHit*> > dbg_hoc;
-		vector<vector<DTrkHit*> > dbg_hol;
-		vector<vector<DTrkHit*> > dbg_hot;
+		vector<Dtrkhit*> trkhits; // sorted by z
+		vector<Dtrkhit*> trkhits_r_sorted; // sorted by dist. from beam line
+		vector<Dtrkhit*> hits_in_seed;
+		vector<Dtrkhit*> hits_on_circle;
+		vector<Dtrkhit*> hits_on_line;
+		vector<Dtrkhit*> hits_on_track;
+		vector<vector<Dtrkhit*> > dbg_in_seed;
+		vector<vector<Dtrkhit*> > dbg_hoc;
+		vector<vector<Dtrkhit*> > dbg_hol;
+		vector<vector<Dtrkhit*> > dbg_hot;
 		vector<DQuickFit*> dbg_seed_fit;
 		vector<DQuickFit*> dbg_track_fit;
 		vector<int> dbg_seed_index;
@@ -84,7 +82,7 @@ class DFactory_DMCTrackCandidate_B:public DFactory<DMCTrackCandidate>{
 		unsigned int MAX_SEED_HITS;
 		float MAX_CIRCLE_DIST;
 		float MAX_PHI_Z_DIST;
-		float MAX_DEBUG_BUFFERS;
+		unsigned int MAX_DEBUG_BUFFERS;
 		float TARGET_Z_MIN;
 		float TARGET_Z_MAX;
 		float phizangle_bin_size;
@@ -92,10 +90,11 @@ class DFactory_DMCTrackCandidate_B:public DFactory<DMCTrackCandidate>{
 		float x0,y0,r0;
 		float phizangle, z_vertex;
 		float phizangle_min, phizangle_max;
+		string TRACKHIT_SOURCE;
 		
 		TH1F *phizangle_hist, *zvertex_hist, *phi_relative;
 		
 };
 
-#endif // _DFactory_DMCTrackCandidate_B_
+#endif // _DFactory_DTrackCandidate_
 
--- libraries/include/DTrackCandidate.h
+++ libraries/include/DTrackCandidate.h
@@ -1,33 +1,30 @@
 // $Id$
 //
-//    File: DMCTrackCandidate.h
+//    File: DTrackCandidate.h
 // Created: Sun Apr  3 12:38:16 EDT 2005
 // Creator: davidl (on Darwin Harriet.local 7.8.0 powerpc)
 //
 
-#ifndef _DMCTrackCandidate_
-#define _DMCTrackCandidate_
+#ifndef _DTrackCandidate_
+#define _DTrackCandidate_
 
 #include "DObject.h"
 #include "DFactory.h"
 
 #define MAX_IHITS 256
 
-class DMCTrackCandidate:public DObject{
+class DTrackCandidate:public DObject{
 	public:
-		HDCLASSDEF(DMCTrackCandidate);
+		HDCLASSDEF(DTrackCandidate);
 		
-		int Nhits;
-		int ihit[MAX_IHITS];	///< index of hits in MCCheatHits factory
+		vector<identifier_t> hitid;	///< ids of hits in DTrackHit factory
 		float x0,y0;			///< center of circle
 		float z_vertex;		///< z coordinate of vertex
 		float dphidz;			///< dphi/dz in radians per cm
 		float q;					///< electric charge 
 		float p, p_trans;		///< total and transverse momenta in GeV/c
 		float phi, theta;		///< theta and phi in radians
-		int track;				///< track from which most hits came from
-		int Ntrackhits;		///< number of hits in this candidate that came from "track"
 };
 
-#endif // _DMCTrackCandidate_
+#endif // _DTrackCandidate_
 
