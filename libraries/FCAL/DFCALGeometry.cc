a80cc689cac98d83bd3c595ec10d129f23254c1c
Matt Shepherd <shepherd@jlab.org>
2005-09-15 19:53:32 +0000

    FCAL geometry class

---
+++ libraries/FCAL/DFCALGeometry.cc
@@ -0,0 +1,112 @@
+// $Id$
+//
+//    File: DFCALGeometry.cc
+// Created: Wed Aug 24 10:18:56 EST 2005
+// Creator: shepherd (on Darwin 129-79-159-16.dhcp-bl.indiana.edu 8.2.0 powerpc)
+//
+
+#include "DFCALGeometry.h"
+#include "TVector2.h"
+
+//---------------------------------
+// DFCALGeometry    (Constructor)
+//---------------------------------
+DFCALGeometry::DFCALGeometry() : m_numActiveBlocks( 0 )
+{
+	
+	double innerRadius = ( kBeamHoleSize - 1 ) / 2. * kBlockSize * sqrt(2.);
+
+	// inflate the innner radius by 1% to for "safe" comparison
+	innerRadius *= 1.01;
+	
+	for( int row = 0; row < kBlocksTall; row++ ){
+		for( int col = 0; col < kBlocksWide; col++ ){
+			
+			// transform to beam axis
+			m_positionOnFace[row][col] = 
+			   TVector2( ( col + .5 - kBlocksWide / 2.0 ) * kBlockSize,
+					     ( row + .5 - kBlocksTall / 2.0 ) * kBlockSize );
+			
+			double radius = m_positionOnFace[row][col].Mod();
+			
+			if( ( radius < kRadius ) && ( radius > innerRadius ) ){
+
+				m_activeBlock[row][col] = true;
+				
+				// build the "channel map"
+				m_channelNumber[row][col] = m_numActiveBlocks;
+				m_row[m_numActiveBlocks] = row;
+				m_column[m_numActiveBlocks] = col;
+
+				m_numActiveBlocks++;
+			}
+			else{
+				
+				m_activeBlock[row][col] = false;
+			}
+		}
+	}
+		
+	cout << "FCAL Geometry initialized with " << m_numActiveBlocks 
+		<< " active blocks." << endl;
+}
+
+//---------------------------------
+// ~DFCALGeometry    (Destructor)
+//---------------------------------
+DFCALGeometry::~DFCALGeometry()
+{
+
+}
+
+bool
+DFCALGeometry::isBlockActive( int row, int column ) const
+{
+	assert(    row >= 0 &&    row < kBlocksTall );
+	assert( column >= 0 && column < kBlocksWide );
+	
+	return m_activeBlock[row][column];	
+}
+
+int
+DFCALGeometry::row( float y ) const 
+{	
+	return static_cast<int>( y / kBlockSize + ( kBlocksTall - 1 ) / 2 );
+}
+
+int
+DFCALGeometry::column( float x ) const 
+{	
+	return static_cast<int>( x / kBlockSize + ( kBlocksWide - 1 ) / 2 );
+}
+
+TVector2
+DFCALGeometry::positionOnFace( int row, int column ) const
+{ 
+	assert(    row >= 0 &&    row < kBlocksTall );
+	assert( column >= 0 && column < kBlocksWide );
+	
+	return m_positionOnFace[row][column]; 
+}
+
+TVector2
+DFCALGeometry::positionOnFace( int channel ) const
+{
+	assert( channel >= 0 && channel < m_numActiveBlocks );
+	
+	return positionOnFace( m_row[channel], m_column[channel] );
+}
+
+int
+DFCALGeometry::channel( int row, int column ) const
+{
+	if( isBlockActive( row, column ) ){
+		
+		return m_channelNumber[row][column]; 
+	}
+	else{
+		
+		cerr << "ERROR: request for channel number of inactive block!" << endl;
+		return -1;
+	}
+}
---
+++ libraries/include/DFCALGeometry.h
@@ -0,0 +1,78 @@
+// $Id$
+//
+//    File: DFCALGeometry.h
+// Created: Wed Aug 24 10:09:27 EST 2005
+// Creator: shepherd (on Darwin 129-79-159-16.dhcp-bl.indiana.edu 8.2.0 powerpc)
+//
+
+#ifndef _DFCALGeometry_
+#define _DFCALGeometry_
+
+#include "DFactory.h"
+#include "DObject.h"
+#include "TVector2.h"
+#include "units.h"
+
+/*
+class Vector2 {
+	
+public:
+	
+	Vector2() : m_x( 0 ), m_y( 0 ) {}
+	Vector2( double x, double y ) : m_x( x ), m_y( y ) {}
+	
+	double Mod( void ) const { return sqrt( m_x * m_x +
+											m_y * m_y ); }
+	
+private:
+	
+	double m_x;
+	double m_y;
+};
+*/
+
+class DFCALGeometry : public DObject {
+
+public:
+	
+	HDCLASSDEF(DFCALGeometry);
+	
+	static const int kBlocksWide      = 53;
+	static const int kBlocksTall      = 53;
+	static const int kMaxChannels     = kBlocksWide * kBlocksTall;
+	static const int kBeamHoleSize    =  3;
+	static const double kBlockSize    =    4 * k_cm;
+	static const double kRadius       = 1.08 * k_m;
+
+	DFCALGeometry();
+	~DFCALGeometry();
+	
+	bool isBlockActive( int row, int column ) const;
+	int  numActiveBlocks() const { return m_numActiveBlocks; }
+	
+	TVector2 positionOnFace( int row, int column ) const;
+	TVector2 positionOnFace( int channel ) const;
+	
+	int channel( int row, int column ) const;
+
+	int row   ( int channel ) const { return m_row[channel];    }
+	int column( int channel ) const { return m_column[channel]; }
+	
+	// get row and column from x and y positions
+	int row   ( float y ) const;
+	int column( float x ) const;
+	
+private:
+
+	bool   m_activeBlock[kBlocksTall][kBlocksWide];
+	TVector2 m_positionOnFace[kBlocksTall][kBlocksWide];
+
+	int    m_channelNumber[kBlocksTall][kBlocksWide];
+	int    m_row[kMaxChannels];
+	int    m_column[kMaxChannels];
+	
+	int    m_numActiveBlocks;
+};
+
+#endif // _DFCALGeometry_
+
