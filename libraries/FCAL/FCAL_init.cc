51f177d1634eb868ac4ce3a06b19266b5acc7171
Matt Shepherd <shepherd@jlab.org>
2005-09-15 20:00:05 +0000

    add init statements for new factories

--- libraries/FCAL/FCAL_init.cc
+++ libraries/FCAL/FCAL_init.cc
@@ -3,12 +3,18 @@
 #include "DEventLoop.h"
 #include "DFactory_DFCALHit.h"
 #include "DFactory_DFCALShower.h"
+#include "DFactory_DFCALGeometry.h"
+#include "DFactory_DHDDMForwardShower.h"
+#include "DFactory_DFCALMCResponse.h"
 
 derror_t FCAL_init(DEventLoop *loop)
 {
 	/// Create and register FCAL data factories
 	loop->AddFactory(new DFactory_DFCALHit());
 	loop->AddFactory(new DFactory_DFCALShower());
+	loop->AddFactory(new DFactory_DFCALGeometry());
+	loop->AddFactory(new DFactory_DHDDMForwardShower());
+	loop->AddFactory(new DFactory_DFCALMCResponse());
 
 	return NOERROR;
 }
