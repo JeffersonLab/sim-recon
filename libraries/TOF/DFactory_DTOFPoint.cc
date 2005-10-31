d592a5ea4322ee0b132fc81f916c91f4e283d441
Ryan Mitchell <remitche@jlab.org>
2005-10-31 22:42:33 +0000

    Starting to find points in the TOF based on projections of tracks (temporarily using truth information in place of tracking information).

--- libraries/TOF/DFactory_DTOFPoint.cc
+++ libraries/TOF/DFactory_DTOFPoint.cc
@@ -7,8 +7,14 @@
 
 #include <cassert>	
 
+#include "DFactory_DHDDMTOFTruth.h"
 #include "DFactory_DTOFPoint.h"
 #include "DFactory_DTOFHit.h"
+
+#define MAXTOFHITS 50
+#define VELOCITY 15.0
+#define LONGBARLENGTH 258.0
+#define BARWIDTH 6.0
 
 //------------------
 // evnt
@@ -21,26 +27,47 @@ derror_t DFactory_DTOFPoint::evnt(DEventLoop *loop, int eventnumber)
   vector<const DTOFHit*> hits;
   eventLoop->Get(hits);
 
+  vector<const DHDDMTOFTruth*> tracks;
+  eventLoop->Get(tracks);
+
+  unsigned int usedlist[MAXTOFHITS*4];
   for (unsigned int i = 0; i < hits.size(); i++){
+    usedlist[i] = 0;
+  }
 
-    const DTOFHit *hit = hits[i];
-    DTOFPoint *point = new DTOFPoint;
-
-    point->x         = 0.0;
-    point->y         = hit->y;
-    point->z         = 0.0;
-    point->E         = hit->E;
-    point->t         = hit->t;
-    point->nhits     = 1;
-    point->hits[0]   = i;
-    point->chisq     = 0.0;
-
-    _data.push_back(point);
-
+  for (unsigned int i = 0; i < tracks.size(); i++){
+    float x = tracks[i]->x;
+    float y = tracks[i]->y;
+    float z = tracks[i]->z;
+    unsigned int trackid = i;
+    unsigned int nhits = 0;
+    unsigned int hitlist[16];
+    for (unsigned int j = 0; j < hits.size(); j++){
+      float y0 = hits[j]->y;
+      if (((hits[j]->orientation == 0)&&(fabs(x-y0)<=1.0*BARWIDTH))||
+          ((hits[j]->orientation == 1)&&(fabs(y-y0)<=1.0*BARWIDTH))){
+        hitlist[nhits] = j;
+        nhits++;
+      }
+    }
+    if (nhits > 0){
+      DTOFPoint *point = new DTOFPoint;
+      point->x = x;
+      point->y = y;
+      point->z = z;
+      point->nhits = nhits;
+      for (unsigned int j = 0; j < nhits; j++){point->hits[j] = hitlist[j];}
+      point->dedx = 0.0;
+      point->chisq = 0.0;
+      point->t = 0.0;
+      point->trackid = trackid;
+      _data.push_back(point);
+    }
   }
 
   return NOERROR;
 }
+
 
 //------------------
 // toString
