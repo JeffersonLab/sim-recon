6ecad5e9c8c24a43fedeabb0b77f29a8c4adbb2c
Ryan Mitchell <remitche@jlab.org>
2005-10-31 22:40:13 +0000

    Added the "trackid" field to link a TOF point with a track.

--- libraries/include/DTOFPoint.h
+++ libraries/include/DTOFPoint.h
@@ -15,12 +15,13 @@ class DTOFPoint:public DObject{
     public:
         HDCLASSDEF(DTOFPoint);
 
-        float x, y, z;       //reconstructed position
-        float t;             //reconstructed time
-        float E;             //reconstructed energy deposition
-        unsigned int nhits;  //number of hits in this point
-        int hits[8];         //indices to DTOFHit (temporary form)
-        float chisq;         //chisquare of this point
+        unsigned int trackid;  //index to DHDDMTOFTruth (temporary)
+        float x, y, z;         //reconstructed position
+        float t;               //reconstructed time
+        float dedx;            //reconstructed dedx
+        unsigned int nhits;    //number of hits in this point
+        unsigned int hits[16]; //indices to DTOFHit (temporary form)
+        float chisq;           //chisquare of this point
 
 };
 
