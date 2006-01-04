b38613279f27d793b8ab637010c23146347df60f
David Lawrence <davidl@jlab.org>
2006-01-04 21:28:38 +0000

    Commented out event method declaration since it is not actually used

--- libraries/include/DFactory_DCDCHit.h
+++ libraries/include/DFactory_DCDCHit.h
@@ -22,7 +22,7 @@ class DFactory_DCDCHit:public DFactory<DCDCHit>{
 	protected:
 		//derror_t init(void);						///< Called once at program start.
 		//derror_t brun(DEventLoop *eventLoop, int runnumber);	///< Called everytime a new run number is detected.
-		derror_t evnt(DEventLoop *eventLoop, int eventnumber);	///< Called every event.
+		//derror_t evnt(DEventLoop *eventLoop, int eventnumber);	///< Called every event.
 		//derror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
 		//derror_t fini(void);						///< Called after last event of last event source has been processed.
 };
