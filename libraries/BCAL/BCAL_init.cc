c8c5da2e0897b1ace4ce47868f56770cc0ec5e52
Chuncheng Xu <xu@jlab.org>
2006-02-09 19:05:59 +0000

    added new factories

--- libraries/BCAL/BCAL_init.cc
+++ libraries/BCAL/BCAL_init.cc
@@ -2,11 +2,19 @@
 
 #include "DEventLoop.h"
 #include "DFactory_DBCALHit.h"
+#include "DFactory_DHDDMBCALHit.h"
+#include "DFactory_DBCALMCResponse.h"
+#include "DFactory_DBCALGeometry.h"
+#include "DFactory_DBCALShower.h"
 
 derror_t BCAL_init(DEventLoop *loop)
 {
 	/// Create and register BCAL data factories
 	loop->AddFactory(new DFactory_DBCALHit());
+	loop->AddFactory(new DFactory_DHDDMBCALHit());
+	loop->AddFactory(new DFactory_DBCALMCResponse());
+	loop->AddFactory(new DFactory_DBCALGeometry());
+	loop->AddFactory(new DFactory_DBCALShower());
 
 	return NOERROR;
 }
