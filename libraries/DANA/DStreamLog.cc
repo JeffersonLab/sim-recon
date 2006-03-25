58de563e8ac2354f9d814d833905f8a68a777eb1
Craig Bookwalter <craigb@jlab.org>
2006-03-25 20:38:43 +0000

    added multi-line entries

--- libraries/DANA/DStreamLog.cc
+++ libraries/DANA/DStreamLog.cc
@@ -12,6 +12,10 @@ DStreamLog::DStreamLog(const std::ostream& os, const std::string& tag) :
 std::ostream(new DStreamLogBuffer(os.rdbuf(), tag.c_str()))
 {}
 
+DStreamLog& DStreamLog::endMsg(DStreamLog& dSL) {
+	dSL << static_cast<char>(6); 
+	return dSL;
+}
 
 DStreamLog::~DStreamLog() {
 	delete rdbuf();
