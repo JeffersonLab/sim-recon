b2537f3e06a0741003bfa00bbe864dffb5c8ef4b
Craig Bookwalter <craigb@jlab.org>
2006-04-07 14:52:29 +0000

    Changed "globalLayer" and "globalPlane" of DFDCHit to "gLayer" and "gPlane"

--- libraries/include/DFDCGeometry.h
+++ libraries/include/DFDCGeometry.h
@@ -12,22 +12,22 @@
 
 class DFDCGeometry : public DObject {
 	public:
-		inline int globalLayer(const DFDCHit* h) {
+		inline int gLayer(const DFDCHit* h) {
 			return 3*(h->module - 1) + (h->layer - 1) + 1;
 		}		
 		
-		inline int globalPlane(const DFDCHit* h) {
+		inline int gPlane(const DFDCHit* h) {
 			return 9*(h->module-1) + 3*(h->layer-1) + (h->plane-1) + 1;
 		}
 				
 		float getLayerZ(const DFDCHit* h) {
-			if (h->globalPlane <= 18)  
-				return 227.5 + h->globalPlane*0.5;			// Thing 1
-			if (h->globalPlane <= 36)	   
-				return 283.5 + (h->globalPlane - 18)*0.5;	// Thing 2
-			if (h->globalPlane <= 54)	   
-				return 358.5 + (h->globalPlane - 36)*0.5;	// Thing 3
-			return 394.5 + (h->globalPlane - 540*0.5); 		// Thing 4
+			if (h->gPlane <= 18)  
+				return 227.5 + h->gPlane*0.5;			// Thing 1
+			if (h->gPlane <= 36)	   
+				return 283.5 + (h->gPlane - 18)*0.5;	// Thing 2
+			if (h->gPlane <= 54)	   
+				return 358.5 + (h->gPlane - 36)*0.5;	// Thing 3
+			return 394.5 + (h->gPlane - 540*0.5); 		// Thing 4
 		}
 				
 		inline float getWireR(const DFDCHit* h) {
--- libraries/include/DFDCHit.h
+++ libraries/include/DFDCHit.h
@@ -17,14 +17,20 @@ class DFDCHit : public DObject{
 		int layer;				// 1, 2, or 3
 		int module;				// 1 through 8
 		int element;			// wire or strip number
-	    int plane;				// for cathodes only: u or v plane
-	    int globalPlane;		// 1 through 72
-	    int globalLayer;		// 1 through 24
+	    int plane;				// for cathodes only: u(3) or v(1) plane, u@-45,v@+45  
+	    int gPlane;				// 1 through 72
+	    int gLayer;				// 1 through 24
 	    float dE;				// charge deposited
 	    float t;				// drift time
 	    float r;				// perpendicular distance from center of chamber to wire/strip center
 	    int type;				// cathode=1, anode=0
+	    
+//	    friend ostream& operator<<(ostream& os, const DFDCHit* h);
+//	    friend ostream& operator<<(ostream& os, const DFDCHit& h);
+	    
+	    	
 };
+
 
 #endif // _DFDCHit_
 
