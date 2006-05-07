6396ba713c296a79c924065837d8a15ec2a207f8
Craig Bookwalter <craigb@jlab.org>
2006-05-07 01:06:21 +0000

    Added better comments.

--- libraries/include/DFactory_DFDCTruth.h
+++ libraries/include/DFactory_DFDCTruth.h
@@ -1,3 +1,10 @@
+//***************************************************************************** 
+// DFactory_DFDCTruth.h: DFactory for extracting truth points for the FDC from
+// the HDDM file
+// Author: Craig Bookwalter (craigb at jlab.org)
+// Date: Mar 2006
+//*****************************************************************************
+
 #ifndef DFACTORY_DFDCTRUTH_H
 #define DFACTORY_DFDCTRUTH_H
 
@@ -6,19 +13,49 @@
 #include "DFDCTruth.h"
 #include "DException.h"
 
+#include <sstream>
+
+///
+/// class DFactory_DFDCTruth: definition for a DFactory that
+/// extracts FDC truth points from an HDDM data file.
+///
 class DFactory_DFDCTruth : public DFactory<DFDCTruth>{
 	public:
+		///
+		/// DFactory_DFDCTruth::DFactory_DFDCTruth():
+		/// default constructor -- empty for now
+		///
 		DFactory_DFDCTruth();
+
+		///
+		/// DFactory_DFDCTruth::~DFactory_DFDCTruth():
+		/// default destructor -- empty for now as well
+		///
 		~DFactory_DFDCTruth();
+
+		///
+		/// DFactory_DFDCTruth::Extract_HDDM():
+		/// reads an event from a data file and converts FDC truth information into 
+		/// DFDCTruth objects. For more detail on the s_Blah_t structures, see
+		/// the documentation for hddm_s.h.
+		///
 		derror_t Extract_HDDM(s_HDDM_t *hddm_s, vector<void*> &v);
+
+		///
+		/// DFactory_DFDCTruth::toString():
+		/// Print a sensible std::string representation of the contents of this factory.
+		///
 		const string toString(void);
 	
 	protected:
-		//derror_t init(void);						///< Called once at program start.
-		//derror_t brun(DEventLoop *eventLoop, int runnumber);	///< Called everytime a new run number is detected.
-		derror_t evnt(DEventLoop *eventLoop, int eventnumber);	///< Called every event.
-		//derror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
-		//derror_t fini(void);						///< Called after last event of last event source has been processed.
+		///
+		/// DFactory_DFDCTruth::evnt():
+		/// this function would be defined if this factory was processing data
+		/// already in memory; since this factory reads an HDDM file and puts
+		/// truth data into memory, the DFactory_DFDCTruth::Extract_HDDM() method
+		/// is used.
+		///
+		derror_t evnt(DEventLoop *eventLoop, int eventnumber);
 };
 
 #endif // DFACTORY_DFDCTRUTH_H
