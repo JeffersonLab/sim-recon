ad3de05cb1e7bb0a86a9998611334cb00d6cc3b3
name unknown <no_author@no_node>
2006-11-06 19:15:54 +0000

    Add MyProcessor to Run call

--- programs/Analysis/hd_root/hd_root.cc
+++ programs/Analysis/hd_root/hd_root.cc
@@ -24,21 +24,9 @@ int main(int narg, char *argv[])
 
 	// Instantiate an event loop object
 	DApplication app(narg, argv);
-	
-	// Get the list of shared objects (if any) and try setting their
-	// TFile pointer address globals so that they use the same
-	// ROOT file for output.
-	vector<void*> sohandles = app.GetSharedObjectHandles();
-	for(unsigned int i=0; i<sohandles.size(); i++){
-		SetTFilePtrAddress_t *SetTFilePtrAddress = (SetTFilePtrAddress_t*)dlsym(sohandles[i], "SetTFilePtrAddress");
-		if(SetTFilePtrAddress){
-			(*SetTFilePtrAddress)(&tfilePtr);
-		}
-	}
-	tfilePtr = NULL;
 
 	// Run though all events, calling our event processor's methods
-	app.Run(NULL);
+	app.Run(&myproc);
 	
 	return 0;
 }
