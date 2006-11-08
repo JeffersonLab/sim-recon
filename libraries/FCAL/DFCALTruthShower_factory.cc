468a31f260a76d76292be23db5c97735c97f6b9e
David Lawrence <davidl@jlab.org>
2006-11-08 04:17:33 +0000

    Filled in toString method. Was just a placeholder before.

--- libraries/FCAL/DFCALTruthShower_factory.cc
+++ libraries/FCAL/DFCALTruthShower_factory.cc
@@ -18,22 +18,22 @@ const string DFCALTruthShower_factory::toString(void)
 	Get();
 	if(_data.size()<=0)return string(); // don't print anything if we have no data!
 
-	// Put the class specific code to produce nicely formatted ASCII here.
-	// The JFactory_base class has several methods defined to help. They
-	// rely on positions of colons (:) in the header. Here's an example:
-	//
-	//		printheader("row:    x:     y:");
-	//
-	// 	for(int i=0; i<_data.size(); i++){
-	//			DFCALTruthShower *myDFCALTruthShower = _data[i];
-	//
-	//			printnewrow();
-	//			printcol("%d",	i);
-	//			printcol("%1.3f",	myDFCALTruthShower->x);
-	//			printcol("%3.2f",	myDFCALTruthShower->y);
-	//			printrow();
-	//		}
-	//
-	return _table;
+	printheader("id:      x:      y:      z:   E(MeV): t(ns): primary: track:");
 
+	for(unsigned int i=0; i<_data.size(); i++){
+		DFCALTruthShower *shower = _data[i];
+
+		printnewrow();
+		printcol("%d",	shower->id);
+		printcol("%3.1f",	shower->x());
+		printcol("%3.1f",	shower->y());
+		printcol("%3.1f",	shower->z());
+		printcol("%3.3f",	shower->E()*1000.0);
+		printcol("%3.1f",	shower->t());
+		printcol("%d",	shower->primary());
+		printcol("%d",	shower->track());
+		printrow();
+	}
+
+	return _table;
 }
