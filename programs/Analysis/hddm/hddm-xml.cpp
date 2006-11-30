975734ade5a8c44b5ee88f9b705a75364f29795f
Richard Jones <jonesrt@jlab.org>
2006-11-30 01:08:42 +0000

    hddm-xml.cpp [rtj]    - putting back David's changes to type casting, probably need to keep.

--- programs/Analysis/hddm/hddm-xml.cpp
+++ programs/Analysis/hddm/hddm-xml.cpp
@@ -57,6 +57,7 @@
 
 #include "particleType.h"
 
+
 #define X(str) XString(str).unicode_str()
 #define S(str) str.c_str()
 
@@ -351,9 +352,9 @@ void XMLmaker::constructXML(xstream::xdr::istream& ifx,
          }
          else if (typeS == "Particle_t")
          {
-            Particle_t value;
-            ifx >> (int)value;
-            attrStr << " " << nameS << "=\"" << ParticleType(value) << "\"";
+            int32_t value;
+            ifx >> value;
+            attrStr << " " << nameS << "=\"" << ParticleType((Particle_t)value) << "\"";
          }
          else if (typeS == "string" || typeS == "anyURI")
          {
