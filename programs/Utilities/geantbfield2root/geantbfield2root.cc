f16ead28e446ae35e7b0cd6b35f5f44dd6c9a7da
name unknown <no_author@no_node>
2006-12-08 20:37:36 +0000

    Modified to check against lasted hdds-generated magnetic field. Added tree of field from DMagneticFieldMap evaluated at same points as gufld for more direct comparison. -DL

--- programs/Utilities/geantbfield2root/geantbfield2root.cc
+++ programs/Utilities/geantbfield2root/geantbfield2root.cc
@@ -31,9 +31,16 @@ int main(int narg, char *argv[])
 	ROOTfile->SetCompressionLevel(6);
 	cout<<"Opened ROOT file \"bfield.root\""<<endl;
 
-	// Create Tree
-	TTree *tree = new TTree("Bfield","Magnetic Field");
-	tree->Branch("B",val,"x/F:y:z:r:phi:Bx:By:Bz:Br:Bphi");
+	// Create Magnetic Field Map
+	new JParameterManager(); // normally, this is created by JApplication!
+	DMagneticFieldMap *bfield = new DMagneticFieldMap();
+
+	// Create Trees
+	TTree *gtree = new TTree("gfield","HDGeant Magnetic Field");
+	gtree->Branch("B",val,"x/F:y:z:r:phi:Bx:By:Bz:Br:Bphi");
+
+	TTree *btree = new TTree("bfield","DANA Magnetic Field");
+	btree->Branch("B",val,"x/F:y:z:r:phi:Bx:By:Bz:Br:Bphi");
 	
 	// Space grid
 	for(float r=0.0; r<65.0; r+=2.0){
@@ -45,6 +52,11 @@ int main(int narg, char *argv[])
 				u[2] = z;
 				gufld_(u,B);
 				//B[0]=B[1]=B[2] = 0.0;
+				
+				// Geant uses kGauss. Convert to Tesla
+				B[0]/=10.0;
+				B[1]/=10.0;
+				B[2]/=10.0;
 			
 				double Br = (B[0]*u[0] + B[1]*u[1])/(r+1.0E-20);
 				double Bphi = (B[1]*u[0] - B[0]*u[1])/(r+1.0E-20);
@@ -60,7 +72,30 @@ int main(int narg, char *argv[])
 				val[8] = Br;
 				val[9] = Bphi;
 
-				tree->Fill();
+				gtree->Fill();
+				
+				// Access DANA field map at the same point
+				double x,y,z,Bx,By,Bz;
+				bfield->GetBilinear(x=val[0], y=val[1], z=val[2], Bx, By, Bz);
+				B[0] = Bx;
+				B[1] = By;
+				B[2] = Bz;
+
+				Br = (B[0]*u[0] + B[1]*u[1])/(r+1.0E-20);
+				Bphi = (B[1]*u[0] - B[0]*u[1])/(r+1.0E-20);
+
+				val[0] = u[0];
+				val[1] = u[1];
+				val[2] = u[2];
+				val[3] = r;
+				val[4] = phi;
+				val[5] = B[0];
+				val[6] = B[1];
+				val[7] = B[2];
+				val[8] = Br;
+				val[9] = Bphi;
+
+				btree->Fill();
 			}
 		}
 	}
