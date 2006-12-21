9c7d303a03693cfdcfae4891d4d3b4735cb8a3ee
David Lawrence <davidl@jlab.org>
2006-12-21 15:39:12 +0000

    Last change used wrong values for z-position of FDC packages (I was looking at the wrong numbers in HDDS oops!). This is now fixed and should be current with what is in HDDS.

--- libraries/FDC/DFDCGeometry.cc
+++ libraries/FDC/DFDCGeometry.cc
@@ -34,33 +34,33 @@ DFDCGeometry::DFDCGeometry(void)
 		float angle=0.0;
 		float z_anode=212.0+95.5;
 		switch(layer){
-			case  1: z_anode+= -95.6-2.0;	angle=  degrees00; break;
-			case  2: z_anode+= -95.6+0.0;	angle= +degrees60; break;
-			case  3: z_anode+= -95.6+2.0;	angle= -degrees60; break;
-			case  4: z_anode+= -83.4-2.0;	angle=  degrees00; break;
-			case  5: z_anode+= -83.4+0.0;	angle= +degrees60; break;
-			case  6: z_anode+= -83.4+2.0;	angle= -degrees60; break;
+			case  1: z_anode+= -92.5-2.0;	angle=  degrees00; break;
+			case  2: z_anode+= -92.5+0.0;	angle= +degrees60; break;
+			case  3: z_anode+= -92.5+2.0;	angle= -degrees60; break;
+			case  4: z_anode+= -86.5-2.0;	angle=  degrees00; break;
+			case  5: z_anode+= -86.5+0.0;	angle= +degrees60; break;
+			case  6: z_anode+= -86.5+2.0;	angle= -degrees60; break;
 
-			case  7: z_anode+= -35.6-2.0;	angle=  degrees00; break;
-			case  8: z_anode+= -35.6+0.0;	angle= +degrees60; break;
-			case  9: z_anode+= -35.6+2.0;	angle= -degrees60; break;
-			case 10: z_anode+= -23.4-2.0;	angle=  degrees00; break;
-			case 11: z_anode+= -23.4+0.0;	angle= +degrees60; break;
-			case 12: z_anode+= -23.4+2.0;	angle= -degrees60; break;
+			case  7: z_anode+= -32.5-2.0;	angle=  degrees00; break;
+			case  8: z_anode+= -32.5+0.0;	angle= +degrees60; break;
+			case  9: z_anode+= -32.5+2.0;	angle= -degrees60; break;
+			case 10: z_anode+= -26.5-2.0;	angle=  degrees00; break;
+			case 11: z_anode+= -26.5+0.0;	angle= +degrees60; break;
+			case 12: z_anode+= -26.5+2.0;	angle= -degrees60; break;
 
-			case 13: z_anode+= +23.4-2.0;	angle=  degrees00; break;
-			case 14: z_anode+= +23.4+0.0;	angle= +degrees60; break;
-			case 15: z_anode+= +23.4+2.0;	angle= -degrees60; break;
-			case 16: z_anode+= +35.6-2.0;	angle=  degrees00; break;
-			case 17: z_anode+= +35.6+0.0;	angle= +degrees60; break;
-			case 18: z_anode+= +35.6+2.0;	angle= -degrees60; break;
+			case 13: z_anode+= +26.5-2.0;	angle=  degrees00; break;
+			case 14: z_anode+= +26.5+0.0;	angle= +degrees60; break;
+			case 15: z_anode+= +26.5+2.0;	angle= -degrees60; break;
+			case 16: z_anode+= +32.5-2.0;	angle=  degrees00; break;
+			case 17: z_anode+= +32.5+0.0;	angle= +degrees60; break;
+			case 18: z_anode+= +32.5+2.0;	angle= -degrees60; break;
 
-			case 19: z_anode+= +83.4-2.0;	angle=  degrees00; break;
-			case 20: z_anode+= +83.4+0.0;	angle= +degrees60; break;
-			case 21: z_anode+= +83.4+2.0;	angle= -degrees60; break;
-			case 22: z_anode+= +95.6-2.0;	angle=  degrees00; break;
-			case 23: z_anode+= +95.6+0.0;	angle= +degrees60; break;
-			case 24: z_anode+= +95.6+2.0;	angle= -degrees60; break;
+			case 19: z_anode+= +86.5-2.0;	angle=  degrees00; break;
+			case 20: z_anode+= +86.5+0.0;	angle= +degrees60; break;
+			case 21: z_anode+= +86.5+2.0;	angle= -degrees60; break;
+			case 22: z_anode+= +92.5-2.0;	angle=  degrees00; break;
+			case 23: z_anode+= +92.5+0.0;	angle= +degrees60; break;
+			case 24: z_anode+= +92.5+2.0;	angle= -degrees60; break;
 		}
 		
 		// Somewhere between HDDS and here I'm missing a sign. I'm not
