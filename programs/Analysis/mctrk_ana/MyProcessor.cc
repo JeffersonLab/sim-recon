632de21065578f7efaf777abfcc2c3fd7c6f1ebd
David Lawrence <davidl@jlab.org>
2007-02-06 04:17:14 +0000

    Added chi-sq to fitp tree.

--- programs/Analysis/mctrk_ana/MyProcessor.cc
+++ programs/Analysis/mctrk_ana/MyProcessor.cc
@@ -46,7 +46,7 @@ jerror_t MyProcessor::init(void)
 	
 	// Create Tree
 	fit_parms = new TTree("fitp","Helical Fit parameters");
-	fit_parms->Branch("F",val,"p/F:px:py:pz:x:y:z:pcan:pcanx:pcany:pcanz:canz:Ro:x0:y0:phi:theta:p_thrn:px_thrn:py_thrn:pz_thrn:phi_thrn:theta_thrn:fittable:nhits");
+	fit_parms->Branch("F",val,"p/F:px:py:pz:x:y:z:pcan:pcanx:pcany:pcanz:canz:Ro:x0:y0:phi:theta:p_thrn:px_thrn:py_thrn:pz_thrn:phi_thrn:theta_thrn:fittable:nhits:chisq");
 
 	gPARMS->GetParameter("TRKFIT:CANDIDATE_TAG", CANDIDATE_TAG);
 
@@ -121,6 +121,7 @@ jerror_t MyProcessor::evnt(JEventLoop *loop, int eventnumber)
 		val[22] = thrown->theta;
 		val[23] = (float)trackeff->fittable;
 		val[24] = (float)trackeff->Nhits_thrown;
+		val[25] = track->chisq;
 		
 		fit_parms->Fill();
 		UnlockState();
