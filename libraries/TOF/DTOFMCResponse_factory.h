992ae0299e2ead54e37cc54308b8932c114c2597
Benedikt Zihlmann <zihlmann@jlab.org>
2007-08-02 21:32:48 +0000

    set also here the calibration parameter in the class definition to private

--- libraries/TOF/DTOFMCResponse_factory.h
+++ libraries/TOF/DTOFMCResponse_factory.h
@@ -14,31 +14,31 @@
 
 
 class DTOFMCResponse_factory:public JFactory<DTOFMCResponse>{
-	public:
-		DTOFMCResponse_factory(){};
-		~DTOFMCResponse_factory(){};
-		const string toString(void);
-
-		double ATTEN_LENGTH  ;   // attenuation length in paddle
-		double C_EFFECTIVE   ;   // effective light speed in paddle
-		double TWO_HIT_RESOL ;   // two hit timing resolution in paddle
-		double THRESH_MEV    ;   // energy threshold in paddle
-		double PHOTONS_PERMEV;   // scintillation photon generated per MeV energy deposition 
-		double THETA_MAX     ;   // total internal reflection
-		double PMT_SURFACE   ;   // PMT surface in cm^2
-		double REFLECT_EFF   ;   // reflection efficiency of light 
-		double PHE_EFF       ;   // efficiency to create photo elecrons
-		double PMT_GAIN      ;   // PMT gain factor 4*10^7
-		double ECHARGE       ;   // electric charge in C
-		double ADC_RES       ;   // adc resolution pC/count
-		double TOF_CENT_TRES ;   // time resolution of tof paddel for hit in the center log10(0.2ns)
-		double TDC_RES       ;   // TDC resolution in ns
-		double HALFPADDLE    ;   // half length of paddle
-
-	private:
-
-		jerror_t brun(JEventLoop *loop, int runnumber);
-		jerror_t evnt(JEventLoop *loop, int eventnumber);	///< Invoked via JEventProcessor virtual method
+ public:
+  DTOFMCResponse_factory(){};
+  ~DTOFMCResponse_factory(){};
+  const string toString(void);
+ private:
+  double ATTEN_LENGTH  ;   // attenuation length in paddle
+  double C_EFFECTIVE   ;   // effective light speed in paddle
+  double TWO_HIT_RESOL ;   // two hit timing resolution in paddle
+  double THRESH_MEV    ;   // energy threshold in paddle
+  double PHOTONS_PERMEV;   // scintillation photon generated per MeV energy deposition 
+  double THETA_MAX     ;   // total internal reflection
+  double PMT_SURFACE   ;   // PMT surface in cm^2
+  double REFLECT_EFF   ;   // reflection efficiency of light 
+  double PHE_EFF       ;   // efficiency to create photo elecrons
+  double PMT_GAIN      ;   // PMT gain factor 4*10^7
+  double ECHARGE       ;   // electric charge in C
+  double ADC_RES       ;   // adc resolution pC/count
+  double TOF_CENT_TRES ;   // time resolution of tof paddel for hit in the center log10(0.2ns)
+  double TDC_RES       ;   // TDC resolution in ns
+  double HALFPADDLE    ;   // half length of paddle
+  
+ private:
+  
+  jerror_t brun(JEventLoop *loop, int runnumber);
+  jerror_t evnt(JEventLoop *loop, int eventnumber);	///< Invoked via JEventProcessor virtual method
 };
 
 #endif // _DTOFMCResponse_factory_
