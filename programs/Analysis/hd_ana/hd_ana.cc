9702e4153b357811fd2007695a1aaa99e65729a9
David Lawrence <davidl@jlab.org>
2007-11-11 05:07:57 +0000

    Added usage statement to hd_ana.

--- programs/Analysis/hd_ana/hd_ana.cc
+++ programs/Analysis/hd_ana/hd_ana.cc
@@ -6,6 +6,9 @@
 
 #include "DANA/DApplication.h"
 
+void Usage(JApplication &app);
+
+
 //-----------
 // main
 //-----------
@@ -14,9 +17,26 @@ int main(int narg, char *argv[])
 	// Instantiate an event loop object
 	DApplication app(narg, argv);
 
+	if(narg<=1)Usage(app);
+
 	// Run though all events, calling our event processor's methods
 	app.Run(NULL, 1);
 	
 	return 0;
+}
+
+//-----------
+// Usage
+//-----------
+void Usage(JApplication &app)
+{
+	cout<<endl;
+	cout<<"Usage:"<<endl;
+	cout<<"    hd_ana [options] source1 source2 source3 ..."<<endl;
+	cout<<endl;
+	app.Usage();
+	cout<<endl;
+	
+	exit(0);
 }
 
