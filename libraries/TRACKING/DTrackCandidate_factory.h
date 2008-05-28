ae4d8c901f92dd64e76c277b4cebf7648c77ad0e
Simon Taylor <staylor@jlab.org>
2008-05-28 23:38:04 +0000

    First attempt at candidate matching between the CDC and FDC based on swimming both sets to the CDC endplate.  To first order this code only cuts down the number of candidates fed to the final fitter, but it does try to use CDC and FDC hits for FDC candidates that project to a radius less than 28 cm at the endplate, where the inclusion of even a handful of CDC axial layers seems to improve the quality of the candidate on average.

--- libraries/TRACKING/DTrackCandidate_factory.h
+++ libraries/TRACKING/DTrackCandidate_factory.h
@@ -12,19 +12,29 @@
 #include <JANA/JFactory.h>
 #include <JANA/JEventLoop.h>
 using namespace jana;
-
+#include <TH2F.h>
 #include "DTrackCandidate.h"
 
 class DMagneticFieldMap;
 
 class DTrackCandidate_factory:public JFactory<DTrackCandidate>{
-	public:
-		DTrackCandidate_factory(){};
-		~DTrackCandidate_factory(){};
+ public:
+  DTrackCandidate_factory(){DEBUG_HISTS=true;};
+  ~DTrackCandidate_factory(){};
 
+  
+ protected:
+  virtual jerror_t evnt(JEventLoop *loop, int eventnumber);	///< Invoked via JEventProcessor virtual method
+  virtual jerror_t brun(JEventLoop* eventLoop,int runnumber);
 
-	protected:
-		virtual jerror_t evnt(JEventLoop *loop, int eventnumber);	///< Invoked via JEventProcessor virtual method
+ private:
+  const DMagneticFieldMap *bfield;
+
+  bool DEBUG_HISTS;
+  TH2F *match_dist,*match_dist_vs_p;
+
+  DVector3 cdc_endplate;
+  double endplate_rmax;
 };
 
 #endif // _DTrackCandidate_factory_
