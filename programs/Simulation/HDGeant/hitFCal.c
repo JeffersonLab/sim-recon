dc3eab537c0c6d2d625f4d99557b2169f23806ac
Benedikt Zihlmann <zihlmann@jlab.org>
2008-08-05 15:47:34 +0000

    initialize the momentum, time and particle type of the primary particle hitting the FCAL in the FcalTruthShower structure

--- programs/Simulation/HDGeant/hitFCal.c
+++ programs/Simulation/HDGeant/hitFCal.c
@@ -71,6 +71,14 @@ void hitForwardEMcal (float xin[4], float xout[4],
          showers->in[0].y = xin[1];
          showers->in[0].z = xin[2];
          showers->in[0].E = pin[3];
+
+         showers->in[0].px = pin[0];
+         showers->in[0].py = pin[1];
+         showers->in[0].pz = pin[2];
+
+	 showers->in[0].t = t;
+	 showers->in[0].ptype = ipart;
+
          showers->mult = 1;
          showerCount++;
       }
