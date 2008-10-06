391ef2b58d277681341afd01eddeb769e040a34c
David Lawrence <davidl@jlab.org>
2008-10-06 13:28:46 +0000

    Added THROWN version of DParticle factory.

--- libraries/PID/PID_init.cc
+++ libraries/PID/PID_init.cc
@@ -9,6 +9,7 @@ using namespace jana;
 #include "DTwoGammaFit_factory_PI0.h"
 #include "DTwoGammaFit_factory_ETA.h"
 #include "DParticle_factory.h"
+#include "DParticle_factory_THROWN.h"
 
 #include "DBeamPhoton.h"
 typedef JFactory<DBeamPhoton> DBeamPhoton_factory;
@@ -25,6 +26,7 @@ jerror_t PID_init(JEventLoop *loop)
 	loop->AddFactory(new DTwoGammaFit_factory_ETA);
 	loop->AddFactory(new DBeamPhoton_factory);
 	loop->AddFactory(new DParticle_factory);
+	loop->AddFactory(new DParticle_factory_THROWN);
 
 	return NOERROR;
 }
