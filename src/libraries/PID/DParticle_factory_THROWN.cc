28e55c4be88d0bbc7b954d7fee474afb9ab1afec
Mark Ito <marki@jlab.org>
2008-10-23 19:40:58 +0000

    Merge in David's change to r4393 on trunk

--- src/libraries/PID/DParticle_factory_THROWN.cc
+++ src/libraries/PID/DParticle_factory_THROWN.cc
@@ -267,7 +267,7 @@ void DParticle_factory_THROWN::AddFDCPseudoHits(DReferenceTrajectory *rt, vector
 		// based on both the anode and cathode hits.
 		probability *= TMath::Prob(resic/sigma_cathode, 1);
 
-		if(probability<=MIN_HIT_PROB)fdchits.push_back(hit);
+		if(probability>=MIN_HIT_PROB)fdchits.push_back(hit);
 
 		if(debug_level>10)_DBG_<<"s="<<s<<" doca="<<doca<<" dist="<<dist<<" resi="<<resi<<" tof="<<tof<<" prob="<<probability<<endl;
 	}
