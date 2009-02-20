35b9aff94319663581370af2718dc55127cb05b3
David Lawrence <davidl@jlab.org>
2009-02-20 18:30:32 +0000

    Use separate resolution/efficiency maps for protons and pions

--- programs/Simulation/plugins/hdparsim/DTrackingResolutionGEANT.cc
+++ programs/Simulation/plugins/hdparsim/DTrackingResolutionGEANT.cc
@@ -23,27 +23,40 @@ DTrackingResolutionGEANT::DTrackingResolutionGEANT()
 	//int argc=0;
 	//TApplication *app = new TApplication("myapp", &argc, NULL);
 
-	// Get ROOT file from web if it is not already here
-	const char *url = "http://www.jlab.org/Hall-D/datatables/hd_res_charged.root";
-	getwebfile(url);
 	
 	TDirectory *savedir = gDirectory;
+	
+	ReadTableInfo("hd_res_charged_pion.root", pion_info);
+	ReadTableInfo("hd_res_charged_proton.root", proton_info);
 
-	//---------------- hd_res_charged ------------------
+	if(savedir)savedir->cd();
+
+}
+
+//----------------
+// ReadTableInfo
+//----------------
+void DTrackingResolutionGEANT::ReadTableInfo(const char *fname, TableInfo &ti)
+{
+	// Get ROOT file from web if it is not already here
+	char url[512] = "http://www.jlab.org/Hall-D/datatables/";
+	strcat(url, fname);
+	getwebfile(url);
+
 	// Open ROOT file
-	file = new TFile("hd_res_charged.root");
-	if(!file->IsOpen()){
+	ti.file = new TFile(fname);
+	if(!ti.file->IsOpen()){
 		cout<<endl;
 		cout<<"Couldn't open resolution file \"hd_res_charged.root\"!"<<endl;
 		cout<<"Make sure it exists in the current directory and is readable,"<<endl;
 		cout<<endl;
 		exit(0);
 	}
-	cout<<"Opened \""<<file->GetName()<<"\""<<endl;
+	cout<<"Opened \""<<ti.file->GetName()<<"\""<<endl;
 
 	// Read pt resolution histogram
-	file->GetObject("dpt_over_pt_vs_p_vs_theta", pt_res_hist);
-	if(!pt_res_hist){
+	ti.file->GetObject("dpt_over_pt_vs_p_vs_theta", ti.pt_res_hist);
+	if(!ti.pt_res_hist){
 		cout<<endl;
 		cout<<"Couldn't find resolution histogram \"dpt_over_pt_vs_p_vs_theta\""<<endl;
 		cout<<"in ROOT file!"<<endl;
@@ -52,8 +65,8 @@ DTrackingResolutionGEANT::DTrackingResolutionGEANT()
 	}
 
 	// Read theta resolution histogram
-	file->GetObject("dtheta_vs_p_vs_theta", theta_res_hist);
-	if(!theta_res_hist){
+	ti.file->GetObject("dtheta_vs_p_vs_theta", ti.theta_res_hist);
+	if(!ti.theta_res_hist){
 		cout<<endl;
 		cout<<"Couldn't find resolution histogram \"dtheta_vs_p_vs_theta\""<<endl;
 		cout<<"in ROOT file!"<<endl;
@@ -62,8 +75,8 @@ DTrackingResolutionGEANT::DTrackingResolutionGEANT()
 	}
 
 	// Read phi resolution histogram
-	file->GetObject("dphi_vs_p_vs_theta", phi_res_hist);
-	if(!phi_res_hist){
+	ti.file->GetObject("dphi_vs_p_vs_theta", ti.phi_res_hist);
+	if(!ti.phi_res_hist){
 		cout<<endl;
 		cout<<"Couldn't find resolution histogram \"dphi_vs_p_vs_theta\""<<endl;
 		cout<<"in ROOT file!"<<endl;
@@ -72,8 +85,8 @@ DTrackingResolutionGEANT::DTrackingResolutionGEANT()
 	}
 
 	// Read in efficiency histogram
-	file->GetObject("eff_vs_p_vs_theta", efficiency_hist);
-	if(!efficiency_hist){
+	ti.file->GetObject("eff_vs_p_vs_theta", ti.efficiency_hist);
+	if(!ti.efficiency_hist){
 		cout<<endl;
 		cout<<"Couldn't find efficiency histogram \"eff_vs_p_vs_theta\""<<endl;
 		cout<<"in ROOT file!"<<endl;
@@ -82,7 +95,6 @@ DTrackingResolutionGEANT::DTrackingResolutionGEANT()
 	}
 
 	
-	if(savedir)savedir->cd();
 }
 
 //---------------------------------
@@ -90,13 +102,46 @@ DTrackingResolutionGEANT::DTrackingResolutionGEANT()
 //---------------------------------
 DTrackingResolutionGEANT::~DTrackingResolutionGEANT()
 {
-	if(file)delete file;
+	if(pion_info.file)delete pion_info.file;
+	if(proton_info.file)delete proton_info.file;
 }
 
 //----------------
 // GetResolution
 //----------------
 void DTrackingResolutionGEANT::GetResolution(int geanttype, const TVector3 &mom, double &pt_res, double &theta_res, double &phi_res)
+{
+	switch(geanttype){
+		case 14:
+			GetResolution(proton_info, geanttype, mom, pt_res, theta_res, phi_res);
+			break;
+		case 8: // pi+
+		case 9: // pi-
+		default: // assume everything else is close to pion resolutions
+			GetResolution(pion_info, geanttype, mom, pt_res, theta_res, phi_res);
+	}
+}
+
+//----------------
+// GetEfficiency
+//----------------
+double DTrackingResolutionGEANT::GetEfficiency(int geanttype, const TVector3 &mom)
+{
+	switch(geanttype){
+		case 14:
+			return GetEfficiency(proton_info, geanttype, mom);
+			break;
+		case 8: // pi+
+		case 9: // pi-
+		default: // assume everything else is close to pion resolutions
+			return GetEfficiency(pion_info, geanttype, mom);
+	}
+}
+
+//----------------
+// GetResolution
+//----------------
+void DTrackingResolutionGEANT::GetResolution(TableInfo &ti, int geanttype, const TVector3 &mom, double &pt_res, double &theta_res, double &phi_res)
 {
 	/// Return the momentum and angular resolutions for a charged
 	/// particle based on results from GEANT-based Monte Carlo studies.
@@ -107,28 +152,28 @@ void DTrackingResolutionGEANT::GetResolution(int geanttype, const TVector3 &mom,
 	// the theta and p bins once.
 	double p = mom.Mag();
 	double theta = mom.Theta()*57.3;
-	int pbin = pt_res_hist->GetYaxis()->FindBin(p);
-	int thetabin = pt_res_hist->GetXaxis()->FindBin(theta);
+	int pbin = ti.pt_res_hist->GetYaxis()->FindBin(p);
+	int thetabin = ti.pt_res_hist->GetXaxis()->FindBin(theta);
 	
 	// For tracks with momentum out of the range of our table, use the
 	// resolutions for the largest momentum we have
-	if(pbin>pt_res_hist->GetNbinsY())pbin=pt_res_hist->GetNbinsY();
+	if(pbin>ti.pt_res_hist->GetNbinsY())pbin=ti.pt_res_hist->GetNbinsY();
 	
-	if(pbin<1 || pbin>pt_res_hist->GetNbinsY()){pt_res=theta_res=phi_res=0.0; return;}
-	if(thetabin<1 || thetabin>pt_res_hist->GetNbinsX()){pt_res=theta_res=phi_res=0.0; return;}
+	if(pbin<1 || pbin>ti.pt_res_hist->GetNbinsY()){pt_res=theta_res=phi_res=0.0; return;}
+	if(thetabin<1 || thetabin>ti.pt_res_hist->GetNbinsX()){pt_res=theta_res=phi_res=0.0; return;}
 	
 	// Here we should do an interpolation from the surrounding bins.
 	// We have fairly small bins though so I can afford to be
 	// lazy :)
-	pt_res = pt_res_hist->GetBinContent(thetabin, pbin); // return as fraction
-	theta_res = theta_res_hist->GetBinContent(thetabin, pbin); // return in milliradians
-	phi_res = phi_res_hist->GetBinContent(thetabin, pbin); // return in milliradians
+	pt_res = ti.pt_res_hist->GetBinContent(thetabin, pbin); // return as fraction
+	theta_res = ti.theta_res_hist->GetBinContent(thetabin, pbin); // return in milliradians
+	phi_res = ti.phi_res_hist->GetBinContent(thetabin, pbin); // return in milliradians
 }
 
 //----------------
 // GetEfficiency
 //----------------
-double DTrackingResolutionGEANT::GetEfficiency(int geanttype, const TVector3 &mom)
+double DTrackingResolutionGEANT::GetEfficiency(TableInfo &ti, int geanttype, const TVector3 &mom)
 {
 	/// Return the reconstruction efficiency for a charged
 	/// particle based on results from GEANT-based Monte Carlo studies.
@@ -136,20 +181,20 @@ double DTrackingResolutionGEANT::GetEfficiency(int geanttype, const TVector3 &mo
 	// Find bins for this momentum.
 	double p = mom.Mag();
 	double theta = mom.Theta()*57.3;
-	int pbin = efficiency_hist->GetYaxis()->FindBin(p);
-	int thetabin = efficiency_hist->GetXaxis()->FindBin(theta);
+	int pbin = ti.efficiency_hist->GetYaxis()->FindBin(p);
+	int thetabin = ti.efficiency_hist->GetXaxis()->FindBin(theta);
 	
 	// For tracks with momentum out of the range of our table, use the
 	// resolutions for the largest momentum we have
-	if(pbin>efficiency_hist->GetNbinsY())pbin=efficiency_hist->GetNbinsY();
+	if(pbin>ti.efficiency_hist->GetNbinsY())pbin=ti.efficiency_hist->GetNbinsY();
 	
-	if(pbin<1 || pbin>efficiency_hist->GetNbinsY())return 0.0;
-	if(thetabin<1 || thetabin>efficiency_hist->GetNbinsX())return 0.0;
+	if(pbin<1 || pbin>ti.efficiency_hist->GetNbinsY())return 0.0;
+	if(thetabin<1 || thetabin>ti.efficiency_hist->GetNbinsX())return 0.0;
 	
 	// Here we should do an interpolation from the surrounding bins.
 	// We have fairly small bins though so I can afford to be
 	// lazy :)
-	return efficiency_hist->GetBinContent(thetabin, pbin);
+	return ti.efficiency_hist->GetBinContent(thetabin, pbin);
 }
 
 
--- programs/Simulation/plugins/hdparsim/DTrackingResolutionGEANT.h
+++ programs/Simulation/plugins/hdparsim/DTrackingResolutionGEANT.h
@@ -15,21 +15,34 @@
 
 class DTrackingResolutionGEANT:public DTrackingResolution{
 	public:
+
+		class TableInfo{
+			public:
+				TFile *file;
+				TH2D* pt_res_hist;
+				TH2D* theta_res_hist;
+				TH2D* phi_res_hist;
+				TH2D* efficiency_hist;
+		};
+
 		DTrackingResolutionGEANT();
 		virtual ~DTrackingResolutionGEANT();
 		virtual const char* className(void){return static_className();}
 		static const char* static_className(void){return "DTrackingResolutionGEANT";}
 
+		void ReadTableInfo(const char *fname, TableInfo &ti);
+		
+		// Accessor methods called through virtual method of DTrackingResolution
 		void GetResolution(int geanttype, const TVector3 &mom, double &pt_res, double &theta_res, double &phi_res);
 		double GetEfficiency(int geanttype, const TVector3 &mom);
 
-	private:
-		TFile *file;
-		TH2D* pt_res_hist;
-		TH2D* theta_res_hist;
-		TH2D* phi_res_hist;
-		TH2D* efficiency_hist;
+		// Workhorse methods that actually do the work
+		void GetResolution(TableInfo &ti, int geanttype, const TVector3 &mom, double &pt_res, double &theta_res, double &phi_res);
+		double GetEfficiency(TableInfo &ti, int geanttype, const TVector3 &mom);
 
+	private:
+		TableInfo pion_info;
+		TableInfo proton_info;
 };
 
 #endif // _DTrackingResolutionGEANT_
