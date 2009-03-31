37d983086e989668429a2db15bcefc77790aea8c
Mark Ito <marki@jlab.org>
2009-03-31 19:38:34 +0000

    add drift time correction

--- programs/Analysis/plugins/trkfitterLSLM/combinedResidFunc.cc
+++ programs/Analysis/plugins/trkfitterLSLM/combinedResidFunc.cc
@@ -1,6 +1,8 @@
 #include <CLHEP/Matrix/Matrix.h>
 #include <CLHEP/Matrix/Vector.h>
 
+#define SPEED_OF_LIGHT 29.9792548
+
 using namespace std;
 
 #include "FDC/DFDCPseudo_factory.h"
@@ -188,11 +190,11 @@ void combinedResidFunc::residAndDeriv(const HepVector *x, void *data, HepVector 
 HepVector combinedResidFunc::pseudo2HepVector(const DFDCPseudo &ppoint) {
   double x;
   double y;
-  double t;
+  double ct;
   double z = ppoint.wire->origin(2);
-  trajPtr->getXYT(z, x, y, t); // on trajectory
+  trajPtr->getXYT(z, x, y, ct); // on trajectory
   double delta_x = 0.0, delta_y = 0.0;
-  getCorrectionValue(ppoint, x, y, z, t, delta_x, delta_y);
+  getCorrectionValue(ppoint, x, y, z, ct, delta_x, delta_y);
   bool ispos = getCorrectionSign(ppoint, x, y, delta_x, delta_y);
   HepVector point(3);
   if (ispos) {
@@ -231,13 +233,13 @@ bool combinedResidFunc::getCorrectionSign(const DFDCPseudo &ppoint, double x, do
   return ispos;
 }
 
-void combinedResidFunc::getCorrectionValue(const DFDCPseudo &ppoint, double x, double y, double z, double t, double &delta_x, double &delta_y) {
-  //double driftDist = (ppoint.time - t)*DRIFT_VELOCITY;
-  double driftDist = ppoint.time*DRIFT_VELOCITY;
+void combinedResidFunc::getCorrectionValue(const DFDCPseudo &ppoint, double x, double y, double z, double ct, double &delta_x, double &delta_y) {
+  double driftDist = (ppoint.time - ct/c)*DRIFT_VELOCITY;
+  //double driftDist = ppoint.time*DRIFT_VELOCITY;
   double cosangle = ppoint.wire->udir(1);
   double sinangle= ppoint.wire->udir(0);
   double alpha = 0.0; // for now
-  if (debug_level > 3) cout << "x, y, z " << x << ' ' << y << ' ' << z << endl;
+  if (debug_level > 3) cout << "x, y, z, ct = " << x << ' ' << y << ' ' << z << ' ' << ct << " time, ct/c = " << ppoint.time << " " << ct/c << " driftdist = " << driftDist << endl;
   double lorentzShift = lorentz_def->GetLorentzCorrection(x, y, z, alpha, driftDist);
   double ds = -lorentzShift;
   double dw = driftDist;
