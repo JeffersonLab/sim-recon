dfe77d3e7ec28ce002f3dab8181e8a779d1947f1
Simon Taylor <staylor@jlab.org>
2009-05-08 15:09:10 +0000

    Make wire positions and directions consistent with current (5/8/09) version of CDC geometry in CentralDC_HDDS.xml.

--- libraries/CDC/DCDCTrackHit_factory.cc
+++ libraries/CDC/DCDCTrackHit_factory.cc
@@ -45,214 +45,215 @@ jerror_t DCDCTrackHit_factory::brun(JEventLoop *loop, int runnumber)
   dgeom  = dapp->GetDGeometry(runnumber);
 
   vector<double>cdc_origin;
-  vector<double>cdc_half_length;
+  //vector<double>cdc_half_length;
+  vector<double>cdc_length;
+  vector<double>cdc_center;
   dgeom->Get("//posXYZ[@volume='CentralDC']/@X_Y_Z",cdc_origin);
-  dgeom->Get("//posXYZ[@volume='centralDC_option-1']/@X_Y_Z",cdc_half_length);
+  dgeom->Get("//tubs[@name='STRA']/@Rio_Z",cdc_length);
 
-
-	Z_MIN = cdc_origin[2];
-	Z_MAX = Z_MIN + 2.*cdc_half_length[2];
-
-	gPARMS->SetDefaultParameter("CDC:Z_MIN",Z_MIN);
-	gPARMS->SetDefaultParameter("CDC:Z_MAX",Z_MAX);
+  Z_MIN = cdc_origin[2];
+  Z_MAX = Z_MIN + cdc_length[2];
+  
+  gPARMS->SetDefaultParameter("CDC:Z_MIN",Z_MIN);
+  gPARMS->SetDefaultParameter("CDC:Z_MAX",Z_MAX);
 	
-	// We use the mutex and wire_table_initialized flag to
-	// make sure the table is initialized only once.
-	pthread_mutex_lock(&wire_mutex);
-	if(wire_table_initialized){
-		pthread_mutex_unlock(&wire_mutex);
-		return NOERROR;
-	}
-	
-	// Note: This information should ideally come from HDDS, but there
-	// is currently no trivial mechanism by which to access it from here.
+  // We use the mutex and wire_table_initialized flag to
+  // make sure the table is initialized only once.
+  pthread_mutex_lock(&wire_mutex);
+  if(wire_table_initialized){
+    pthread_mutex_unlock(&wire_mutex);
+    return NOERROR;
+  }
+  
+  // Note: This information should ideally come from HDDS, but there
+  // is currently no trivial mechanism by which to access it from here.
+  
+  // We calculate the straw coordinates knowing:
+  //
+  // 1.) The number of straws in a ring given the ring number
+  // 2.) The radius of the wire at the midplane of the specified ring
+  // 3.) The first straw is always at phi=0 in the ring.
+  //
+  // Currently, the CDC design has 28 layers corresponding to ring=1-28.
 
-	// We calculate the straw coordinates knowing:
-	//
-	// 1.) The number of straws in a ring given the ring number
-	// 2.) The radius of the wire at the midplane of the specified ring
-	// 3.) The first straw is always at phi=0 in the ring.
-	//
-	// Currently, the CDC design has 23 layers corresponding to ring=1-23.
-	// The stereo angles are as follows:
-	//
-	// layers 5,6,14,15 are +6 degrees
-	// layers 7,8,16,17 are -6 degrees
-	// all other layers are zero degrees
+  // Layers 5-8 and 17-20 are at negative stereo angles (roughly -6 degrees)
+  // Layers 9-12 and 21-24 are at positive stereo angles (roughly +6 degrees)
+  // The remaining layers have no stereo angle
+  
+  float deg2rad = M_PI/180.0;
+  float degrees6 = 6.0*deg2rad;
+  float degrees0 = 0.0;
+  double L = Z_MAX - Z_MIN; // full length of wire
+  for(int ring=1; ring<=CDC_MAX_RINGS; ring++){
+    int myNstraws=0;
+    float radius = 0.0;
+    float stereo=0.0;
+    float phi_shift=0.0;
+    float deltaX=0.0;
+    float deltaY=0.0;
+    float rotX=0.0;
+    float rotY=0.0;
 
-	float deg2rad = M_PI/180.0;
-	float degrees6 = 6.0*deg2rad;
-	float degrees0 = 0.0;
-	double L = Z_MAX - Z_MIN; // full length of wire
-	for(int ring=1; ring<=CDC_MAX_RINGS; ring++){
-		int myNstraws=0;
-		float radius = 0.0;
-		float stereo=0.0;
-		float phi_shift=0.0;
-		float deltaX=0.0;
-		float deltaY=0.0;
-		float rotX=0.0;
-		float rotY=0.0;
-
-		switch(ring){
-			// Geometry "C" from http://www.jlab.org/Hall-D/software/wiki/index.php/CDC_Future_MC_Studies_Discussion%2C_September_29%2C_2008
-
-			// axial
-			case  1:	myNstraws=  43;	radius= 10.960;	stereo=  degrees0; phi_shift= 0.00000;	break;
-			case  2:	myNstraws=  43;	radius= 12.341;	stereo=  degrees0; phi_shift= 4.18605;	break;
-			case  3:	myNstraws=  55;	radius= 14.029;	stereo=  degrees0; phi_shift= 2.00000;	break;
-			case  4:	myNstraws=  55;	radius= 15.410;	stereo=  degrees0; phi_shift= 5.27272727272;	break;
-
-			// -stereo
-			case  5:	myNstraws=  66;	radius= 17.085;	stereo= -degrees6; phi_shift= 0.33333;	break;
-			case  6:	myNstraws=  66;	phi_shift= 0.33333;	deltaX= 18.450;	deltaY= 0.886027;	rotX=-6.500;	rotY=-0.30;	break;
-			case  7:	myNstraws=  80;	radius= 20.581;	stereo= -degrees6; phi_shift= -0.5000;	break;
-			case  8:	myNstraws=  80;	phi_shift= -0.5000;	deltaX= 21.950;	deltaY= 0.87224;	rotX=-6.475;	rotY=-0.27;	break;
-
-			// +stereo
-			case  9:	myNstraws=  93;	radius= 23.980;	stereo= +degrees6; phi_shift= 1.1000;	break;
-			case 10:	myNstraws=  93;	phi_shift= 1.1000;	deltaX= 25.35;	deltaY= 0.857573;	rotX=+6.35;	rotY=+0.24;	break;
-			case 11:	myNstraws= 106;	radius= 27.380;	stereo= +degrees6; phi_shift= -1.40;	break;
-			case 12:	myNstraws= 106;	phi_shift= -1.400;	deltaX= 28.800;	deltaY= 0.835;	rotX=+6.3;	rotY=+0.21;	break;
-
-			// axial
-			case 13:	myNstraws= 124;	radius= 31.65;	stereo=  degrees0; phi_shift= 0.5000000;	break;
-			case 14:	myNstraws= 124;	radius= 33.05;	stereo=  degrees0; phi_shift= 1.9516000;	break;
-			case 15:	myNstraws= 133;	radius= 34.7;	stereo=  degrees0; phi_shift= 1.0000000;	break;
-			case 16:	myNstraws= 133;	radius= 36.1;	stereo=  degrees0; phi_shift= 2.3533834;	break;
-
-			// -stereo
-			case 17:	myNstraws= 145;	radius= 37.720;	stereo= -degrees6; phi_shift= 0.2;	break;
-			case 18:	myNstraws= 145;	phi_shift= 0.2;	deltaX= 39.12;	deltaY= 0.8321106;	rotX=-6.25;	rotY=-0.16;	break;
-			case 19:	myNstraws= 158;	radius= 40.89;	stereo= -degrees6; phi_shift= 0.7;	break;
-			case 20:	myNstraws= 158;	phi_shift= 0.7;	deltaX=42.29 ;	deltaY= 0.8391063;	rotX=-6.25;	rotY=-0.13;	break;
-
-			// +stereo
-			case 21:	myNstraws= 171;	radius= 44.05;	stereo= +degrees6; phi_shift= 1.1000;	break;
-			case 22:	myNstraws= 171;	phi_shift= 1.1000;	deltaX=45.46 ;	deltaY= 0.8349124;	rotX=+6.20;	rotY=+0.11;	break;
-			case 23:	myNstraws= 184;	radius= 47.20;	stereo= +degrees6; phi_shift= 1.40;	break;
-			case 24:	myNstraws= 184;	phi_shift= 1.400;	deltaX= 48.63;	deltaY= 0.8303831;	rotX=+6.18;	rotY=+0.10;	break;
-
-			// axial
-			case 25:	myNstraws= 197;	radius= 51.0;	stereo=  degrees0; phi_shift= 0.200000000;	break;
-			case 26:	myNstraws= 197;	radius= 52.4;	stereo=  degrees0; phi_shift= 1.113705000;	break;
-			case 27:	myNstraws= 210;	radius= 54.05;	stereo=  degrees0; phi_shift= 0.800000000;	break;
-			case 28:	myNstraws= 210;	radius= 55.45;	stereo=  degrees0; phi_shift= 1.657142857;	break;
-
-			default:
-				cerr<<__FILE__<<":"<<__LINE__<<" Invalid value for CDC ring ("<<ring<<") should be 1-28 inclusive!"<<endl;
-		}
-		Nstraws[ring-1] = myNstraws;
-		
-		// I'm not sure why this is needed, but empirically, it just is.
-		//stereo = -stereo;
-		//rotX = -rotX;
-		//rotY = -rotY;
+    switch(ring){
+      // axial
+    case  1:	myNstraws=  42;	radius= 10.7219; stereo=  degrees0; phi_shift= 0.00000;	break;
+    case  2:	myNstraws=  42;	radius= 12.0970; stereo=  degrees0; phi_shift= 4.285714;	break;
+    case  3:	myNstraws=  54;	radius= 13.7803; stereo=  degrees0; phi_shift= 2.00000;	break;
+    case  4:	myNstraws=  54;	radius= 15.1621; stereo=  degrees0; phi_shift= 5.3333333;	break;
+      
+      // -stereo
+    case  5:	myNstraws=  66;	radius= 16.9321; stereo= -degrees6; phi_shift= 0.33333;	break;
+    case  6:	myNstraws=  66;	phi_shift= 0.33333;	deltaX= 18.2948;	deltaY= 0.871486;	rotX= -6.47674;	rotY= -0.302853;	break;
+    case  7:	myNstraws=  80;	radius= 20.5213; stereo= -degrees6; phi_shift= -0.5000;	break;
+    case  8:	myNstraws=  80;	phi_shift= -0.5000;	deltaX= 21.8912;	deltaY= 0.860106;	rotX=-6.39548;	rotY=-0.245615;	break;
+      
+      // +stereo
+    case  9:	myNstraws=  93;	radius= 23.8544; stereo= +degrees6; phi_shift= 1.1000;	break;
+    case 10:	myNstraws=  93;	phi_shift= 1.1000;	deltaX=25.229;	deltaY= 0.852573;	rotX=+6.34142;	rotY=+0.208647;	break;
+    case 11:	myNstraws= 106;	radius= 27.1877; stereo= +degrees6; phi_shift= -1.40;	break;
+    case 12:	myNstraws= 106;	phi_shift= -1.400;	deltaX= 28.5658;	deltaY= 0.846871;	rotX=+6.30035;	rotY=+0.181146;	break;
+      
+      // axial
+    case 13:	myNstraws= 123;	radius= 31.3799; stereo=  degrees0; phi_shift= 0.5000000;	break;
+    case 14:	myNstraws= 123;	radius= 32.7747; stereo=  degrees0; phi_shift= 1.9634146;	break;
+    case 15:	myNstraws= 135;	radius= 34.4343; stereo=  degrees0; phi_shift= 1.0000000;	break;
+    case 16:	myNstraws= 135;	radius= 35.8301; stereo=  degrees0; phi_shift= 2.3333333;	break;
+      
+      // -stereo
+    case 17:	myNstraws= 146;	radius= 37.4446; stereo= -degrees6; phi_shift= 0.2;	break;
+    case 18:	myNstraws= 146;	phi_shift= 0.2;	deltaX= 38.8295;	deltaY= 0.835653;	rotX=-6.21919;	rotY=-0.128247;	break;
+    case 19:	myNstraws= 158;	radius= 40.5364; stereo= -degrees6; phi_shift= 0.7;	break;
+    case 20:	myNstraws= 158;	phi_shift= 0.7;	deltaX=41.9225 ;	deltaY= 0.833676;	rotX=-6.20274;	rotY=-0.118271;	break;
+      
+      // +stereo
+    case 21:	myNstraws= 170;	radius= 43.6152; stereo= +degrees6; phi_shift= 1.1000;	break;
+    case 22:	myNstraws= 170;	phi_shift= 1.1000;	deltaX= 45.0025;	deltaY= 0.83173;	rotX=6.18859;	rotY=+0.109325;	break;
+    case 23:	myNstraws= 182;	radius= 46.6849; stereo= +degrees6; phi_shift= 1.40;	break;
+    case 24:	myNstraws= 182;	phi_shift= 1.400;	deltaX= 48.0733;	deltaY=  0.829899;	rotX=+6.1763;	rotY=+0.101315;	break;
+      
+      // axial
+    case 25:	myNstraws= 197;	radius= 50.3747; stereo=  degrees0; phi_shift= 0.200000000;	break;
+    case 26:	myNstraws= 197;	radius= 51.7722; stereo=  degrees0; phi_shift= 1.113705;	break;
+    case 27:	myNstraws= 209;	radius= 53.3631; stereo=  degrees0; phi_shift= 0.800000000;	break;
+    case 28:	myNstraws= 209;	radius= 54.7617; stereo=  degrees0; phi_shift= 1.661244;	break;
+      
+    default:
+      cerr<<__FILE__<<":"<<__LINE__<<" Invalid value for CDC ring ("<<ring<<") should be 1-28 inclusive!"<<endl;
+    }
+    Nstraws[ring-1] = myNstraws;
+    
+    // I'm not sure why this is needed, but empirically, it just is.
+    //stereo = -stereo;
+    //rotX = -rotX;
+    //rotY = -rotY;
+    
+    // Convert phi_shift to radians
+    phi_shift *= M_PI/180.0;
+    
+    float dphi = 2.0*M_PI/(float)myNstraws; // phi angle difference between straws
+    for(int straw=1; straw<=myNstraws; straw++){
+      DCDCWire *w = &wire[ring-1][straw-1];
+      
+      float phi = phi_shift + (float)(straw-1)*dphi;
+      
+      // Define position of midpoint of wire, phi of midpoint, and m_x, m_y
+      w->ring = ring;
+      w->straw = straw;
+      if (rotY==0.){
+	w->origin.SetX(radius*cos(phi));
+	w->origin.SetY(radius*sin(phi));
+      }
+      else{
+	w->origin.SetX(deltaX);
+	w->origin.SetY(deltaY);
+	w->origin.RotateZ(phi);
+      }
+      w->origin.SetZ((Z_MAX + Z_MIN)/2.0);
+      w->phi = phi;
+      //w->L = L/cos(stereo);
+      //w->L = L + (stereo==degrees0 ? 0.0:1.5); // to make consistent with HDDS
 			
-		// Convert phi_shift to radians
-		phi_shift *= M_PI/180.0;
-		
-		float dphi = 2.0*M_PI/(float)myNstraws; // phi angle difference between straws
-		for(int straw=1; straw<=myNstraws; straw++){
-			DCDCWire *w = &wire[ring-1][straw-1];
-
-			float phi = phi_shift + (float)(straw-1)*dphi;
-		
-			// Define position of midpoint of wire, phi of midpoint, and m_x, m_y
-			w->ring = ring;
-			w->straw = straw;
-			if (rotY==0.){
-			  w->origin.SetX(radius*cos(phi));
-			  w->origin.SetY(radius*sin(phi));
+      // Here, we need to define a coordinate system for the wire
+      // in which the wire runs along one axis. We call the directions
+      // of the axes in this coordinate system s,t, and u with
+      // the wire running in the "u" direction. The "s" direction
+      // will be defined by the direction pointing from the beamline
+      // to the midpoint of the wire.
+      w->udir.SetXYZ(0.0, 0.0,1.0);
+      if (rotY==0.){
+	w->udir.RotateX(stereo);	
+	w->udir.RotateZ(phi);
 			}
-			else{
-			  w->origin.SetX(deltaX);
-			  w->origin.SetY(deltaY);
-			  w->origin.RotateZ(phi);
-			}
-			w->origin.SetZ((Z_MAX + Z_MIN)/2.0);
-			w->phi = phi;
-			//w->L = L/cos(stereo);
-			//w->L = L + (stereo==degrees0 ? 0.0:1.5); // to make consistent with HDDS
-			
-			// Here, we need to define a coordinate system for the wire
-			// in which the wire runs along one axis. We call the directions
-			// of the axes in this coordinate system s,t, and u with
-			// the wire running in the "u" direction. The "s" direction
-			// will be defined by the direction pointing from the beamline
-			// to the midpoint of the wire.
-			w->udir.SetXYZ(0.0, 0.0,1.0);
-			if (rotY==0.){
-			  w->udir.RotateX(stereo);	
-			  w->udir.RotateZ(phi);
-			}
-			else{
-			  w->udir.SetXYZ(0.0, 0.0,1.0);
-			  w->udir.RotateY(rotY*deg2rad);	
-			  w->udir.RotateX(rotX*deg2rad);
-			  w->udir.RotateZ(phi);
-			}
-			
-			// With the addition of close-packed stereo wires, the vector connecting
-			// the center of the wire to the beamline ("s" direction) is not necessarily
-			// perpendicular to the beamline. By definition, we want the "s" direction
-			// to be perpendicular to the wire direction "u" and pointing at the beamline.
-			// 
-			// NOTE: This extensive comment is here because the result, when implmented
-			// below caused a WORSE residual distribution in the close-packed stereo
-			// layers. Owing to lack of time currently to track the issue down (most
-			// likely in DReferenceTrajectory) I'm commenting out the "correct" calculation
-			// of s, but leaving this comment so the issue can be revisited later. This
-			// error leads to around 100 micron errors in the C.P.S. wires, but they
-			// are completely washed out when the position smearing of 150 microns is applied
-			// making the error unnoticable except when position smearing is not applied.
-			//
-			// April 2, 2009  D.L.
-			//
-			// Here is how this is calculated -- We define a vector equation with 2 unknowns
-			// Z and S:
-			//
-			//    Zz + Ss = W
-			//
-			// where:  z = unit vector in z direction
-			//         s = unit vector in "s" direction
-			//         W = vector pointing to center of wire in lab coordinates
-			//
-			//  Rearranging, we get:
-			//
-			//     s = (W - Zz)/S
-			//
-			//  Since s must be perpendicular to u, we take a dot product of s and u
-			// and set it equal to zero to determine Z:
-			//
-			//    u.s = 0 = u.(W - Zz)/S  =>  u.W = Zu.z
-			//
-			//   or
-			//
-			//     Z = u.W/u.z
-			//
-			//  Thus, the s direction is just given by (W - (u.W/u.z)z)
-			//
-						
-			//w->sdir=w->origin-TVector3(0,0,w->origin.Z());
-			w->sdir = w->origin - TVector3(0.0, 0.0, w->udir.Dot(w->origin)/w->udir.Z());  // see above comments
-			w->sdir.SetMag(1.0);
+      else{
+	w->udir.SetXYZ(0.0, 0.0,1.0);
+	//w->udir.RotateY(rotY*deg2rad);	
+	w->udir.RotateX(rotX*deg2rad);
+	w->udir.RotateY(rotY*deg2rad);	
+	w->udir.RotateZ(phi);
+      }
+      
+      // With the addition of close-packed stereo wires, the vector connecting
+      // the center of the wire to the beamline ("s" direction) is not necessarily
+      // perpendicular to the beamline. By definition, we want the "s" direction
+      // to be perpendicular to the wire direction "u" and pointing at the beamline.
+      // 
+      // NOTE: This extensive comment is here because the result, when implmented
+      // below caused a WORSE residual distribution in the close-packed stereo
+      // layers. Owing to lack of time currently to track the issue down (most
+      // likely in DReferenceTrajectory) I'm commenting out the "correct" calculation
+      // of s, but leaving this comment so the issue can be revisited later. This
+      // error leads to around 100 micron errors in the C.P.S. wires, but they
+      // are completely washed out when the position smearing of 150 microns is applied
+      // making the error unnoticable except when position smearing is not applied.
+      //
+      // April 2, 2009  D.L.
+      //
+      // Here is how this is calculated -- We define a vector equation with 2 unknowns
+      // Z and S:
+      //
+      //    Zz + Ss = W
+      //
+      // where:  z = unit vector in z direction
+      //         s = unit vector in "s" direction
+      //         W = vector pointing to center of wire in lab coordinates
+      //
+      //  Rearranging, we get:
+      //
+      //     s = (W - Zz)/S
+      //
+      //  Since s must be perpendicular to u, we take a dot product of s and u
+      // and set it equal to zero to determine Z:
+      //
+      //    u.s = 0 = u.(W - Zz)/S  =>  u.W = Zu.z
+      //
+      //   or
+      //
+      //     Z = u.W/u.z
+      //
+      //  Thus, the s direction is just given by (W - (u.W/u.z)z)
+      //
+      
+      //w->sdir=w->origin-TVector3(0,0,w->origin.Z());
+      w->sdir = w->origin - TVector3(0.0, 0.0, w->udir.Dot(w->origin)/w->udir.Z());  // see above comments
+      w->sdir.SetMag(1.0);
+      
+      w->tdir = w->udir.Cross(w->sdir);
+      w->tdir.SetMag(1.0); // This isn't really needed
+      
+      w->stereo = w->udir.Angle(TVector3(0,0,1));
+      //if(rotX>0.0)w->stereo = -w->stereo;
+      w->L = L/cos(w->stereo);
 
-			w->tdir = w->udir.Cross(w->sdir);
-			w->tdir.SetMag(1.0); // This isn't really needed
 
-			w->stereo = w->udir.Angle(TVector3(0,0,1));
-			//if(rotX>0.0)w->stereo = -w->stereo;
-			w->L = L/cos(w->stereo);
-		}
-	}
-
-	// Flag table as initialized and release the lock so other threads
-	// can continue.
-	wire_table_initialized=true;
-	pthread_mutex_unlock(&wire_mutex);
-
-	return NOERROR;
+    }
+  }
+  
+  // Flag table as initialized and release the lock so other threads
+  // can continue.
+  wire_table_initialized=true;
+  pthread_mutex_unlock(&wire_mutex);
+  
+  return NOERROR;
 }
 
 //------------------
