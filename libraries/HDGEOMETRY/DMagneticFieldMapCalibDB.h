92147d54ad2c1c8291f31db2cc532a62c45a27d9
David Lawrence <davidl@jlab.org>
2009-10-13 18:06:45 +0000

    Switching default from Ansys generated map to Poisson/Superfish generated map. This is motivated by the fact that the Poisson map appears smoother as a function of R and that the Ansys map shows a distinct 'step' at r=few cm that intuitively seems unphysical.

--- libraries/HDGEOMETRY/DMagneticFieldMapCalibDB.h
+++ libraries/HDGEOMETRY/DMagneticFieldMapCalibDB.h
@@ -23,8 +23,8 @@ using namespace jana;
 
 class DMagneticFieldMapCalibDB:public DMagneticFieldMap{
 	public:
-		DMagneticFieldMapCalibDB(JApplication *japp, string namepath = "Magnets/Solenoid/solenoid_1500_20090312-2");
-		DMagneticFieldMapCalibDB(JCalibration *jcalib, string namepath = "Magnets/Solenoid/solenoid_1500_20090312-2");
+		DMagneticFieldMapCalibDB(JApplication *japp, string namepath = "Magnets/Solenoid/solenoid_1500_poisson_20090814_01");
+		DMagneticFieldMapCalibDB(JCalibration *jcalib, string namepath = "Magnets/Solenoid/solenoid_1500_poisson_20090814_01");
 		virtual ~DMagneticFieldMapCalibDB();
 		
 		int ReadMap(string namepath, int runnumber=1, string context="");
