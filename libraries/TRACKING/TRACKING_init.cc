f835e7b3edee3fc4d67a669bdb541584c7d157d5
David Lawrence <davidl@jlab.org>
2009-10-15 12:42:55 +0000

    Remove obsolete ALT2 fitter.

--- libraries/TRACKING/TRACKING_init.cc
+++ libraries/TRACKING/TRACKING_init.cc
@@ -10,7 +10,6 @@
 #include "DTrackCandidate_factory_FDCpseudo.h"
 #include "DTrackCandidate_factory_CDC_or_FDCpseudo.h"
 #include "DTrack_factory_THROWN.h"
-#include "DTrack_factory_ALT2.h"
 #include "DTrack_factory_Kalman.h"
 #include "DTrackFitter_factory.h"
 #include "DTrackFitter_factory_ALT1.h"
@@ -29,7 +28,6 @@ typedef JFactory<DMCTrajectoryPoint> DMCTrajectoryPoint_factory;
 jerror_t TRACKING_init(JEventLoop *loop)
 {
 	/// Create and register TRACKING data factories
-	loop->AddFactory(new DTrack_factory_ALT2());
 	loop->AddFactory(new DTrack_factory());
 	loop->AddFactory(new DTrack_factory_Kalman());
 	loop->AddFactory(new DTrackCandidate_factory());
