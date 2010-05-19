896e9512ae11de8f86c3c39be4bf93438c8cc990
Simon Taylor <staylor@jlab.org>
2010-05-19 22:19:04 +0000

    Add the KalmanSIMD factory.

--- src/libraries/TRACKING/TRACKING_init.cc
+++ src/libraries/TRACKING/TRACKING_init.cc
@@ -17,6 +17,7 @@
 #include "DTrackFitter_factory.h"
 #include "DTrackFitter_factory_ALT1.h"
 #include "DTrackFitter_factory_Kalman.h"
+#include "DTrackFitter_factory_KalmanSIMD.h"
 #include "DTrackHitSelector_factory.h"
 #include "DTrackHitSelector_factory_ALT1.h"
 #include "DTrackHitSelector_factory_THROWN.h"
@@ -49,7 +50,8 @@ jerror_t TRACKING_init(JEventLoop *loop)
 	loop->AddFactory(new DTrackTimeBased_factory_THROWN());
 	loop->AddFactory(new DTrackFitter_factory());
 	loop->AddFactory(new DTrackFitter_factory_ALT1());
-	loop->AddFactory(new DTrackFitter_factory_Kalman());
+	loop->AddFactory(new DTrackFitter_factory_Kalman());	
+	loop->AddFactory(new DTrackFitter_factory_KalmanSIMD());
 	loop->AddFactory(new DTrackHitSelector_factory());
 	loop->AddFactory(new DTrackHitSelector_factory_ALT1());
 	loop->AddFactory(new DTrackHitSelector_factory_THROWN());
