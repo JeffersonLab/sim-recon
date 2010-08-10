36ba0e0fe6471e19c33fbacfa8f6885664061727
Simon Taylor <staylor@jlab.org>
2010-08-10 14:10:00 +0000

    Create new DTagger object for storing the tagger info from hddm.

--- src/libraries/TAGGER/TAGGER_init.cc
+++ src/libraries/TAGGER/TAGGER_init.cc
@@ -1,14 +1,16 @@
 // $Id$
 
-/// At this point in time (April 2008), there is no tagger
-/// reconstruction done. This is simply a place holder.
-
 #include <JANA/JEventLoop.h>
 using namespace jana;
 
+#include "DTagger.h"
+typedef JFactory<DTagger> DTagger_factory;
+
+
 jerror_t TAGGER_init(JEventLoop *loop)
 {
-	/// Create and register TAGGER data factories
-
-	return NOERROR;
+  /// Create and register TAGGER data factories
+  loop->AddFactory(new DTagger_factory());
+  
+  return NOERROR;
 }
