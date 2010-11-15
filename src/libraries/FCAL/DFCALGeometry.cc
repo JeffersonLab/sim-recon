900f765390f56bf355aaf408b31eb6b2cae5c46a
David Lawrence <davidl@jlab.org>
2010-11-15 19:14:58 +0000

    Fix a problem introduced (by me) when line stating "FCAL Geometry initalized with ..." is printed was changed to use jout instead of cout. This caused seg. faults when running hdview2 because it creates a DFCALGeometry object in global code space (outside of main) causing it to call the constructor before jout was properly set up. This at least caused seg. faults (prior to ever reaching main()) on OS X 10.6. I have a vague recollection of contradictory evidence on Linux.

--- src/libraries/FCAL/DFCALGeometry.cc
+++ src/libraries/FCAL/DFCALGeometry.cc
@@ -50,8 +50,12 @@ m_numActiveBlocks( 0 )
 			}
 		}
 	}
-		
-	jout << "FCAL Geometry initialized with " << m_numActiveBlocks 
+
+	// In hdview2, a DFCALGeometry object is created globally (before 
+	// main() is called) so that jout is not yet defined leading to
+	// seg. faults before entering main(). Use cout here to avoid these
+	// crashes.
+	cout << "FCAL Geometry initialized with " << m_numActiveBlocks 
 		<< " active blocks." << endl;
 }
 
