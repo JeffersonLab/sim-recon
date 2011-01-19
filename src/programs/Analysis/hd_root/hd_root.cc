bdf96af8bc02f0c076c281776463aeab3e40ffce
David Lawrence <davidl@jlab.org>
2011-01-19 14:29:02 +0000

    Allow output filename used by hd_root to be set via the OUTPUT_FILENAME configuration parameter. The -o fname command line option is still available and takes precedence.
    Also, the -o fname arguments (if present) are removed from
    the argument list passed to DApplication. This is so JANA
    won't think the "fname" part is an input file and print the
    misleading "Unable to open event source" message.

--- src/programs/Analysis/hd_root/hd_root.cc
+++ src/programs/Analysis/hd_root/hd_root.cc
@@ -15,8 +15,11 @@ using namespace std;
 typedef void SetTFilePtrAddress_t(TFile **);
 TFile* tfilePtr = NULL;
 string OUTPUT_FILENAME = "hd_root.root";
+string COMMAND_LINE_OUTPUT_FILENAME = "";
+bool filename_from_command_line = false;
 
 void ParseCommandLineArguments(int &narg, char *argv[]);
+void DecideOutputFilename(void);
 void Usage(void);
 
 
@@ -33,7 +36,10 @@ int main(int narg, char *argv[])
 
 	// Instantiate an event loop object
 	DApplication app(narg, argv);
-
+	
+	// Decide on the output filename
+	DecideOutputFilename();
+	
 	// Run though all events, calling our event processor's methods
 	app.monitor_heartbeat = 0;
 	app.Run(&myproc);
@@ -48,7 +54,7 @@ int main(int narg, char *argv[])
 void ParseCommandLineArguments(int &narg, char *argv[])
 {
 	if(narg==1)Usage();
-
+	
 	for(int i=1;i<narg;i++){
 		if(argv[i][0] != '-')continue;
 		switch(argv[i][1]){
@@ -65,13 +71,43 @@ void ParseCommandLineArguments(int &narg, char *argv[])
 					cerr<<"\"-o\" requires a filename!"<<endl;
 					exit(-1);
 				}
-				OUTPUT_FILENAME = argv[i+1];
-_DBG_<<"OUTPUT_FILENAME="<<OUTPUT_FILENAME<<endl;
+				COMMAND_LINE_OUTPUT_FILENAME = argv[i+1];
+				filename_from_command_line = true;
+				
+				// Remove the "-o fname" arguments from file list so
+				// JANA won't think the "fname" is an input file.
+				for(int j=i; j<(narg-2); j++)argv[j] = argv[j+2];
+				narg -= 2;
 				break;
 		}
 	}
 }
 
+//-----------
+// DecideOutputFilename
+//-----------
+void DecideOutputFilename(void)
+{
+	/// Decide on the output filename to use based on the command line
+	/// input and configuration parameter input. The command line takes
+	/// precedence. This also makes sure to copy the filename that is
+	/// being used into the configuration parameter.
+
+	// Set the default output filename (avoids later warnings from JANA)
+	gPARMS->SetDefaultParameter("OUTPUT_FILENAME", OUTPUT_FILENAME,"Output filename used by hd_root");
+	
+	// If the user specified an output filename on the command line,
+	// use it to overwrite the config. parameter/default one
+	if(filename_from_command_line){
+		OUTPUT_FILENAME = COMMAND_LINE_OUTPUT_FILENAME;
+	
+		// Set the actual output filename in config. param.
+		gPARMS->SetParameter("OUTPUT_FILENAME", OUTPUT_FILENAME);
+	}
+
+	jout<<"OUTPUT_FILENAME: "<<OUTPUT_FILENAME<<endl;
+
+}
 
 //-----------
 // Usage
