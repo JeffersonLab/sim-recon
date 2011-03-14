34563811de3c1a6f5f3e78603b1dd84b26bcf334
Matt Shepherd <shepherd@jlab.org>
2011-03-14 21:01:10 +0000

    Fix BUG in SECTOR_SHIFT for encoding layer, module, and sector into a single integer.  Versions prior to this that utilized the routines to convert to cellId and then back to layer, sector would have had corrupted layer, sector values.  Add additional functions to get the location of the center and size of cells in r, phi.

--- src/libraries/BCAL/DBCALGeometry.cc
+++ src/libraries/BCAL/DBCALGeometry.cc
@@ -44,3 +44,137 @@ DBCALGeometry::DBCALGeometry()
       assert (false);
     }
 }
+
+int
+DBCALGeometry::module( int cellId ) {
+  
+  return ( cellId & MODULE_MASK ) >> MODULE_SHIFT;
+}
+
+int
+DBCALGeometry::layer( int cellId ) {
+  
+  return ( cellId & LAYER_MASK ) >> LAYER_SHIFT;
+}
+
+int
+DBCALGeometry::sector( int cellId ) {
+  
+  return ( cellId & SECTOR_MASK ) >> SECTOR_SHIFT;
+}
+
+int
+DBCALGeometry::cellId( int module, int layer, int sector ) {
+  
+  return ( ( module << MODULE_SHIFT ) | 
+           ( layer << LAYER_SHIFT   ) | 
+           ( sector << SECTOR_SHIFT ) );
+}
+
+float
+DBCALGeometry::r( int cell ) {
+ 
+  int lay = layer( cell );
+  
+  float innerRad, outerRad;
+ 
+  if( lay <= NBCALLAYS1 ) { // we're in the inner region
+   
+    // the size of the layer in SiPM units
+    int laySize = ( BCALMID - 1 ) / NBCALLAYS1;
+    
+    innerRad = m_radius[ laySize * ( lay - 1 ) ];
+    
+    // index below is same as:  laySize * ( lay - 1 ) + laySize
+    outerRad = m_radius[ laySize * lay ];
+  }
+  else{
+    
+    // find the size of the outer layer in SiPM units
+    int laySize = ( 11 - BCALMID ) / NBCALLAYS2;
+    
+    // subtract the number of inner layers to get
+    // layer number within the outer section
+    lay -= NBCALLAYS1;
+    
+    innerRad = m_radius[ BCALMID - 1 + laySize * ( lay - 1 ) ];
+    outerRad = m_radius[ BCALMID - 1 + laySize * lay ];
+  }
+  
+  return 0.5 * ( innerRad + outerRad );
+}
+
+float
+DBCALGeometry::rSize( int cell ) {
+  
+  int lay = layer( cell );
+  
+  float innerRad, outerRad;
+  
+  if( lay <= NBCALLAYS1 ) { // in the inner region
+    
+    // the size of the layer in SiPM units
+    int laySize = ( BCALMID - 1 ) / NBCALLAYS1;
+    
+    innerRad = m_radius[ laySize * ( lay - 1 ) ];
+    
+    // index below is same as:  laySize * ( lay - 1 ) + laySize
+    outerRad = m_radius[ laySize * lay ];
+  }
+  else{ // in the outer region
+    
+    // find the size of the outer layer in SiPM units
+    int laySize = ( 11 - BCALMID ) / NBCALLAYS2;
+    
+    // subtract the number of inner layers to get
+    // layer number within the outer section
+    lay -= NBCALLAYS1;
+    
+    innerRad = m_radius[ BCALMID - 1 + laySize * ( lay - 1 ) ];
+    outerRad = m_radius[ BCALMID - 1 + laySize * lay ];
+  }
+  
+  return ( outerRad - innerRad );
+}
+
+float
+DBCALGeometry::phi( int cell ) {
+ 
+  int lay = layer( cell );
+  int sect = sector( cell );
+  
+  float sectSize;
+  
+  if( lay <= NBCALLAYS1 ) { 
+    
+    sect += NBCALSECS1 * ( module( cell ) - 1 );
+    sectSize = 2 * PI / ( NBCALMODS * NBCALSECS1 );
+  }
+  else {
+    
+    sect += NBCALSECS2 * ( module( cell ) - 1 );
+    sectSize = 2 * PI / ( NBCALMODS * NBCALSECS2 );
+  }
+  
+  return sectSize * ( sect - 0.5 );
+}
+
+float
+DBCALGeometry::phiSize( int cell ) {
+  
+  int lay = layer( cell );
+  
+  float sectSize;
+  
+  if( lay <= NBCALLAYS1 ) { 
+    
+    sectSize = 2 * 3.1416 / ( NBCALMODS * NBCALSECS1 );
+  }
+  else {
+    
+    sectSize = 2 * 3.1416 / ( NBCALMODS * NBCALSECS2 );
+  }
+  
+  return sectSize;
+}
+
--- src/libraries/BCAL/DBCALGeometry.h
+++ src/libraries/BCAL/DBCALGeometry.h
@@ -20,43 +20,49 @@ using namespace jana;
 #define MODULE_MASK 0xFF00
 #define LAYER_SHIFT 4
 #define LAYER_MASK 0x00F0
-#define SECTOR_SHIFT 4
+#define SECTOR_SHIFT 0
 #define SECTOR_MASK 0X000F
 
-class DBCALGeometry : public JObject{
-    
-public:
-    
-    JOBJECT_PUBLIC(DBCALGeometry);
-    
-    DBCALGeometry();
-    
-    enum End { kUpstream, kDownstream };
-        
-    static int NBCALMODS;         ///> number of modules
-    static int NBCALLAYS1;        ///> number of layers in first 10 ccm 
-    static int NBCALLAYS2;        ///> number of layers in last  15 cm 
-    static int NBCALSECS1;        ///> number of sectors in first 10cm of Mod 
-    static int NBCALSECS2;        ///> number of sectors in last 15cm of Mod 
-    static float BCALINNERRAD;    ///> innner radius of BCAL in cm
-    static int BCALMID;         ///> first outer layer (default 7)
-    static float m_radius[11];
-    float BCALMIDRAD;      ///> mid radius of BCAL in cm
-    static float BCALOUTERRAD;    ///> outer radius of BCAL in cm
-    static float BCALFIBERLENGTH; ///> BCAL Scintilator fiber lenth in cm
-    static float GLOBAL_CENTER;  ///> center of BCAL in gloobal coordinate system
-    
-    static float ATTEN_LENGTH;   ///> attenuation length
-    static float C_EFFECTIVE;    ///> speed of light in fibers 
-    
-    static inline int module( int cellId ) { return ( cellId & MODULE_MASK ) >> MODULE_SHIFT; };
-    static inline int layer( int cellId ) { return ( cellId & LAYER_MASK ) >> LAYER_SHIFT; };
-    static inline int sector( int cellId ) { return ( cellId & SECTOR_MASK ) >> SECTOR_SHIFT; };                
-    
-    static inline int cellId( int module, int layer, int sector ) {
-        return ( ( module << MODULE_SHIFT ) | ( layer << LAYER_SHIFT ) | 
-                 ( sector << SECTOR_SHIFT ) ); }
+#define PI 3.1416
 
+class DBCALGeometry : public JObject {
+  
+public:
+  
+  JOBJECT_PUBLIC( DBCALGeometry );
+  
+  DBCALGeometry();
+  
+  enum End { kUpstream, kDownstream };
+  
+  static int NBCALMODS;         ///> number of modules
+  static int NBCALLAYS1;        ///> number of layers in first 10 ccm 
+  static int NBCALLAYS2;        ///> number of layers in last  15 cm 
+  static int NBCALSECS1;        ///> number of sectors in first 10cm of Mod 
+  static int NBCALSECS2;        ///> number of sectors in last 15cm of Mod 
+  static float BCALINNERRAD;    ///> innner radius of BCAL in cm
+  static int BCALMID;         ///> first outer layer (default 7)
+  static float m_radius[11];
+  float BCALMIDRAD;      ///> mid radius of BCAL in cm
+  static float BCALOUTERRAD;    ///> outer radius of BCAL in cm
+  static float BCALFIBERLENGTH; ///> BCAL Scintilator fiber lenth in cm
+  static float GLOBAL_CENTER;  ///> center of BCAL in gloobal coordinate system
+  
+  static float ATTEN_LENGTH;   ///> attenuation length
+  static float C_EFFECTIVE;    ///> speed of light in fibers 
+  
+  static int module( int cellId );  
+  static int layer( int cellId );
+  static int sector( int cellId );
+
+  static int cellId( int module, int layer, int sector );
+
+  static float phi( int cellId );
+  static float phiSize( int cellId );
+  
+  static float r( int cellId );
+  static float rSize( int cellId );
+  
 };
 
 #endif // _DBCALGeometry_
