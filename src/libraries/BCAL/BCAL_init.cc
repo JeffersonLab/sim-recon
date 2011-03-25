586324dbd8da7cbc2dad3f05b146d35d245e238c
Matt Shepherd <shepherd@jlab.org>
2011-03-25 15:07:47 +0000

    register new shower factory

--- src/libraries/BCAL/BCAL_init.cc
+++ src/libraries/BCAL/BCAL_init.cc
@@ -3,6 +3,7 @@
 #include <JANA/JEventLoop.h>
 #include "DBCALGeometry_factory.h"
 #include "DBCALShower_factory_KLOE.h"
+#include "DBCALShower_factory.h"
 #include "DBCALCluster_factory.h"
 #include "DBCALPhoton_factory.h"
 #include "DBCALHit.h"
@@ -18,6 +19,7 @@ jerror_t BCAL_init(JEventLoop *loop)
 	loop->AddFactory(new DBCALHit_factory());
 	loop->AddFactory(new DBCALGeometry_factory());
 	loop->AddFactory(new DBCALShower_factory_KLOE());
+  loop->AddFactory(new DBCALShower_factory());
   loop->AddFactory(new DBCALCluster_factory());
 	loop->AddFactory(new DBCALTruthShower_factory());
 	loop->AddFactory(new DBCALPhoton_factory());
