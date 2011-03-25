c82017af9dab8a3880786c8cba541ce5440ae592
Matt Shepherd <shepherd@jlab.org>
2011-03-25 15:02:49 +0000

    add diagnostic trees that can be filled by setting BCAL_CLUSTER_DIAGNOSTIC

--- src/libraries/BCAL/DBCALCluster_factory.cc
+++ src/libraries/BCAL/DBCALCluster_factory.cc
@@ -27,6 +27,75 @@ bool ClusterSort( const DBCALCluster& c1, const DBCALCluster& c2 ){
   return ( c1.E() > c2.E() );
 }
 
+// right now mergeSig is the only parameter that is used to control
+// merging of clusters and points
+
+DBCALCluster_factory::DBCALCluster_factory() : m_mergeSig( 8 ){
+  
+}
+
+#ifdef BCAL_CLUSTER_DIAGNOSTIC
+
+jerror_t
+DBCALCluster_factory::init(void){
+
+  m_rootFile = new TFile( "bcal_clust_diag.root", "RECREATE" );
+  
+  m_twoEndPtTr = new TTree( "twoEndPtTr", "BCAL two-ended points" );
+  m_twoEndPtTr->Branch( "n2EPt", &m_n2EPt, "n2EPt/I" );
+  m_twoEndPtTr->Branch( "rhoPt", m_rhoPt, "rhoPt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "phiPt", m_phiPt, "phiPt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "thePt", m_thePt, "thePt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "rhoSPt", m_rhoSPt, "rhoSPt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "phiSPt", m_phiSPt, "phiSPt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "theSPt", m_theSPt, "theSPt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "ePt", m_ePt, "ePt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "tPt", m_tPt, "tPt[n2EPt]/F" );
+  m_twoEndPtTr->Branch( "t0Pt", m_t0Pt, "t0Pt[n2EPt]/F" );
+  
+  m_firstClustTr = new TTree( "firstClustTr", "First-pass clusters" );
+  m_firstClustTr->Branch( "nCl", &m_nCl, "nCl/I" );
+  m_firstClustTr->Branch( "nPts", m_nPts, "nPts[nCl]/I" );
+  m_firstClustTr->Branch( "rhoCl", m_rhoCl, "rhoCl[nCl]/F" );
+  m_firstClustTr->Branch( "phiCl", m_phiCl, "phiCl[nCl]/F" );
+  m_firstClustTr->Branch( "theCl", m_theCl, "theCl[nCl]/F" );
+  m_firstClustTr->Branch( "rhoSCl", m_rhoSCl, "rhoSCl[nCl]/F" );
+  m_firstClustTr->Branch( "phiSCl", m_phiSCl, "phiSCl[nCl]/F" );
+  m_firstClustTr->Branch( "theSCl", m_theSCl, "theSCl[nCl]/F" );
+  m_firstClustTr->Branch( "eCl", m_eCl, "eCl[nCl]/F" );
+  m_firstClustTr->Branch( "tCl", m_tCl, "tCl[nCl]/F" );
+  
+  m_ovrlpTr = new TTree( "ovrlpTr", "Point Cluster Overlap" );
+  m_ovrlpTr->Branch( "dPhi", &m_dPhi, "dPhi/F" );
+  m_ovrlpTr->Branch( "dThe", &m_dThe, "dThe/F" );
+  m_ovrlpTr->Branch( "sigPhi", &m_sigPhi, "sigPhi/F" );
+  m_ovrlpTr->Branch( "sigThe", &m_sigThe, "sigThe/F" );
+  m_ovrlpTr->Branch( "eClus", &m_eClus, "eClus/F" );
+  m_ovrlpTr->Branch( "rhoClus", &m_rhoClus, "rhoClus/F" );
+  m_ovrlpTr->Branch( "phiClus", &m_phiClus, "phiClus/F" );
+  m_ovrlpTr->Branch( "theClus", &m_theClus, "theClus/F" );
+  m_ovrlpTr->Branch( "nClClus", &m_nClClus, "nClClus/I" );
+  
+  return NOERROR;
+
+}
+
+jerror_t
+DBCALCluster_factory::fini( void ){
+  
+  m_rootFile->cd();
+  
+  m_twoEndPtTr->Write();
+  m_firstClustTr->Write();
+  m_ovrlpTr->Write();
+  m_rootFile->Write();
+  m_rootFile->Close();
+  
+  return NOERROR;
+}
+
+#endif
+
 jerror_t
 DBCALCluster_factory::evnt( JEventLoop *loop, int eventnumber ){
 
@@ -44,6 +113,12 @@ DBCALCluster_factory::evnt( JEventLoop *loop, int eventnumber ){
     
     const DBCALHit& hit = (**hitPtr);
     
+    // mcsmear will produce hits with energy zero if the hits do not
+    // exceed the threshold -- we want to suppress these hits so we know
+    // exactly how many "real" hits we have in a cell
+    
+    if( hit.E < 0.1*k_MeV ) continue;
+    
     int id = DBCALGeometry::cellId( hit.module, hit.layer, hit.sector );
     
     if( cellHitMap.find( id ) == cellHitMap.end() ){
@@ -55,8 +130,7 @@ DBCALCluster_factory::evnt( JEventLoop *loop, int eventnumber ){
   }
   
   // now go through this list and group hits into BCAL points
-  // this combines information from both ends -- while doing so
-  // remove the "consumed" cells from the map
+  // this combines information from both ends 
   
   vector< const DBCALPoint* > twoEndPoint;
   
@@ -72,30 +146,62 @@ DBCALCluster_factory::evnt( JEventLoop *loop, int eventnumber ){
       m_bcalPoints.push_back( new DBCALPoint( *(mapItr->second[0]), 
                                               *(mapItr->second[1]) ) );
 
-      twoEndPoint.push_back( m_bcalPoints.back() );
+      twoEndPoint.push_back( m_bcalPoints.back() );      
     }
   }
 
-  /*
-  // debugging print of points:
-  cout << "BCAL Points: \tE\tphi\tdphi\ttheta\tdtheta" << endl;
-  for( vector< const DBCALPoint* >::iterator pt = twoEndPoint.begin();
-       pt != twoEndPoint.end();
-       ++pt ){
-    
-    cout << "          \t" << (**pt).E() << "\t" << (**pt).phi() << "\t" << (**pt).sigPhi()
-         << "\t" << (**pt).theta() << "\t" << (**pt).sigTheta() << endl;
+#ifdef BCAL_CLUSTER_DIAGNOSTIC
+  
+  m_rootFile->cd();
+  
+  m_n2EPt = twoEndPoint.size();
+  assert( m_n2EPt <= MAX_POINT );
+  
+  for( int i = 0; i < m_n2EPt; ++i ){
+   
+    m_rhoPt[i] = twoEndPoint[i]->rho();
+    m_phiPt[i] = twoEndPoint[i]->phi();
+    m_thePt[i] = twoEndPoint[i]->theta();
+    m_rhoSPt[i] = twoEndPoint[i]->sigRho();
+    m_phiSPt[i] = twoEndPoint[i]->sigPhi();
+    m_theSPt[i] = twoEndPoint[i]->sigTheta();
+    m_ePt[i] = twoEndPoint[i]->E();
+    m_tPt[i] = twoEndPoint[i]->t();
+    m_t0Pt[i] = twoEndPoint[i]->tInnerRadius();
   }
-  */
+  
+  m_twoEndPtTr->Fill();
+  
+#endif // BCAL_CLUSTER_DIAGNOSTIC
   
   // now try to clusterize the points
   
   vector< DBCALCluster > clusters = clusterize( twoEndPoint );
   
+#ifdef BCAL_CLUSTER_DIAGNOSTIC
+  
+  m_nCl = clusters.size();
+  assert( m_nCl <= MAX_CLUST );
+  
+  for( int i = 0; i < m_nCl; ++i ){
+    
+    m_nPts[i] = clusters[i].nCells();
+    m_rhoCl[i] = clusters[i].rho();
+    m_phiCl[i] = clusters[i].phi();
+    m_theCl[i] = clusters[i].theta();
+    m_rhoSCl[i] = clusters[i].sigRho();
+    m_phiSCl[i] = clusters[i].sigPhi();
+    m_theSCl[i] = clusters[i].sigTheta();
+    m_eCl[i] = clusters[i].E();
+    m_tCl[i] = clusters[i].t();
+  }
+  
+  m_firstClustTr->Fill();
+  
+#endif // BCAL_CLUSTER_DIAGNOSTIC
+  
+  
   // now we should try to add on single hits...
-  //
-  // MRS note:  14-Mar-11 -- don't seem to see any single ended hits
-  // maybe problem in generation of DBCALHit?
   for( map< int, vector< const DBCALHit* > >::iterator mapItr = cellHitMap.begin();
       mapItr != cellHitMap.end();
       ++mapItr ){
@@ -120,7 +226,7 @@ DBCALCluster_factory::evnt( JEventLoop *loop, int eventnumber ){
           // given the location of the cluster, we need the best guess
           // for z with respect to target at this radius
           float z = r / tan( clust->theta() );
-      
+          
           // make a point there
           DBCALPoint* pt = new DBCALPoint( *hit, z );
           
@@ -279,6 +385,9 @@ DBCALCluster_factory::overlap( const DBCALCluster& highEClust,
   sqrt( highEClust.sigPhi() * highEClust.sigPhi() +
         lowEClust.sigPhi()  * lowEClust.sigPhi() );
   
+  // right now this is a rectangular cut, but it could be circular
+  // or elliptical
+  
   return( ( sigTheta < m_mergeSig ) && ( sigPhi < m_mergeSig ) );  
 }
 
@@ -289,7 +398,7 @@ DBCALCluster_factory::overlap( const DBCALCluster& clust,
   float sigTheta = fabs( clust.theta() - point->theta() ) / 
   sqrt( clust.sigTheta() * clust.sigTheta() +
        point->sigTheta()  * point->sigTheta() );
-  
+
   // difference in phi is tricky due to overlap at 0/2pi
   // order based on phi and then take the minimum of the difference
   // and the difference with 2pi added to the smallest
@@ -305,6 +414,25 @@ DBCALCluster_factory::overlap( const DBCALCluster& clust,
   sqrt( clust.sigPhi() * clust.sigPhi() +
        point->sigPhi()  * point->sigPhi() );
   
+#ifdef BCAL_CLUSTER_DIAGNOSTIC
+  
+  m_dPhi = deltaPhi;
+  m_dThe = fabs( clust.theta() - point->theta() );
+  m_sigPhi = sigPhi;
+  m_sigThe = sigTheta;
+  m_eClus = clust.E();
+  m_rhoClus = clust.rho();
+  m_phiClus = clust.phi();
+  m_theClus = clust.theta();
+  m_nClClus = clust.nCells();
+  
+  m_ovrlpTr->Fill();
+  
+#endif // BCAL_CLUSTER_DIAGNOSTIC
+  
+  // right now this is a rectangular cut, but it could be circular
+  // or elliptical
+  
   return( ( sigTheta < m_mergeSig ) && ( sigPhi < m_mergeSig ) );
 }
 
@@ -315,7 +443,7 @@ DBCALCluster_factory::overlap( const DBCALCluster& clust,
   int cellId = DBCALGeometry::cellId( hit->module, hit->layer, hit->sector );
 
   float cellPhi = DBCALGeometry::phi( cellId );
-  float cellSigPhi = DBCALGeometry::phiSize( cellId ) / sqrt( 12 );
+  float cellSigPhi = DBCALGeometry::phiSize( cellId );
              
   // annoying +- 2pi business to try to find the best delta phi
                
--- src/libraries/BCAL/DBCALCluster_factory.h
+++ src/libraries/BCAL/DBCALCluster_factory.h
@@ -16,14 +16,16 @@ using namespace jana;
 #include "BCAL/DBCALHit.h"
 #include "BCAL/DBCALCluster.h"
 
+#include "TTree.h"
+#include "TFile.h"
+
+//#define BCAL_CLUSTER_DIAGNOSTIC
+
 class DBCALCluster_factory : public JFactory< DBCALCluster > {
   
 public:
   
-  // right now mergeSig is the only parameter that is used to control
-  // merging of clusters and points
-  
-  DBCALCluster_factory() : m_mergeSig( 15 ){}
+  DBCALCluster_factory();
   ~DBCALCluster_factory(){}
   
 private:
@@ -55,6 +57,53 @@ private:
   // a future stage; for now have this factory own and maintain them
   
   vector< DBCALPoint* > m_bcalPoints;
+    
+#ifdef BCAL_CLUSTER_DIAGNOSTIC
+  
+#define MAX_POINT 1000
+#define MAX_CLUST 50 
+  
+  jerror_t init();
+  jerror_t fini();
+  
+  TFile* m_rootFile;
+  TTree* m_twoEndPtTr;
+  TTree* m_firstClustTr;
+  TTree* m_ovrlpTr;
+
+  mutable int m_n2EPt;
+  mutable float m_rhoPt[MAX_POINT];
+  mutable float m_phiPt[MAX_POINT];
+  mutable float m_thePt[MAX_POINT];
+  mutable float m_rhoSPt[MAX_POINT];
+  mutable float m_phiSPt[MAX_POINT];
+  mutable float m_theSPt[MAX_POINT];
+  mutable float m_ePt[MAX_POINT];
+  mutable float m_tPt[MAX_POINT];
+  mutable float m_t0Pt[MAX_POINT];
+  
+  mutable int m_nCl;
+  mutable int m_nPts[MAX_CLUST];
+  mutable float m_rhoCl[MAX_CLUST];
+  mutable float m_phiCl[MAX_CLUST];
+  mutable float m_theCl[MAX_CLUST];
+  mutable float m_rhoSCl[MAX_CLUST];
+  mutable float m_phiSCl[MAX_CLUST];
+  mutable float m_theSCl[MAX_CLUST];
+  mutable float m_eCl[MAX_CLUST];
+  mutable float m_tCl[MAX_CLUST];
+  
+  mutable float m_dPhi;
+  mutable float m_dThe;
+  mutable float m_sigPhi;
+  mutable float m_sigThe;
+  mutable float m_eClus;
+  mutable float m_rhoClus;
+  mutable float m_theClus;
+  mutable float m_phiClus;
+  mutable int m_nClClus;
+  
+#endif
   
 };
 
