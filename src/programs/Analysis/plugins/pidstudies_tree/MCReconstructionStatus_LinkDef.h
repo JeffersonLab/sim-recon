2e301324dce41536f02a5d90378ee5f59eef3fb3
Paul Mattione <pmatt@jlab.org>
2011-10-06 13:17:03 +0000

    Plugin for stuyding the quality of the PID.

---
+++ src/programs/Analysis/plugins/pidstudies_tree/MCReconstructionStatus_LinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#include <vector>
+#include "MCReconstructionStatus.h"
+#include "ReconstructedHypothesis.h"
+/*
+#pragma link C++ class ReconstructedHypothesis+;
+#pragma link C++ class vector<ReconstructedHypothesis*>+;
+#pragma link C++ class vector<ReconstructedHypothesis>+;
+*/
+#pragma link C++ class MCReconstructionStatus+;
+#pragma link C++ class vector<MCReconstructionStatus*>+;
+#pragma link C++ class vector<MCReconstructionStatus>+;
+
+#endif
+
