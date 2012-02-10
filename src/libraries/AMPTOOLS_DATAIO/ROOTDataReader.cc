badc3daa72dc82e471a002b5dce2f0d11b4327d4
Igor Senderovich <senderov@jlab.org>
2012-02-10 05:51:33 +0000

    Enhancements to the ROOTData Reader/Writer: full kinematics tree name selection, selection of updating/overwriting the ROOT file, use of event weights, and opening files through xrootd

--- src/libraries/AMPTOOLS_DATAIO/ROOTDataReader.cc
+++ src/libraries/AMPTOOLS_DATAIO/ROOTDataReader.cc
@@ -11,13 +11,18 @@
 #include "TTree.h"
 
 ROOTDataReader::ROOTDataReader( const string& inFileName,
-                               const string& inTreeName ) :
-m_eventCounter( 0 )
+				const string& inTreeName, bool useWeight) :
+  m_eventCounter( 0 ),
+  m_useWeight(useWeight)
 {
   
 	TH1::AddDirectory( kFALSE );
-	
-	m_inFile = new TFile( inFileName.c_str() );
+
+	//this way of opening files works with URLs of the form
+	// root://xrootdserver/path/to/myfile.root
+	m_inFile = TFile::Open( inFileName.c_str() ); 
+	//m_inFile = new TFile( inFileName.c_str() );
+
 	m_inTree = static_cast<TTree*>( m_inFile->Get( inTreeName.c_str() ) );
   
 	m_inTree->SetBranchAddress( "nPart", &m_nPart );
@@ -33,6 +38,10 @@ m_eventCounter( 0 )
 	m_inTree->SetBranchAddress( "pxRecoil", &m_pxRecoil );
 	m_inTree->SetBranchAddress( "pyRecoil", &m_pyRecoil );
 	m_inTree->SetBranchAddress( "pzRecoil", &m_pzRecoil );
+	if(m_inTree->GetBranch("weight") != NULL)
+	  m_inTree->SetBranchAddress( "weight", &m_weight );
+	else
+	  m_useWeight=false;
 }
 
 void
@@ -51,33 +60,32 @@ ROOTDataReader::getEvent()
 {
   if( m_eventCounter < static_cast< unsigned int >( m_inTree->GetEntries() ) ){
     //  if( m_eventCounter < 10 ){
- 		
-		m_inTree->GetEntry( m_eventCounter++ );
-		assert( m_nPart < Kinematics::kMaxParticles );
     
-		vector< HepLorentzVector > particleList;
-		
-    particleList.
-    push_back( HepLorentzVector( m_pxBeam, m_pyBeam, m_pzBeam, m_eBeam ) );
-    particleList.
-    push_back( HepLorentzVector( m_pxRecoil, m_pyRecoil, m_pzRecoil, m_eRecoil ) );
+    m_inTree->GetEntry( m_eventCounter++ );
+    assert( m_nPart < Kinematics::kMaxParticles );
     
-		for( int i = 0; i < m_nPart; ++i ){
-			
-			particleList.push_back( HepLorentzVector( m_px[i], m_py[i], m_pz[i], m_e[i] ) );
-		}
-		
-		return new Kinematics( particleList );
-	}
-	else{
-		
-		return NULL;
-	}
+    vector< HepLorentzVector > particleList;
+    
+    particleList.
+      push_back( HepLorentzVector( m_pxBeam, m_pyBeam, m_pzBeam, m_eBeam ) );
+    particleList.
+      push_back( HepLorentzVector( m_pxRecoil, m_pyRecoil, m_pzRecoil, m_eRecoil ) );
+    
+    for( int i = 0; i < m_nPart; ++i ){
+      
+      particleList.push_back( HepLorentzVector( m_px[i], m_py[i], m_pz[i], m_e[i] ) );
+    }
+    
+    return new Kinematics( particleList, m_useWeight ? m_weight : 1.0 );
+  }
+  else{
+    
+    return NULL;
+  }
 }
 
 unsigned int
 ROOTDataReader::numEvents() const
 {	
-  //  return 10;
   return static_cast< unsigned int >( m_inTree->GetEntries() );
 }
