d5bcf4ab11302513da030ed8de295833f3650d74
David Lawrence <davidl@jlab.org>
2012-04-25 11:40:50 +0000

    Added some convenience functions for extracting probabilities from DPiPlus and DPiMinus object's underlying tracks.

--- src/libraries/PID/DPiMinus.h
+++ src/libraries/PID/DPiMinus.h
@@ -11,25 +11,61 @@
 #include <JANA/JObject.h>
 #include <JANA/JFactory.h>
 #include <PID/DKinematicData.h>
+#include <TMath.h>
 
 class DPiMinus:public DKinematicData{
 	public:
 		JOBJECT_PUBLIC(DPiMinus);
 	
-		DPiMinus(const DKinematicData *kd):DKinematicData(*kd){}
+		DPiMinus(const DKinematicData *kd):DKinematicData(*kd),hypoth(NULL){}
+		DPiMinus(const DChargedTrackHypothesis *hypoth):DKinematicData(*hypoth),hypoth(hypoth){}
 		
 		/// This class is used to hold a copy of the reconstructed
 		/// objects for which the pi- hypothesis is the most probable. 
 		/// It should not be used for an in-depth physics analysis
 		/// because there may be other hypotheses for this track that
 		/// are nearly as probable.
-		
+
+		// The following are convienience method for accessing values in the 
+		// DChargedTrackHypothesis object on which this is based
+		double GetFOM(void) const {
+			if(!hypoth)return -1.0;
+			return (double)hypoth->dFOM;
+		}	
+	
+		double GetChiSq(void) const {
+			if(!hypoth)return -1.0;
+			return (double)hypoth->dChiSq;
+		}
+	
+		double GetNDF(void) const {
+			if(!hypoth)return -1.0;
+			return (double)hypoth->dNDF;
+		}	
+
+		double GetProb(void) const {
+			if(!hypoth)return -1.0;
+			return TMath::Prob((double)hypoth->dChiSq, (double)hypoth->dNDF);
+		}
+	
+		double GetProbTrack(void) const {
+			if(!hypoth)return -1.0;
+			return TMath::Prob((double)hypoth->dChiSq_Track, (double)hypoth->dNDF_Track);
+		}
+	
+		const DChargedTrackHypothesis* GetChargedTrackHypothesis(void) const {
+			return hypoth;
+		}
+	
 		// This method is used primarily for pretty printing
 		// the second argument to AddString is printf style format
 		void toStrings(vector<pair<string,string> > &items)const{
 			this->DKinematicData::toStrings(items);
 		}
 		
+	protected:
+		const DChargedTrackHypothesis *hypoth;
+	
 };
 
 #endif // _DPiMinus_
--- src/libraries/PID/DPiPlus.h
+++ src/libraries/PID/DPiPlus.h
@@ -11,12 +11,15 @@
 #include <JANA/JObject.h>
 #include <JANA/JFactory.h>
 #include <PID/DKinematicData.h>
+#include <PID/DChargedTrackHypothesis.h>
+#include <TMath.h>
 
 class DPiPlus:public DKinematicData{
 	public:
 		JOBJECT_PUBLIC(DPiPlus);
 	
-		DPiPlus(const DKinematicData *kd):DKinematicData(*kd){}
+		DPiPlus(const DKinematicData *kd):DKinematicData(*kd),hypoth(NULL){}
+		DPiPlus(const DChargedTrackHypothesis *hypoth):DKinematicData(*hypoth),hypoth(hypoth){}
 		
 		/// This class is used to hold a copy of the reconstructed
 		/// objects for which the pi+ hypothesis is the most probable. 
@@ -24,11 +27,45 @@ class DPiPlus:public DKinematicData{
 		/// because there may be other hypotheses for this track that
 		/// are nearly as probable.
 		
+		// The following are convienience method for accessing values in the 
+		// DChargedTrackHypothesis object on which this is based
+		double GetFOM(void) const {
+			if(!hypoth)return -1.0;
+			return (double)hypoth->dFOM;
+		}	
+		
+		double GetChiSq(void) const {
+			if(!hypoth)return -1.0;
+			return (double)hypoth->dChiSq;
+		}
+		
+		double GetNDF(void) const {
+			if(!hypoth)return -1.0;
+			return (double)hypoth->dNDF;
+		}	
+		
+		double GetProb(void) const {
+			if(!hypoth)return -1.0;
+			return TMath::Prob((double)hypoth->dChiSq, (double)hypoth->dNDF);
+		}
+	
+		double GetProbTrack(void) const {
+			if(!hypoth)return -1.0;
+			return TMath::Prob((double)hypoth->dChiSq_Track, (double)hypoth->dNDF_Track);
+		}
+		
+		const DChargedTrackHypothesis* GetChargedTrackHypothesis(void) const {
+			return hypoth;
+		}
+		
 		// This method is used primarily for pretty printing
 		// the second argument to AddString is printf style format
 		void toStrings(vector<pair<string,string> > &items)const{
 			this->DKinematicData::toStrings(items);
 		}
+
+	protected:
+		const DChargedTrackHypothesis *hypoth;
 		
 };
 
