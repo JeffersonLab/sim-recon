fd39ea062beb3634eeaa4e06eca04eec92b53bb7
William Levine <wilevine@jlab.org>
2012-06-21 20:45:00 +0000

    BCAL reconstruction: change z error calculation

--- src/libraries/BCAL/DBCALCluster.cc
+++ src/libraries/BCAL/DBCALCluster.cc
@@ -146,11 +146,8 @@ DBCALCluster::makeFromPoints(){
   m_sig_theta /= sqrt(n_eff2);*/
 
   // The method below for determining sig_theta works better than the one
-  // above. sigma_z is determined by fitting. sigma_z is supposed to be of
-  // the form a/sqrt(E), but this form fits the data better. This discrepancy
-  // is probably due to contributions from dark hits and incomplete
-  // gathering of hits to form a cluster.
-  double sigma_z = 1.17/sqrt(m_E)+.17/m_E;
+  // above. sigma_z is determined using errors when reconstructing MC data.
+  double sigma_z = sqrt(1.394*1.394/m_E + 0.859*0.859);
   m_sig_theta = sigma_z*sin(m_theta)*sin(m_theta)/DBCALGeometry::BCALINNERRAD;
   
   m_phi = atan2(sum_sin_phi,sum_cos_phi);
