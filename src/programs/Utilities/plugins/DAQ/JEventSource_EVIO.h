ff5dbc36c002a19b924ec79a8a1fb50b2725cf6e
David Lawrence <davidl@jlab.org>
2013-01-02 11:55:11 +0000

    Split F1TDC parser into two. One for the old style and one for the proposed style. Still has problems with initial mc2coda file so several debug messages are left in at the moment.

--- src/programs/Utilities/plugins/DAQ/JEventSource_EVIO.h
+++ src/programs/Utilities/plugins/DAQ/JEventSource_EVIO.h
@@ -179,6 +179,8 @@ class JEventSource_EVIO: public jana::JEventSource{
 		void Parsef250Bank(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
 		void Parsef125Bank(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
 		void ParseF1TDCBank(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
+		void ParseF1TDCBank_style1(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
+		void ParseF1TDCBank_style2(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
 		void ParseTSBank(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
 		void ParseTIBank(int32_t rocid, const uint32_t* &iptr, const uint32_t *iend, list<ObjList*> &events);
 
