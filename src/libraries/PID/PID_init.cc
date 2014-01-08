15c039749d8ef6a3a263af975769648481feabb2
Paul Mattione <pmatt@jlab.org>
2014-01-08 21:12:19 +0000

    Deleted deprecated classes/factories from PID library: DKinFit, DTwoGammaFit*, DVertexIndpendentResults, DPiPlus, DPiMinus, DParticleSet, DPhysicsEvent

--- src/libraries/PID/PID_init.cc
+++ src/libraries/PID/PID_init.cc
@@ -3,24 +3,15 @@
 #include <JANA/JEventLoop.h>
 using namespace jana;
 
-#include "DTwoGammaFit_factory.h"
-#include "DTwoGammaFit_factory_PI0.h"
-#include "DTwoGammaFit_factory_ETA.h"
 #include "DParticleID_factory.h"
 #include "DParticleID_factory_PID1.h"
 #include "DChargedTrack_factory.h"
 #include "DChargedTrackHypothesis_factory.h"
-#include "DChargedTruthMatch_factory.h"
 #include "DNeutralParticle_factory.h"
 #include "DNeutralParticleHypothesis_factory.h"
 #include "DNeutralShower_factory.h"
-#include "DVertexIndependentResults_factory.h"
 #include "DVertex_factory.h"
 #include "DVertex_factory_THROWN.h"
-#include "DPhysicsEvent_factory.h"
-#include "DParticleSet_factory.h"
-#include "DPiPlus_factory.h"
-#include "DPiMinus_factory.h"
 #include "DEventRFBunch_factory.h"
 #include "DEventRFBunch_factory_Thrown.h"
 #include "DDetectorMatches_factory.h"
@@ -38,25 +29,17 @@ typedef JFactory<DMCReaction> DMCReaction_factory;
 jerror_t PID_init(JEventLoop *loop)
 {
 	/// Create and register PID data factories
-	loop->AddFactory(new DTwoGammaFit_factory_PI0);
-	loop->AddFactory(new DTwoGammaFit_factory_ETA);
 	loop->AddFactory(new DBeamPhoton_factory);
 	loop->AddFactory(new DMCReaction_factory);
 	loop->AddFactory(new DParticleID_factory);
 	loop->AddFactory(new DParticleID_factory_PID1);
 	loop->AddFactory(new DChargedTrack_factory);
 	loop->AddFactory(new DChargedTrackHypothesis_factory);
-	loop->AddFactory(new DChargedTruthMatch_factory);
 	loop->AddFactory(new DNeutralParticle_factory);
 	loop->AddFactory(new DNeutralParticleHypothesis_factory);
 	loop->AddFactory(new DNeutralShower_factory);
-	loop->AddFactory(new DVertexIndependentResults_factory);
 	loop->AddFactory(new DVertex_factory);
 	loop->AddFactory(new DVertex_factory_THROWN);
-	loop->AddFactory(new DParticleSet_factory);
-	loop->AddFactory(new DPhysicsEvent_factory);
-	loop->AddFactory(new DPiPlus_factory);
-	loop->AddFactory(new DPiMinus_factory);
 	loop->AddFactory(new DEventRFBunch_factory);
 	loop->AddFactory(new DEventRFBunch_factory_Thrown);
 	loop->AddFactory(new DDetectorMatches_factory);
