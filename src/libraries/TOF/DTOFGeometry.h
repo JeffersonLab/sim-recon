e6b5c0d4eee7b129e5c84c9d9f8c797859022910
Benedikt Zihlmann <zihlmann@jlab.org>
2014-01-31 18:03:50 +0000

    modify the code to accomodate the changes in the TOF geometry with half width long paddles close to the beam hole. Note: it is necessary to update and recompile the geometry hdds!!!!!

--- src/libraries/TOF/DTOFGeometry.h
+++ src/libraries/TOF/DTOFGeometry.h
@@ -19,25 +19,23 @@ class DTOFGeometry:public JObject{
   JOBJECT_PUBLIC(DTOFGeometry);
 
   int NLONGBARS;        ///> number of long scintillator bars
+  int NWIDEBARS;        ///> number of long scintillator bars
+  int NBARS;            ///> number of long scintillator bars
   int NSHORTBARS;       ///> number of short scintillator bars
   float LONGBARLENGTH;  ///> length of the long scintillators
   float SHORTBARLENGTH; ///> length of the short scintillators
   float BARWIDTH;       ///> width of the scintillator bars
-
-    
+  float YPOS[50];       ///> y position for bar number
+  
   float bar2y(int bar, int orientation)  const ///> convert bar number to the
   ///> position of the center of the
   ///> bar in local coordinations
   {
     float y;
-    if (bar <= NLONGBARS){
-      y = ((NLONGBARS/2.0)+(NSHORTBARS/4.0)-(bar-0.5))*BARWIDTH;
-      if (bar > (NLONGBARS/2)) y -= (NSHORTBARS/2.0)*BARWIDTH;
-    }
-    else{
-      y = ((NSHORTBARS/4.0)-(((bar-NLONGBARS+1)/2)-0.5))*BARWIDTH;
-    }
+    y = YPOS[bar];
+
     if (orientation == 0) y *= -1.0;
+
     return y;
   }
   
@@ -45,25 +43,49 @@ class DTOFGeometry:public JObject{
   int y2bar(float x, float y, int orientation) const   ///> convert local x,y to 
   ///> bar number
   {
+    int N1 = NWIDEBARS/2;
+    int N2 = (NLONGBARS - NWIDEBARS )/2;
+    int N3 = NSHORTBARS/4;
+    int N4 = (NLONGBARS - NWIDEBARS )/2;
+    int N5 = NWIDEBARS/2;
     int bar;
     if (orientation == 0) {float temp = y; y = -1.0*x; x = temp;}
-    if (fabs(y) < (NSHORTBARS/4.0)*BARWIDTH){
-      bar = NLONGBARS + (int)((NSHORTBARS/4.0*BARWIDTH-y)/BARWIDTH)*2 + 1;
-      if (x > 0) bar++;
+
+    if (fabs(y)<N3*BARWIDTH){
+      
+      if (x>0) {
+	bar = N1+N2+N3+N4+N5 + (int)((y+N3*BARWIDTH)/BARWIDTH) + 1;
+      } else {
+	bar = N1 + N2 + (int)((y+N3*BARWIDTH)/BARWIDTH) ;
+      }
+      
+    } else if (fabs(y)<(N3*BARWIDTH + N2*BARWIDTH/2.)){
+      if (y<0){
+	bar = N1+N2 + ((int)((y + N3*BARWIDTH)/(BARWIDTH/2.))-1);
+      } else {
+	bar = N1+N2+N3+N3 + (int)((y - N3*BARWIDTH)/(BARWIDTH/2.));
+      }
+      
+    } else { // if (fabs(y)<(N1*BARWIDTH+N2*BARWIDTH/2.+N3*BARWIDTH)){
+      if (y<0){
+	bar = N1 + ((int)((y + N2*BARWIDTH/2 + N3*BARWIDTH)/BARWIDTH)-1);
+      } else {
+	bar = N1+N2+N3+N3+N4 + ((int)((y - N2*BARWIDTH/2 - N3*BARWIDTH)/BARWIDTH));
+      }
     }
-    else{
-      bar = (int)(((NLONGBARS/2.0+NSHORTBARS/4.0)*BARWIDTH-y)/BARWIDTH) + 1;
-      if (bar > NLONGBARS/2) bar -= NSHORTBARS/2;
-    }
+    
+
     return bar;
   }
 
 		void toStrings(vector<pair<string,string> > &items)const{
-			AddString(items, "", "%d", NLONGBARS);
-			AddString(items, "", "%d", NSHORTBARS);
-			AddString(items, "", "%6.3f", LONGBARLENGTH);
-			AddString(items, "", "%6.3f", SHORTBARLENGTH);
-			AddString(items, "", "%6.3f", BARWIDTH);
+			AddString(items, "NBARS", "%d", NBARS);
+			AddString(items, "NLONGBARS", "%d", NLONGBARS);
+			AddString(items, "NWIDEBARS", "%d", NWIDEBARS);
+			AddString(items, "NSHORTBARS", "%d", NSHORTBARS);
+			AddString(items, "LONGBARLENGTH", "%6.3f", LONGBARLENGTH);
+			AddString(items, "SHORTBARLENGTH", "%6.3f", SHORTBARLENGTH);
+			AddString(items, "BARWIDTH", "%6.3f", BARWIDTH);
 		}
 };
 
--- src/libraries/TOF/DTOFGeometry_factory.cc
+++ src/libraries/TOF/DTOFGeometry_factory.cc
@@ -14,12 +14,39 @@ jerror_t DTOFGeometry_factory::init(void)
 
   DTOFGeometry *myDTOFGeometry = new DTOFGeometry;
 
-  myDTOFGeometry->NLONGBARS        = 40;
+  myDTOFGeometry->NLONGBARS        = 44;
+  myDTOFGeometry->NWIDEBARS        = 36;
   myDTOFGeometry->NSHORTBARS       = 4;
   myDTOFGeometry->LONGBARLENGTH    = 252.0;
   myDTOFGeometry->SHORTBARLENGTH   = 120.0;
   myDTOFGeometry->BARWIDTH         = 6.0;
-  
+  myDTOFGeometry->NBARS            = myDTOFGeometry->NLONGBARS + myDTOFGeometry->NSHORTBARS;
+
+  for (int k=0;k<myDTOFGeometry->NBARS;k++){
+    for (int k=0;k<18;k++){
+      myDTOFGeometry->YPOS[k] = -123.0 + k*myDTOFGeometry->BARWIDTH;
+    }
+    for (int k=18;k<22;k++){
+      myDTOFGeometry->YPOS[k] = myDTOFGeometry->YPOS[17] + (k-17+0.5)*myDTOFGeometry->BARWIDTH/2.;
+    }
+    for (int k=22;k<24;k++){
+      myDTOFGeometry->YPOS[k] = myDTOFGeometry->YPOS[21] + 0.5*myDTOFGeometry->BARWIDTH/2. +
+	(k-21-0.5)*myDTOFGeometry->BARWIDTH;
+    }
+    for (int k=24;k<28;k++){
+      myDTOFGeometry->YPOS[k] = myDTOFGeometry->YPOS[23] + 0.5*myDTOFGeometry->BARWIDTH +
+	(k-23-0.5)*myDTOFGeometry->BARWIDTH/2.;
+    }
+    for (int k=28;k<46;k++){
+      myDTOFGeometry->YPOS[k] = myDTOFGeometry->YPOS[27] + 0.5*myDTOFGeometry->BARWIDTH/2. +
+	(k-27-0.5)*myDTOFGeometry->BARWIDTH;
+    }
+    for (int k=46;k<48;k++){
+      myDTOFGeometry->YPOS[k] = -3. + (k-46)*myDTOFGeometry->BARWIDTH;
+    }
+
+  }
+
   _data.push_back(myDTOFGeometry);
     
   return NOERROR;
