85ee2c0d7af90e7be52d24a528aacac3978c0d0b
Simon Taylor <staylor@jlab.org>
2014-02-05 12:55:58 +0000

    B=0 field map

---
+++ src/libraries/HDGEOMETRY/DMagneticFieldMapNoField.cc
@@ -0,0 +1,34 @@
+// $Id$
+//
+//    File: DMagneticFieldMapNoField.cc
+
+
+using namespace std;
+
+#include "DMagneticFieldMapNoField.h"
+
+
+
+//---------------------------------
+// DMagneticFieldMapNoField    (Constructor)
+//---------------------------------
+DMagneticFieldMapNoField::DMagneticFieldMapNoField(JApplication *japp, string namepath)
+{
+
+}
+
+//---------------------------------
+// DMagneticFieldMapNoField    (Constructor)
+//---------------------------------
+DMagneticFieldMapNoField::DMagneticFieldMapNoField(JCalibration *jcalib, string namepath)
+{
+
+}
+
+//---------------------------------
+// ~DMagneticFieldMapNoField    (Destructor)
+//---------------------------------
+DMagneticFieldMapNoField::~DMagneticFieldMapNoField()
+{
+
+}
