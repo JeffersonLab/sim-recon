89bddf985e9d8e5f32b0a0ad178153507f6de619
Mark-Macrae Dalton <dalton@jlab.org>
2014-02-14 14:23:18 +0000

    Included code to produce a tree from a Df250PulseIntegral object. Currently this tree would not include any functional information.
    Minor improvements to Doxygen documentation.

--- src/programs/Analysis/plugins/DAQTree/JEventProcessor_DAQTree.cc
+++ src/programs/Analysis/plugins/DAQTree/JEventProcessor_DAQTree.cc
@@ -13,6 +13,7 @@ using namespace jana;
 
 #include <stdint.h>
 #include <DAQ/Df250WindowRawData.h>
+#include <DAQ/Df250PulseIntegral.h>
 
 
 bool Df250WindowRawData_cmp(const Df250WindowRawData *a,const Df250WindowRawData *b){
@@ -62,7 +63,7 @@ jerror_t JEventProcessor_DAQTree::init(void)
 	// japp->RootUnLock();
 	//
 
-
+	printf("Welcome to JEventProcessor_DAQTree\n");
 
 	japp->RootWriteLock();
 
@@ -81,6 +82,12 @@ jerror_t JEventProcessor_DAQTree::init(void)
 	Df250WindowRawData_tree->Branch("w_min",&w_min,"w_min/i");
 	Df250WindowRawData_tree->Branch("w_max",&w_max,"w_max/i");
 	Df250WindowRawData_tree->Branch("w_samp1",&w_samp1,"w_samp1/i");
+
+	/// Trees are created
+	Df250PulseIntegral_tree = new TTree("Df250PulseIntegral",
+										"tree of flash 250 pulse integral for each channel and event");
+	Df250PulseIntegral_tree->Branch("channelnum",&channelnum,"channelnum/i");
+	Df250PulseIntegral_tree->Branch("eventnum",&eventnum,"eventnum/i");
 
 	japp->RootUnLock();
 	return NOERROR;
@@ -112,7 +119,7 @@ jerror_t JEventProcessor_DAQTree::evnt(JEventLoop *loop, int eventnumber)
 	japp->RootWriteLock();
 	eventnum = eventnumber;
 
-	// Get a vector of objects for this event (1 object for each crate/slot/channel)
+	// Get a vector of Df250WindowRawData objects for this event (1 object for each crate/slot/channel)
 	vector<const Df250WindowRawData*> f250WindowRawData_vec;
 	loop->Get(f250WindowRawData_vec);
 	sort(f250WindowRawData_vec.begin(), f250WindowRawData_vec.end(), Df250WindowRawData_cmp);
@@ -133,7 +140,7 @@ jerror_t JEventProcessor_DAQTree::evnt(JEventLoop *loop, int eventnumber)
 		nsamples=samplesvector.size();
 		// loop over the samples to calculate integral, min, max
 		for (uint16_t c_samp=0; c_samp<nsamples; c_samp++) {
-			waveform.push_back(samplesvector[c_samp]);
+			waveform.push_back(samplesvector[c_samp]); // push the sample into the waveform vector
 			if (c_samp==0) {  // use first sample for initialization
 				w_integral = samplesvector[0]; 
 				w_min = samplesvector[0];
@@ -147,6 +154,17 @@ jerror_t JEventProcessor_DAQTree::evnt(JEventLoop *loop, int eventnumber)
 		}
 		Df250WindowRawData_tree->Fill();
 	}
+
+	// Get a vector of Df250WindowRawData objects for this event (1 object for each crate/slot/channel)
+	vector<const Df250PulseIntegral*> f250PulseIntegral_vec;
+	loop->Get(f250PulseIntegral_vec);
+	Nchannels = f250PulseIntegral_vec.size();
+	// Loop over all channels in this event
+	for(unsigned int c_chan=0; c_chan<Nchannels; c_chan++){
+		channelnum = c_chan;
+		Df250PulseIntegral_tree->Fill();
+	}
+
 	japp->RootUnLock();
 	
 	return NOERROR;
