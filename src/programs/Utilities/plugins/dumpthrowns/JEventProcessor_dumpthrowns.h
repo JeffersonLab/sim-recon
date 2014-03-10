170706f1de1f1ea6e6be208846c9158c9e0631c2
David Lawrence <davidl@jlab.org>
2014-03-10 13:34:25 +0000

    Added dumpthrowns plugin and modified dumpcandidates to swim candidate back to the beam line and put POCA values in output file.

--- src/programs/Utilities/plugins/dumpthrowns/JEventProcessor_dumpthrowns.h
+++ src/programs/Utilities/plugins/dumpthrowns/JEventProcessor_dumpthrowns.h
@@ -1,12 +1,12 @@
 // $Id$
 //
-//    File: JEventProcessor_dumpcandidates.h
+//    File: JEventProcessor_dumpthrowns.h
 // Created: Tue Feb  4 09:29:35 EST 2014
 // Creator: davidl (on Linux ifarm1102 2.6.32-220.7.1.el6.x86_64 x86_64)
 //
 
-#ifndef _JEventProcessor_dumpcandidates_
-#define _JEventProcessor_dumpcandidates_
+#ifndef _JEventProcessor_dumpthrowns_
+#define _JEventProcessor_dumpthrowns_
 
 #include <map>
 #include <fstream>
@@ -15,11 +15,11 @@ using namespace std;
 #include <JANA/JEventProcessor.h>
 #include <HDGEOMETRY/DGeometry.h>
 
-class JEventProcessor_dumpcandidates:public jana::JEventProcessor{
+class JEventProcessor_dumpthrowns:public jana::JEventProcessor{
 	public:
-		JEventProcessor_dumpcandidates();
-		~JEventProcessor_dumpcandidates();
-		const char* className(void){return "JEventProcessor_dumpcandidates";}
+		JEventProcessor_dumpthrowns();
+		~JEventProcessor_dumpthrowns();
+		const char* className(void){return "JEventProcessor_dumpthrowns";}
 
 	private:
 		jerror_t init(void);						///< Called once at program start.
@@ -35,13 +35,11 @@ class JEventProcessor_dumpcandidates:public jana::JEventProcessor{
 		unsigned int MAX_CANDIDATE_FILTER;
 		unsigned long events_written;
 		unsigned long events_discarded;
-		DVector3 beam_origin;
-		DVector3 beam_dir;
 		
 		unsigned long GetCDCWireID(const DCDCWire* w){ return w->ring*100 + w->straw;}
 		unsigned long GetFDCWireID(const DFDCWire* w){ return 100000 + w->layer*100 + w->wire;}
 		int GetWireIndex(unsigned long id){ return wireID[id]; }
 };
 
-#endif // _JEventProcessor_dumpcandidates_
+#endif // _JEventProcessor_dumpthrowns_
 
