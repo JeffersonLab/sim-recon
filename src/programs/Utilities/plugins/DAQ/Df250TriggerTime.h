798e681c692eefe6ff6d75c02192f0ace8e76795
David Lawrence <davidl@jlab.org>
2014-04-10 17:49:24 +0000

    Remove the itrigger member of Df250TriggerTime since it inherits this from the DDAQAddress base class.

--- src/programs/Utilities/plugins/DAQ/Df250TriggerTime.h
+++ src/programs/Utilities/plugins/DAQ/Df250TriggerTime.h
@@ -18,10 +18,9 @@ class Df250TriggerTime:public DDAQAddress{
 	public:
 		JOBJECT_PUBLIC(Df250TriggerTime);
 
-		Df250TriggerTime():DDAQAddress(0, 0, 0),itrigger(0),time(0){}
+		Df250TriggerTime():DDAQAddress(0, 0, 0, 0),time(0){}
 		Df250TriggerTime(uint32_t rocid, uint32_t slot, uint32_t itrigger, uint64_t time):DDAQAddress(rocid, slot, 0, itrigger),time(time){}
 		
-		uint32_t itrigger;       // from Event Header
 		uint64_t time;           // from Trigger Time words
 		
 	
