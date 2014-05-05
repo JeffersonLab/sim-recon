632cae9f72e47c87834ab72d6ec5fb65982503c9
David Lawrence <davidl@jlab.org>
2014-05-05 20:26:41 +0000

    Plugin that gives same info. as DAQTree, but dedicated to BCAL by adding the module, layer, sector, end values.

---
+++ src/programs/Analysis/plugins/DAQTreeBCAL/JEventProcessor_DAQTreeBCAL.cc
@@ -0,0 +1,167 @@
+// $Id$
+//
+//    File: JEventProcessor_DAQTreeBCAL.cc
+// Created: Mon May  5 15:20:49 EDT 2014
+// Creator: davidl (on Darwin harriet.jlab.org 13.1.0 i386)
+//
+
+#include <iostream>
+using namespace std;
+
+#include "JEventProcessor_DAQTreeBCAL.h"
+using namespace jana;
+
+#include <BCAL/DBCALDigiHit.h>
+#include <DAQ/Df250PulseIntegral.h>
+#include <DAQ/Df250WindowRawData.h>
+
+
+// Routine used to create our JEventProcessor
+#include <JANA/JApplication.h>
+#include <JANA/JFactory.h>
+extern "C"{
+void InitPlugin(JApplication *app){
+	InitJANAPlugin(app);
+	app->AddProcessor(new JEventProcessor_DAQTreeBCAL());
+}
+} // "C"
+
+
+//------------------
+// JEventProcessor_DAQTreeBCAL (Constructor)
+//------------------
+JEventProcessor_DAQTreeBCAL::JEventProcessor_DAQTreeBCAL()
+{
+
+}
+
+//------------------
+// ~JEventProcessor_DAQTreeBCAL (Destructor)
+//------------------
+JEventProcessor_DAQTreeBCAL::~JEventProcessor_DAQTreeBCAL()
+{
+
+}
+
+//------------------
+// init
+//------------------
+jerror_t JEventProcessor_DAQTreeBCAL::init(void)
+{
+	BCALdigi = new TTree("BCALdigi","DBCALDigiHit objects (w/ waveform samples) for each channel and event");
+	BCALdigi->Branch("channelnum",&channelnum,"channelnum/i");
+	BCALdigi->Branch("eventnum",&eventnum,"eventnum/i");
+	BCALdigi->Branch("rocid",&rocid,"rocid/i");
+	BCALdigi->Branch("slot",&slot,"slot/i");
+	BCALdigi->Branch("channel",&channel,"channel/i");
+	BCALdigi->Branch("itrigger",&itrigger,"itrigger/i");
+	BCALdigi->Branch("waveform",&waveform);
+	BCALdigi->Branch("nsamples",&nsamples,"nsamples/i");
+	BCALdigi->Branch("w_integral",&w_integral,"w_integral/i");
+	BCALdigi->Branch("w_min",&w_min,"w_min/i");
+	BCALdigi->Branch("w_max",&w_max,"w_max/i");
+	BCALdigi->Branch("w_samp1",&w_samp1,"w_samp1/i");
+
+	BCALdigi->Branch("module",&module,"module/i");
+	BCALdigi->Branch("layer",&layer,"layer/i");
+	BCALdigi->Branch("sector",&sector,"sector/i");
+	BCALdigi->Branch("end",&end,"end/i");
+
+	return NOERROR;
+}
+
+//------------------
+// brun
+//------------------
+jerror_t JEventProcessor_DAQTreeBCAL::brun(JEventLoop *eventLoop, int runnumber)
+{
+	// This is called whenever the run number changes
+	return NOERROR;
+}
+
+//------------------
+// evnt
+//------------------
+jerror_t JEventProcessor_DAQTreeBCAL::evnt(JEventLoop *loop, int eventnumber)
+{
+	eventnum = eventnumber;
+
+	// Get the DBCALDigiHit objects
+	vector<const DBCALDigiHit*> bcaldigihits;
+	loop->Get(bcaldigihits);
+
+	// Loop over DBCALDigiHit objects
+	for(unsigned int i=0; i< bcaldigihits.size(); i++){
+
+		// Get Df250PulseIntegral and Df250WindowRawData objects
+		// associated with this. If either fails then an exeption
+		// is thrown (and caught) and the hit is ignored.
+		try {
+			const DBCALDigiHit *bcaldigihit = bcaldigihits[i];
+			const Df250PulseIntegral *pulseintegral;
+			const Df250WindowRawData *windorawdata;
+			bcaldigihit->GetSingle(pulseintegral);
+			pulseintegral->GetSingle(windorawdata);
+			
+			waveform.clear();
+			channelnum = i;
+			rocid = windorawdata->rocid;
+			slot = windorawdata->slot;
+			channel = windorawdata->channel;
+			itrigger = windorawdata->itrigger;
+
+			// Get a vector of the samples for this channel
+			const vector<uint16_t> &samplesvector = windorawdata->samples;
+			nsamples=samplesvector.size();
+
+			// loop over the samples to calculate integral, min, max
+			for (uint16_t c_samp=0; c_samp<nsamples; c_samp++) {
+				waveform.push_back(samplesvector[c_samp]); // push the sample into the waveform vector
+				if (c_samp==0) {  // use first sample for initialization
+					w_integral = samplesvector[0]; 
+					w_min = samplesvector[0];
+					w_max = samplesvector[0];
+					w_samp1 = samplesvector[0];
+				} else {
+					w_integral += samplesvector[c_samp];
+					if (w_min > samplesvector[c_samp]) w_min = samplesvector[c_samp];
+					if (w_max < samplesvector[c_samp]) w_max = samplesvector[c_samp];
+				}
+			}
+			
+			module = bcaldigihit->module;
+			layer = bcaldigihit->layer;
+			sector = bcaldigihit->sector;
+			end = bcaldigihit->end;
+
+
+			// Fill tree
+			BCALdigi->Fill();
+
+		} catch (...) {}
+	}
+
+
+	return NOERROR;
+}
+
+//------------------
+// erun
+//------------------
+jerror_t JEventProcessor_DAQTreeBCAL::erun(void)
+{
+	// This is called whenever the run number changes, before it is
+	// changed to give you a chance to clean up before processing
+	// events from the next run number.
+	return NOERROR;
+}
+
+//------------------
+// fini
+//------------------
+jerror_t JEventProcessor_DAQTreeBCAL::fini(void)
+{
+	// Called before program exit after event processing is finished.
+	return NOERROR;
+}
+
---
+++ src/programs/Analysis/plugins/DAQTreeBCAL/JEventProcessor_DAQTreeBCAL.h
@@ -0,0 +1,50 @@
+// $Id$
+//
+//    File: JEventProcessor_DAQTreeBCAL.h
+// Created: Mon May  5 15:20:49 EDT 2014
+// Creator: davidl (on Darwin harriet.jlab.org 13.1.0 i386)
+//
+
+#ifndef _JEventProcessor_DAQTreeBCAL_
+#define _JEventProcessor_DAQTreeBCAL_
+
+#include <JANA/JEventProcessor.h>
+
+#include <TTree.h>
+
+
+class JEventProcessor_DAQTreeBCAL:public jana::JEventProcessor{
+	public:
+		JEventProcessor_DAQTreeBCAL();
+		~JEventProcessor_DAQTreeBCAL();
+		const char* className(void){return "JEventProcessor_DAQTreeBCAL";}
+
+		TTree *BCALdigi;
+		uint32_t channelnum;         ///< Arbitrary global channel number (sorted by crate, slot, channel)
+		uint32_t eventnum;	         ///< Event number	
+		uint32_t rocid;              ///< (from DDAQAddress) Crate number
+		uint32_t slot;               ///< (from DDAQAddress) Slot number in crate
+		uint32_t channel;            ///< (from DDAQAddress) Channel number in slot
+		uint32_t itrigger;           ///< (from DDAQAddress) Trigger number for cases when this hit was read in a multi-event block (from DDAQAddress)
+		vector<uint32_t> waveform;   ///< STL vector of samples in the waveform for the event
+		uint32_t nsamples;           ///< Number of samples in the waveform
+		uint32_t w_integral;         ///< Sum of all samples in the waveform
+		uint32_t w_min;              ///< Minimum sample in the waveform
+		uint32_t w_max;              ///< Maximum sample in the waveform
+		uint32_t w_samp1;            ///< First sample in the waveform  (for simple analysis in case the STL vector is difficult to access)
+
+		uint32_t module;
+		uint32_t layer;
+		uint32_t sector;
+		uint32_t end;  // 0=upstream, 1=downstream
+
+	private:
+		jerror_t init(void);						///< Called once at program start.
+		jerror_t brun(jana::JEventLoop *eventLoop, int runnumber);	///< Called everytime a new run number is detected.
+		jerror_t evnt(jana::JEventLoop *eventLoop, int eventnumber);	///< Called every event.
+		jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
+		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+};
+
+#endif // _JEventProcessor_DAQTreeBCAL_
+
