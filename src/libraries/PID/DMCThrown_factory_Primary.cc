38f0d49982b36ce579a9c76d4292c131f3966894
Paul Mattione <pmatt@jlab.org>
2014-05-06 21:31:56 +0000

    Forgot to check whether parent particle is resonance (now fixed).

--- src/libraries/PID/DMCThrown_factory_Primary.cc
+++ src/libraries/PID/DMCThrown_factory_Primary.cc
@@ -35,13 +35,13 @@ jerror_t DMCThrown_factory_Primary::evnt(jana::JEventLoop *locEventLoop, int eve
 
 	for(size_t loc_i = 0; loc_i < locMCThrowns.size(); ++loc_i)
 	{
-		int locParentID = locMCThrowns[loc_i]->parentid;
-
 		Particle_t locPID = locMCThrowns[loc_i]->PID();
 		if(locPID == Unknown)
 			continue; //unknown particle
 		if(IsResonance(locPID))
 			continue;
+
+		int locParentID = locMCThrowns[loc_i]->parentid;
 
 		// check if initial particle
 		if(locParentID == 0)
@@ -51,20 +51,22 @@ jerror_t DMCThrown_factory_Primary::evnt(jana::JEventLoop *locEventLoop, int eve
 			continue;
 		}
 
-		// if non-initial particle, see if parent is unknown and present
-		bool locIsParentUnknown = false;
+		// if non-initial particle, see if parent is unknown/resonance and present
+		bool locIsParentResonanceOrUnknown = false;
 		bool locIsParentFoundFlag = false;
 		for(size_t loc_j = 0; loc_j < locMCThrowns.size(); ++loc_j)
 		{
 			if(locMCThrowns[loc_j]->myid != locParentID)
 				continue;
 			locIsParentFoundFlag = true;
-			locIsParentUnknown = (locMCThrowns[loc_j]->PID() == Unknown);
+			Particle_t locParentPID = locMCThrowns[loc_j]->PID();
+			if((locParentPID == Unknown) || IsResonance(locParentPID))
+				locIsParentResonanceOrUnknown = true;
 			break;
 		}
 
-		if((!locIsParentFoundFlag) || (!locIsParentUnknown))
-			continue; // either parent is not found (e.g. came from shower in bcal) or the parent is not unknown (e.g. came from pi0 decay)
+		if((!locIsParentFoundFlag) || (!locIsParentResonanceOrUnknown))
+			continue; // either parent is not found (e.g. came from shower in bcal) or the parent is not unknown/resonance (e.g. came from pi0 decay)
 
 		//parent is found and is unknown: this was a primary particle
 		DMCThrown* locMCThrown = new DMCThrown(*locMCThrowns[loc_i]);
