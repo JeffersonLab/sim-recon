0ccb1fe69b2365ef1ba06eee70ca1a83fbbc84c3
William Levine <wilevine@jlab.org>
2014-05-29 21:19:17 +0000

    Add more info to DBCALTruthShower

--- src/libraries/BCAL/DBCALTruthShower.h
+++ src/libraries/BCAL/DBCALTruthShower.h
@@ -10,21 +10,27 @@ class DBCALTruthShower:public JObject{
 	public:
 		JOBJECT_PUBLIC(DBCALTruthShower);
 
-		int track;
+		int track; ///< This is the unique number that GEANT has assigned the particle
+		int ptype; ///< This is the particle ID number
 		int primary;
 		float phi;
 		float r;
 		float z;
 		float t;
 		float E;
+		float px;
+		float py;
+		float pz;
 
 		void toStrings(vector<pair<string,string> > &items)const{
+			AddString(items, "ptype", "%d", ptype);
 			AddString(items, "track", "%d", track);
 			AddString(items, "primary", "%d", primary);
 			AddString(items, "phi", "%1.3f", phi);
 			AddString(items, "r", "%4.3f", r);
 			AddString(items, "z", "%4.1f", z);
 			AddString(items, "t", "%4.3f", t);
+			AddString(items, "p", "%4.3f", sqrt(px*px + py*py + pz*pz));
 			AddString(items, "E", "%4.3f", E);
 		}
 };
