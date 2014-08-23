280eb8b1dbc341d7052eac5cbe21e017df25251d
Richard Jones <jonesrt@jlab.org>
2014-08-23 22:42:13 +0000

    * DEventSourceHDDMGenerator.cc, DEventSourceRESTGenerator.cc [rtj]    -Removed the code in CheckOpenable() that reads the header from the input     file to check what kind of hddm stream it is, whether a pure mcgen file,     a simulated hits file, a rest file, or whatever. That code worked for     regular files, but if the input stream is a pipe then the CheckOpenable()     method would eat the file header, and leave the stream without a header     when a DEventSource constructor comes along later to open it for reading.     If we want to accomodate reading from pipes, the best we can do is to     guess which variety of input hddm stream it is based on the filename.     That is what the revised versions of these CheckOpenable() methods do.

--- src/libraries/HDDM/DEventSourceHDDMGenerator.cc
+++ src/libraries/HDDM/DEventSourceHDDMGenerator.cc
@@ -17,7 +17,7 @@ using std::string;
 //---------------------------------
 const char* DEventSourceHDDMGenerator::Description(void)
 {
-	return "HDDM";
+   return "HDDM";
 }
 
 //---------------------------------
@@ -25,23 +25,47 @@ const char* DEventSourceHDDMGenerator::Description(void)
 //---------------------------------
 double DEventSourceHDDMGenerator::CheckOpenable(string source)
 {
-	// Restrict HDDM files to having a ".hddm" suffix
-	string suffix = ".hddm";
-	if(source.length() < suffix.length()) return 0.0;
-	if(source.substr(source.length() - suffix.length()) != suffix) return 0.0;
+   // Restrict HDDM files to having a ".hddm" suffix
+   string suffix = ".hddm";
+   if (source.length() < suffix.length())
+      return 0.0;
+   if (source.substr(source.length() - suffix.length()) != suffix)
+      return 0.0;
 
-	ifstream ifs(source.c_str());
-        if (!ifs.good()) {
-           return 0.0;
-        }
-        try {
-        	hddm_s::istream fin(ifs);
-        }
-        catch (std::runtime_error err) {
-                //std::cerr << err.what() << std::endl;
-		return 0.0;
-        }
-	return 1.0;
+   ifstream ifs(source.c_str());
+   if (! ifs.good()) {
+      return 0.0;
+   }
+
+   // At this point we know we can open the input stream for reading,
+   // but we cannot be sure that the contents are actually a valid
+   // hddm class "s" data stream. Problem is, if we read the first
+   // few bytes from the stream to check for the hddm header string,
+   // and is coming from a pipe, then we discard forever the header
+   // bytes and the stream will not be valid if later we decide to
+   // actually open a DEventSourceHDDM object on it. The best we
+   // can do is to look for clues in the filename and guess.
+
+   if (source.find("_rest") != source.npos ||
+       source.find("rest_") != source.npos)
+   {
+      return 0.10;
+   }
+   else if (source.find("hdgeant") != source.npos)
+   {
+      return 0.90;
+   }
+   else if (source.find("_smeared") != source.npos ||
+            source.find("smeared_") != source.npos)
+   {
+      return 0.85;
+   }
+   else if (source.find("_events") != source.npos ||
+            source.find("events_") != source.npos)
+   {
+      return 0.75;
+   }
+   return 0.5;
 }
 
 //---------------------------------
@@ -49,7 +73,5 @@ double DEventSourceHDDMGenerator::CheckOpenable(string source)
 //---------------------------------
 JEventSource* DEventSourceHDDMGenerator::MakeJEventSource(string source)
 {
-	return new DEventSourceHDDM(source.c_str());
+   return new DEventSourceHDDM(source.c_str());
 }
-		
-
--- src/libraries/HDDM/DEventSourceRESTGenerator.cc
+++ src/libraries/HDDM/DEventSourceRESTGenerator.cc
@@ -16,7 +16,7 @@ using std::string;
 //---------------------------------
 const char* DEventSourceRESTGenerator::Description(void)
 {
-	return "REST";
+   return "REST";
 }
 
 //---------------------------------
@@ -24,23 +24,45 @@ const char* DEventSourceRESTGenerator::Description(void)
 //---------------------------------
 double DEventSourceRESTGenerator::CheckOpenable(std::string source)
 {
-	// Restrict HDDM files to having a ".hddm" suffix
-	string suffix = ".hddm";
-	if(source.length() < suffix.length()) return 0.0;
-	if(source.substr(source.length() - suffix.length()) != suffix) return 0.0;
-	
-	ifstream ifs(source.c_str());
-   	if (!ifs.good()) {
-		return 0.0;
-	}
-	try {
-		hddm_r::istream fin(ifs);
-	}
-        catch (std::runtime_error err) {
-                //std::cerr << err.what() << std::endl;
-		return 0.0;
-        }
-	return 1.0;
+   // Restrict HDDM files to having a ".hddm" suffix
+   string suffix = ".hddm";
+   if(source.length() < suffix.length()) return 0.0;
+   if(source.substr(source.length() - suffix.length()) != suffix) return 0.0;
+   
+   ifstream ifs(source.c_str());
+      if (!ifs.good()) {
+      return 0.0;
+   }
+   //
+   // At this point we know we can open the input stream for reading,
+   // but we cannot be sure that the contents are actually a valid
+   // hddm class "s" data stream. Problem is, if we read the first
+   // few bytes from the stream to check for the hddm header string,
+   // and is coming from a pipe, then we discard forever the header
+   // bytes and the stream will not be valid if later we decide to
+   // actually open a DEventSourceREST object on it. The best we
+   // can do is to look for clues in the filename and guess.
+
+   if (source.find("_rest") != source.npos ||
+       source.find("rest_") != source.npos)
+   {
+      return 0.90;
+   }
+   else if (source.find("hdgeant") != source.npos)
+   {
+      return 0.10;
+   }
+   else if (source.find("_smeared") != source.npos ||
+            source.find("smeared_") != source.npos)
+   {
+      return 0.15;
+   }
+   else if (source.find("_events") != source.npos ||
+            source.find("events_") != source.npos)
+   {
+      return 0.25;
+   }
+   return 0.5;
 }
 
 //---------------------------------
@@ -48,5 +70,5 @@ double DEventSourceRESTGenerator::CheckOpenable(std::string source)
 //---------------------------------
 JEventSource* DEventSourceRESTGenerator::MakeJEventSource(std::string source)
 {
-	return new DEventSourceREST(source.c_str());
+   return new DEventSourceREST(source.c_str());
 }
