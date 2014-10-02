dd17b31721b2ae472fa9ffb0c3546f0e8b01f4cc
Paul Mattione <pmatt@jlab.org>
2014-10-02 03:42:34 +0000

    Add histogram macros for RootSpy.

---
+++ src/programs/Analysis/plugins/monitoring_hists/HistMacro_TrackMultiplicity.C
@@ -0,0 +1,27 @@
+// hnamepath: /Independent/Hist_TrackMultiplicity/NumGoodReconstructedParticles
+
+{
+	//Goto Path
+	TDirectory *locDirectory = (TDirectory*)gDirectory->FindObjectAny("Hist_TrackMultiplicity");
+	if(!locDirectory)
+		return;
+	locDirectory->cd();
+
+	//Get Histograms
+	TH2D* locHist = (TH2D*)gDirectory->Get("NumGoodReconstructedParticles");
+
+	//Get/Make Canvas
+	TCanvas *locCanvas = NULL;
+	if(TVirtualPad::Pad() == NULL)
+		locCanvas = new TCanvas("TrackMultiplicity"); //for testing
+	else
+		locCanvas = gPad->GetCanvas();
+
+	//Draw
+	locCanvas->cd(1);
+	gPad->SetTicks();
+	gPad->SetGrid();
+	if(locHist != NULL)
+		locHist->Draw("COLZ");
+}
+
