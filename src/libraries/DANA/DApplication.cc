658a5ffee14e1f696671b0f9ca00302bd7f99e39
Sean Dobbs <sdobbs@jlab.org>
2015-01-29 19:22:05 +0000

    Quit if magnetic field is requested from the CCDB but not found - this can be overriden by specifying a magnetic field map on the command line.

--- src/libraries/DANA/DApplication.cc
+++ src/libraries/DANA/DApplication.cc
@@ -276,6 +276,15 @@ DGeometry* DApplication::GetDGeometry(unsigned int run_number)
 //---------------------------------
 DMagneticFieldMap* DApplication::GetBfield(unsigned int run_number)
 {
+	const char *ccdb_help = 
+		" \n"
+		" Could not load the solenoid field map from the CCDB!\n"
+		" Please specify the solenoid field map to use on the command line, e.g.:\n"
+		" \n"
+		"   -PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520\n"
+		" or\n"
+		"   -PBFIELD_TYPE=NoField\n";
+	
 	pthread_mutex_lock(&mutex);
 
 	// If field map already exists, return it immediately
@@ -288,16 +297,23 @@ DMagneticFieldMap* DApplication::GetBfield(unsigned int run_number)
 	// Allow a trivial homogeneous map to be used if 
 	// specified on the command line
 	string bfield_type = "FineMesh";
+	string bfield_map = "";
 	GetJParameterManager()->SetDefaultParameter("BFIELD_TYPE", bfield_type);
+	if( GetJParameterManager()->Exists("BFIELD_MAP") ) {
+		bfield_map = GetJParameterManager()->GetParameter("BFIELD_MAP")->GetValue();
+	}
 	if(bfield_type=="CalibDB"|| bfield_type=="FineMesh"){
 		// see if we can load the name of the magnetic field map to use from the calib DB
 		JCalibration *jcalib = GetJCalibration(run_number);
 		map<string,string> bfield_map_name;
 		if(jcalib->GetCalib("/Magnets/Solenoid/solenoid_map", bfield_map_name)) {
-			jout << "Couldn't find default solenoid map table in CCDB, using default map ..." << endl;
-
-			// make default Bfield if we can't figure anything out
-			bfield = new DMagneticFieldMapFineMesh(this,run_number);
+			if( bfield_map != "" )  { // still make the map if the magnetic field map is passed in on the command line
+				bfield = new DMagneticFieldMapFineMesh(this,run_number);
+			} else {
+				jerr << "Couldn't find default solenoid map table in CCDB, using default map ..." << endl;
+				jerr << ccdb_help << endl;
+				exit(-1);
+			}
 		} else {
 			if( bfield_map_name.find("map_name") != bfield_map_name.end() ) {
 				if( bfield_map_name["map_name"] == "NoField" )     // special case for no magnetic field
@@ -305,9 +321,13 @@ DMagneticFieldMap* DApplication::GetBfield(unsigned int run_number)
 				else  
 					bfield = new DMagneticFieldMapFineMesh(this,run_number,bfield_map_name["map_name"]);  // pass along the name of the magnetic field map to load
 			} else {
-				jerr << "Couldn't find default solenoid map name in CCDB, using default map ..." << endl;
-				// default Bfield again
-				bfield = new DMagneticFieldMapFineMesh(this,run_number);
+				if( bfield_map != "" )  { // still make the map if the magnetic field map is passed in on the command line
+					bfield = new DMagneticFieldMapFineMesh(this,run_number);
+				} else {
+					jerr << "Couldn't find default solenoid map name in CCDB, using default map ..." << endl;
+					jerr << ccdb_help << endl;
+					exit(-1);
+				}
 			}
 		}
 		jout<<"Created Magnetic field map of type DMagneticFieldMapFineMesh."<<endl;
