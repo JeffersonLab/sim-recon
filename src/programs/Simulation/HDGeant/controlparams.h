08465513f0e053cb8db49a2b855cebfb7aec1369
Mark Ito <marki@jlab.org>
2015-03-24 22:38:59 +0000

    Add FFREAD card to set run number. Here is the comment in control.in:
      If RUNNO card gives a non-negative value, use that value to initialize
      the calibration database and initialize the run number. Run number
      encoded in hddm events will overwrite this run number. Particle gun
      events will use this value.

--- src/programs/Simulation/HDGeant/controlparams.h
+++ src/programs/Simulation/HDGeant/controlparams.h
@@ -11,6 +11,9 @@ typedef struct {
 	int driftclusters;
 	float tgwidth[2];
 	int runtime_geom;
+  int get_next_evt;
+  float trigger_time_signa_ns;
+  int runno_ff;
 }controlparams_t;
 extern controlparams_t controlparams_;
 
