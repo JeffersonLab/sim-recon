e76b0ce5d4fb78b038d24a5e51c9c20c39ff6c0b
Nathan Sparks <nsparks@jlab.org>
2015-05-18 19:59:21 +0000

    plugin for skimming pair spectrometer events; skims events with PSC or PS left-right coincidence, default max tdiff is 10ns

---
+++ src/programs/Utilities/plugins/ps_skim/JEventProcessor_ps_skim.cc
@@ -0,0 +1,112 @@
+// $Id$
+//
+//    File: JEventProcessor_ps_skim.cc
+// Created: Mon May 18 09:52:08 EDT 2015
+// Creator: nsparks (on Linux cua2.jlab.org 2.6.32-431.5.1.el6.x86_64 x86_64)
+//
+
+#include "JEventProcessor_ps_skim.h"
+using namespace jana;
+
+#include "evio_writer/DEventWriterEVIO.h"
+#include "DAQ/DEPICSvalue.h"
+#include <PAIR_SPECTROMETER/DPSCPair.h>
+#include <PAIR_SPECTROMETER/DPSPair.h>
+
+// Routine used to create our JEventProcessor
+#include <JANA/JApplication.h>
+#include <JANA/JFactory.h>
+extern "C"{
+  void InitPlugin(JApplication *app){
+    InitJANAPlugin(app);
+    app->AddProcessor(new JEventProcessor_ps_skim());
+  }
+} // "C"
+
+
+//------------------
+// JEventProcessor_ps_skim (Constructor)
+//------------------
+JEventProcessor_ps_skim::JEventProcessor_ps_skim()
+{
+
+}
+
+//------------------
+// ~JEventProcessor_ps_skim (Destructor)
+//------------------
+JEventProcessor_ps_skim::~JEventProcessor_ps_skim()
+{
+
+}
+
+//------------------
+// init
+//------------------
+jerror_t JEventProcessor_ps_skim::init(void)
+{
+
+  return NOERROR;
+}
+
+//------------------
+// brun
+//------------------
+jerror_t JEventProcessor_ps_skim::brun(JEventLoop *eventLoop, int runnumber)
+{
+  // This is called whenever the run number changes
+  return NOERROR;
+}
+
+//------------------
+// evnt
+//------------------
+jerror_t JEventProcessor_ps_skim::evnt(JEventLoop *loop, int eventnumber)
+{
+
+  const DEventWriterEVIO* locEventWriterEVIO = NULL;
+  loop->GetSingle(locEventWriterEVIO);
+
+  //Save EPICS events
+  vector<const DEPICSvalue*> locEPICSValues;
+  loop->Get(locEPICSValues);
+  if(!locEPICSValues.empty()) {
+    locEventWriterEVIO->Write_EVIOEvent(loop, "ps");
+    return NOERROR;
+  }
+
+  // coarse PS pairs
+  vector<const DPSCPair*> cpairs;
+  loop->Get(cpairs);
+  // fine PS pairs
+  vector<const DPSPair*> fpairs;
+  loop->Get(fpairs);
+  // skim events with at least 1 PSC or PS left-right coincidence
+  if (cpairs.size()>=1||fpairs.size()>=1) {
+    locEventWriterEVIO->Write_EVIOEvent(loop, "ps");
+    return NOERROR;
+  }
+  
+  return NOERROR;
+}
+
+//------------------
+// erun
+//------------------
+jerror_t JEventProcessor_ps_skim::erun(void)
+{
+  // This is called whenever the run number changes, before it is
+  // changed to give you a chance to clean up before processing
+  // events from the next run number.
+  return NOERROR;
+}
+
+//------------------
+// fini
+//------------------
+jerror_t JEventProcessor_ps_skim::fini(void)
+{
+  // Called before program exit after event processing is finished.
+  return NOERROR;
+}
+
---
+++ src/programs/Utilities/plugins/ps_skim/JEventProcessor_ps_skim.h
@@ -0,0 +1,28 @@
+// $Id$
+//
+//    File: JEventProcessor_ps_skim.h
+// Created: Mon May 18 09:52:08 EDT 2015
+// Creator: nsparks (on Linux cua2.jlab.org 2.6.32-431.5.1.el6.x86_64 x86_64)
+//
+
+#ifndef _JEventProcessor_ps_skim_
+#define _JEventProcessor_ps_skim_
+
+#include <JANA/JEventProcessor.h>
+
+class JEventProcessor_ps_skim:public jana::JEventProcessor{
+	public:
+		JEventProcessor_ps_skim();
+		~JEventProcessor_ps_skim();
+		const char* className(void){return "JEventProcessor_ps_skim";}
+
+	private:
+		jerror_t init(void);						///< Called once at program start.
+		jerror_t brun(jana::JEventLoop *eventLoop, int runnumber);	///< Called everytime a new run number is detected.
+		jerror_t evnt(jana::JEventLoop *eventLoop, int eventnumber);	///< Called every event.
+		jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
+		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+};
+
+#endif // _JEventProcessor_ps_skim_
+
