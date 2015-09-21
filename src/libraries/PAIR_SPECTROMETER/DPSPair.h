8f7b3ebee396cf6a1e11ba52f7294743e5531f69
Nathan Sparks <nsparks@jlab.org>
2015-09-21 13:44:06 -0400

    Add pulse-peak/integral to toStrings() for PS pairs
    These items have been added so that they are available as ROOT
    branches in the output of the "janaroot" plugin.
    
    PSC/TAGM hits: Set pulse peak (0/NaN) when there is no fADC hit.

--- src/libraries/PAIR_SPECTROMETER/DPSPair.h
+++ src/libraries/PAIR_SPECTROMETER/DPSPair.h
@@ -19,13 +19,17 @@ class DPSPair:public jana::JObject{
   pair<const DPSHit*,const DPSHit*> ee;	// first:North(left); second:South(right)	
 
   void toStrings(vector<pair<string,string> > &items)const{
-    AddString(items, "column_left", "%d", ee.first->column);
-    AddString(items, "column_right", "%d", ee.second->column);
-    AddString(items, "Epair", "%f", ee.first->E+ee.second->E);
-    AddString(items, "El", "%f", ee.first->E);
-    AddString(items, "Er", "%f", ee.second->E);
-    AddString(items, "tl", "%f", ee.first->t);
-    AddString(items, "tr", "%f", ee.second->t);
+    AddString(items, "column_lhit", "%d", ee.first->column);
+    AddString(items, "column_rhit", "%d", ee.second->column);
+    AddString(items, "E_pair", "%f", ee.first->E+ee.second->E);
+    AddString(items, "E_lhit", "%f", ee.first->E);
+    AddString(items, "E_rhit", "%f", ee.second->E);
+    AddString(items, "t_lhit", "%f", ee.first->t);
+    AddString(items, "t_rhit", "%f", ee.second->t);
+    AddString(items, "integral_lhit", "%f", ee.first->integral);
+    AddString(items, "integral_rhit", "%f", ee.second->integral);
+    AddString(items, "pulse_peak_lhit", "%f", ee.first->pulse_peak);
+    AddString(items, "pulse_peak_rhit", "%f", ee.second->pulse_peak);
   }
 		
 };
