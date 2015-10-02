1aa0ac0f24c3adaac01507bbaceb43d4187e843e
Paul Mattione <pmatt@Pauls-MacBook-Pro-2.local>
2015-10-01 23:17:09 -0400

    Make functions public.

--- src/plugins/Utilities/trackeff_missing/DCustomAction_dEdxCut.h
+++ src/plugins/Utilities/trackeff_missing/DCustomAction_dEdxCut.h
@@ -34,11 +34,12 @@ class DCustomAction_dEdxCut : public DAnalysisAction
 
 		void Initialize(JEventLoop* locEventLoop);
 
+		bool Cut_dEdx(const DChargedTrackHypothesis* locChargedTrackHypothesis) const;
+		bool Cut_dEdx(Particle_t locPID, double locP, double locdEdx, bool locHasNoTimeInfoFlag) const;
+
 	private:
 
 		bool Perform_Action(JEventLoop* locEventLoop, const DParticleCombo* locParticleCombo);
-		bool Cut_dEdx(const DChargedTrackHypothesis* locChargedTrackHypothesis) const;
-		bool Cut_dEdx(Particle_t locPID, double locP, double locdEdx, bool locHasNoTimeInfoFlag) const;
 
 		// Optional: Useful utility functions.
 		// const DAnalysisUtilities* dAnalysisUtilities;
