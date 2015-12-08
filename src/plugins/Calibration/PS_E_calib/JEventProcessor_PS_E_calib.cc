1a091925e274f3c070896014d0edf2879522fd8f
Alex Barnes <aebarnes@jlab.org>
2015-12-08 10:54:34 -0500

    Added plugin for the PS energy calibration

---
+++ src/plugins/Calibration/PS_E_calib/JEventProcessor_PS_E_calib.cc
@@ -0,0 +1,278 @@
+// $Id$
+// //    File: JEventProcessor_PS_E_calib.cc // Created: Thu Jul  9 17:44:32 EDT 5015
+// Creator: aebarnes (on Linux ifarm1401 2.6.32-431.el6.x86_64 x86_64)
+//
+
+#include "JEventProcessor_PS_E_calib.h"
+using namespace jana;
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <math.h>
+
+#include <TTree.h>
+#include <TBranch.h>
+#include <TDirectory.h>
+#include <TProfile.h>
+#include <TH2.h>
+
+#include <TAGGER/DTAGMHit.h>
+#include <TAGGER/DTAGHHit.h>
+#include <PAIR_SPECTROMETER/DPSCPair.h>
+#include <PAIR_SPECTROMETER/DPSPair.h>
+
+#define CORRECTIONS false
+
+// Define constants
+const float Ebw_PS = 0.013;			// Energy bin width for the PS in GeV
+const float Ebl_PS = 2.3;			// Low energy of the PS total energy in GeV
+const float Ebh_PS = 4.9;			// High energy of the PS total energy in GeV
+const float NEb_PS = (Ebh_PS - Ebl_PS)/Ebw_PS;	// Number of energy bins for the PS
+
+const int MAX_COLUMNS = 100;			// Total columns in the TAGM
+const int MAX_COUNTERS = 274;			// Total possible counters in the TAGH
+
+// Declare variables
+double p0 = 0;					// PS energy correction parameter
+double p1 = 0;					// PS energy correction parameter
+double p2 = 0;					// PS energy correction parameter
+// TAGM
+int column = 0;					// TAGM column
+double tm_E = 0;				// TAGM energy
+double tm_t = 0;				// TAGM time
+double tdiff_tm = 0;				// time difference PS avg - TAGM
+// TAGH
+int counter = 0;				// TAGH counter
+double th_E = 0;				// TAGH energy
+double th_t = 0;				// TAGH time
+double tdiff_th = 0;				// time difference PS avg - TAGH
+// PS
+double ps_E = 0;				// total PS energy
+double ps_El = 0;				// left PS arm energy
+double ps_Er = 0;				// right PS arm energy
+double ps_t = 0;				// average PS time
+double ps_tl = 0;				// left PS time
+double ps_tr = 0;				// right PS time
+
+// This is not correct. Fix this when the timing is aligned
+int run = 0;					// Run number
+
+// Declare histograms
+// TAGM
+static TH2F *h_psE_vs_psEl_tm[MAX_COLUMNS];	// PS total vs fraction of PS left
+// TAGH
+static TH2F *h_psE_vs_psEl_th[MAX_COUNTERS];	// PS total vs fraction of PS left
+// Timing check
+static TH1F *h_dt;				// PS - TAGX timing check
+
+// Routine used to create our JEventProcessor
+#include <JANA/JApplication.h>
+#include <JANA/JFactory.h>
+extern "C"{
+void InitPlugin(JApplication *app){
+	InitJANAPlugin(app);
+	app->AddProcessor(new JEventProcessor_PS_E_calib());
+}
+} // "C"
+
+
+//------------------
+// JEventProcessor_PS_E_calib (Constructor)
+//------------------
+JEventProcessor_PS_E_calib::JEventProcessor_PS_E_calib()
+{
+
+}
+
+//------------------
+// ~JEventProcessor_PS_E_calib (Destructor)
+//------------------
+JEventProcessor_PS_E_calib::~JEventProcessor_PS_E_calib()
+{
+
+}
+
+//------------------
+// init
+//------------------
+jerror_t JEventProcessor_PS_E_calib::init(void)
+{
+	// This is called once at program startup. If you are creating
+	// and filling historgrams in this plugin, you should lock the
+	// ROOT mutex like this:
+	//
+	// japp->RootWriteLock();
+	//  ... fill historgrams or trees ...
+	// japp->RootUnLock();
+	//
+
+   japp->RootWriteLock();
+
+   // create root folder tagm
+   TDirectory *tagmDir = gDirectory->mkdir("TAGM");
+   TDirectory *taghDir = gDirectory->mkdir("TAGH");
+
+   // Book 2d histograms
+   // TAGM
+   tagmDir->cd();
+
+   h_dt = new TH1F("h_dt","Time difference PS - TAGM;PS-TAGM (ns)",200,-17,37);
+
+   for (int col = 0; col < MAX_COLUMNS; ++col) {
+      h_psE_vs_psEl_tm[col] = new TH2F(Form("h_psE_vs_psEl_tm_%i",col+1),
+                                       Form("PS E vs PS left, TAGM col %i;\
+                                       Energy asymmetry;PS energy (GeV)",col+1),
+                                       50,0,1,NEb_PS,Ebl_PS,Ebh_PS);
+   }
+
+   // TAGH
+   taghDir->cd();
+
+   for (int hodo = 0; hodo < MAX_COUNTERS; ++hodo) {
+      h_psE_vs_psEl_th[hodo] = new TH2F(Form("psE_vs_psEl_th_%i",hodo+1),
+                                        Form("PS E vs PS left, TAGH counter %i;\
+                                        Energy asymmetry;PS energy (GeV)",hodo+1),
+                                        50,0,1,NEb_PS,Ebl_PS,Ebh_PS);
+   }
+   
+   japp->RootUnLock();
+	return NOERROR;
+}
+
+//------------------
+// brun
+//------------------
+jerror_t JEventProcessor_PS_E_calib::brun(JEventLoop *eventLoop, int runnumber)
+{
+	// This is called whenever the run number changes
+
+   // THIS IS TEMPORARY! FIX ONCE PS-TAGX COINCIDENCE
+   // IS THE SAME FOR ALL RUNS
+   run = runnumber;
+
+   // Get the PS energy corrections from CCDB 
+   std::vector< std::map<std::string, double> > table;
+   std::string ccdb_key = "/PHOTON_BEAM/pair_spectrometer/fine/energy_corrections";
+   if (eventLoop->GetCalib(ccdb_key, table)) {
+      jout << "Error loading " << ccdb_key << " from ccdb!" << std::endl;
+   }
+   for (unsigned int i=0; i < table.size(); ++i) {
+      p0 = (table[i])["constant"];
+      p1 = (table[i])["linear"];
+      p2 = (table[i])["quadratic"];
+   }
+
+	return NOERROR;
+}
+
+//------------------
+// evnt
+//------------------
+jerror_t JEventProcessor_PS_E_calib::evnt(JEventLoop *loop, int eventnumber)
+{
+	// This is called for every event. Use of common resources like writing
+	// to a file or filling a histogram should be mutex protected. Using
+	// loop->Get(...) to get reconstructed objects (and thereby activating the
+	// reconstruction algorithm) should be done outside of any mutex lock
+	// since multiple threads may call this method at the same time.
+	// Here's an example:
+	//
+	// vector<const MyDataClass*> mydataclasses;
+	// loop->Get(mydataclasses);
+	//
+	// japp->RootWriteLock();
+	//  ... fill historgrams or trees ...
+	// japp->RootUnLock();
+
+   vector<const DTAGMHit*>	tm_hits;
+   vector<const DTAGHHit*>	th_hits;
+   vector<const DPSPair*>	ps_pairs;
+   vector<const DPSCPair*>	psc_pairs;
+
+   loop->Get(tm_hits);
+   loop->Get(th_hits);
+   loop->Get(ps_pairs);
+   loop->Get(psc_pairs);
+
+   japp->RootWriteLock();
+
+   if (psc_pairs.size() > 0 ) {
+      for (uint32_t i = 0; i < ps_pairs.size(); ++i) {
+         // left arm
+         ps_El = ps_pairs[i]->ee.first->E;
+         ps_tl = ps_pairs[i]->ee.first->t;
+         // right arm
+         ps_Er = ps_pairs[i]->ee.second->E;
+         ps_tr = ps_pairs[i]->ee.second->t;
+         // combined
+         ps_t = (ps_tl + ps_tr)/2;
+         ps_E = ps_El + ps_Er;
+
+         #if CORRECTIONS
+         ps_E /= p0 + p1*(ps_El/ps_E) + p2*(ps_El/ps_E)*(ps_El/ps_E);
+         #endif
+         
+         // loop over TAGM hits
+         for (uint32_t j = 0; j < tm_hits.size(); ++j) {
+            tm_E = tm_hits[j]->E;
+            tm_t = tm_hits[j]->t;
+            tdiff_tm = ps_t - tm_t;
+            column = tm_hits[j]->column;
+
+            if (!tm_hits[j]->has_fADC || !tm_hits[j]->has_TDC) continue;
+            if (tm_hits[j]->row != 0) continue;
+
+            h_dt->Fill(tdiff_tm);
+
+            if (run == 3180 && tdiff_tm < 26.5 && tdiff_tm >= 22.5) {
+               h_psE_vs_psEl_tm[column-1]->Fill(ps_El/ps_E,ps_E);
+            }
+            else if (run == 3185 && tdiff_tm < 34.5 && tdiff_tm >= 30.5) {
+               h_psE_vs_psEl_tm[column-1]->Fill(ps_El/ps_E,ps_E);
+            }
+         }
+         
+         // loop over TAGH hits
+         for (uint32_t j = 0; j < th_hits.size(); ++j) {
+            th_E = th_hits[j]->E;
+            th_t = th_hits[j]->t;
+            tdiff_th = ps_t - th_t;
+            counter = th_hits[j]->counter_id;
+
+            if (!th_hits[j]->has_fADC || !th_hits[j]->has_TDC) continue;
+
+            if (run == 3180 && tdiff_th < 26.5 && tdiff_th >= 22.5) {
+               h_psE_vs_psEl_th[counter-1]->Fill(ps_El/ps_E,ps_E);
+            }
+            else if (run == 3185 && tdiff_th < 34.5 && tdiff_th >= 30.5) {
+               h_psE_vs_psEl_th[counter-1]->Fill(ps_El/ps_E,ps_E);
+            }
+         }
+      }
+   }
+   japp->RootUnLock();
+
+	return NOERROR;
+}
+
+//------------------
+// erun
+//------------------
+jerror_t JEventProcessor_PS_E_calib::erun(void)
+{
+	// This is called whenever the run number changes, before it is
+	// changed to give you a chance to clean up before processing
+	// events from the next run number.
+	return NOERROR;
+}
+
+//------------------
+// fini
+//------------------
+jerror_t JEventProcessor_PS_E_calib::fini(void)
+{
+	// Called before program exit after event processing is finished.
+	return NOERROR;
+}
+
---
+++ src/plugins/Calibration/PS_E_calib/PSEcorr.C
@@ -0,0 +1,304 @@
+// ROOT macro to grab a histogram and make projections per bin
+// Created by Alex Barnes, 6/30/2015
+
+#include <TH1.h>
+#include <TH2.h>
+#include <TFile.h>
+#include <TProfile.h>
+#include <TFitResult.h>
+#include <TFitResultPtr.h>
+#include <TCanvas.h>
+#include <TROOT.h>
+#include <TMath.h>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+#define CORRECTIONS true	// when true the correction parameters will be applied
+#define TAGM true		// include TAGM when true 
+#define TAGH false		// include TAGH when true
+
+// Declare constants
+#if TAGM
+const int MAX_COLUMNS			= 100;	// Number of TAGM channels
+int bad_channels_tm			= 0;	// Number of channels excluded in fit
+double fitResults_tm[MAX_COLUMNS][3]	= {},{};// fit parameters, 3 pol2 params
+double max_E_tm[MAX_COLUMNS]		= {};	// maximum of fit
+double fit_tm[3]			= {};	// average fit parameters
+#endif
+
+#if TAGH
+const int MAX_COUNTERS			= 274;	// Number of TAGH channels
+int bad_channels_th			= 0;	// Number of channels excluded in fit
+double fitResults_th[MAX_COUNTERS][3]	= {},{};// fit parameters, 3 pol2 params
+double max_E_th[MAX_COUNTERS]		= {};	// maximum of fit
+double fit_th[3]			= {};	// average fit parameters
+#endif
+
+double Ebw_PS = 0.013;				// PS energy bin width
+double Ebl_PS = 2.3;				// PS low energy
+double Ebh_PS = 4.9;				// PS high energy
+double NEb_PS = (Ebh_PS - Ebl_PS)/Ebw_PS;	// Number of bins for PS energy
+
+void PSEcorr(char* inputFile) {
+   TCanvas *c1 = (TCanvas*)gROOT->FindObject("c1");
+   if (c1 == 0)
+      c1 = new TCanvas("c1","c1",0,20,600,500);
+   c1->cd();
+
+   std::cout << "Getting ROOT file" << std::endl;
+   TFile *infile = new TFile(inputFile);
+   TFile *outfile = new TFile("profiles.root", "recreate");
+
+   // Create directories
+   std::cout << "Creating output file directories" << std::endl;
+   outfile->mkdir("TAGM");
+   outfile->mkdir("TAGH");
+
+#if TAGM
+
+#if !CORRECTIONS
+   std::ofstream fout_tm("Eparms-TAGM.out");
+#endif // !CORRECTIONS
+
+#if CORRECTIONS
+   std::fstream parmsFile("Eparms-TAGM.out", ios_base::in);
+   string parm0, parm1, parm2;
+   double p0, p1, p2;
+   int index = 0;
+   if (parmsFile) {
+      std::string line;
+      while (parmsFile >> std::ws && std::getline(parmsFile, line))
+         ;
+      std::istringstream input(line);
+      input >> parm0 >> parm1 >> parm2;
+      p0 = atof(parm0.c_str());
+      p1 = atof(parm1.c_str());
+      p2 = atof(parm2.c_str());
+   }
+#endif // CORRECTIONS
+
+   std::cout << "Getting original TAGM histograms" << std::endl;
+
+   TH2F *h_psE_vs_psEl_tm[MAX_COLUMNS];
+   TProfile *psE_vs_psEl_tm[MAX_COLUMNS];
+   TProfile *psE_vs_psEl_tm_corr[MAX_COLUMNS];
+   TH1D *h_tmp;
+   for (int col = 0; col < MAX_COLUMNS; ++col) {
+      h_psE_vs_psEl_tm[col] = (TH2F*)infile->Get(Form("TAGM/h_psE_vs_psEl_tm_%i",col+1));
+      psE_vs_psEl_tm_corr[col] = new TProfile(Form("psE_vs_psEl_tm_corr_%i",col+1),
+                                              Form("PS E vs PS left, TAGM col %i;\
+                                              Energy asymmetry;PS energy (GeV)",col+1),
+                                              50,0,1,2.3,4.9);
+      psE_vs_psEl_tm_corr[col]->SetMaximum(4.9);
+      psE_vs_psEl_tm_corr[col]->SetMinimum(2.3);
+      h_tmp = h_psE_vs_psEl_tm[col]->ProjectionY();
+      int bMax = h_tmp->GetMaximumBin();
+      int yMax = bMax;
+      int yMin = bMax;
+      for (int i = 0; i < (NEb_PS-bMax); ++i) {
+         if (h_tmp->GetBinContent(bMax+i) < 2) {
+            yMax = bMax + i;
+            break;
+         }
+      }
+      for (int i = 0; i < bMax; ++i) {
+         if (h_tmp->GetBinContent(bMax-i) < 2) {
+            yMin = bMax - i;
+            break;
+         }
+      }
+      psE_vs_psEl_tm[col] = h_psE_vs_psEl_tm[col]->ProfileX(Form("col_%i",col+1),yMin,yMax);
+      int bad_bins = 0;
+      for (int i = 1; i <= 50; ++i) {
+         double bEntries = psE_vs_psEl_tm[col]->GetBinEntries(i);
+         if (bEntries < 5) {
+            psE_vs_psEl_tm[col]->SetBinEntries(i,0);
+            bad_bins++;
+         }
+      }
+      int nentries = psE_vs_psEl_tm[col]->GetEntries();
+      if (nentries == 0 || bad_bins > 45) {
+         for (int i = 0; i < 3; ++i) {
+            fitResults_tm[col][i] = 0;
+            bad_channels_tm++;
+         }
+      }
+      else {
+#if CORRECTIONS
+         for (int i = 1; i <= 50; ++i) {
+            double content = psE_vs_psEl_tm[col]->GetBinContent(i);
+            double asym = (i+0.5)*0.02;
+            //content /= (p0 + p1*asym + p2*asym*asym);
+            psE_vs_psEl_tm_corr[col]->Fill(asym,content);
+         }
+#endif // CORRECTIONS
+
+#if !CORRECTIONS
+         TFitResultPtr ptr = psE_vs_psEl_tm[col]->Fit("pol2","sq");
+         fitResults_tm[col][0] = ptr->Parameter(0);
+         fitResults_tm[col][1] = ptr->Parameter(1);
+         fitResults_tm[col][2] = ptr->Parameter(2);
+
+         // maximum of parabola at c0 - c1^2/(4c2)
+         max_E_tm[col] = -1*fitResults_tm[col][1]*fitResults_tm[col][1];
+         max_E_tm[col] /= 4*fitResults_tm[col][2];
+         max_E_tm[col] += fitResults_tm[col][0];
+
+         for (int i = 0; i < 3; ++i) {
+            fitResults_tm[col][i] /= max_E_tm[col];
+            fit_tm[i] += fitResults_tm[col][i];
+         }
+#endif // !CORRECTIONS
+      }
+
+#if CORRECTIONS
+      outfile->cd("TAGM");
+      psE_vs_psEl_tm_corr[col]->Write();
+#endif // CORRECTIONS
+
+#if !CORRECTIONS
+      outfile->cd("TAGM");
+      psE_vs_psEl_tm[col]->Write();
+#endif // !CORRECTIONS
+   }
+#if !CORRECTIONS
+   for (int i = 0; i < 3; ++i) {
+      fit_tm[i] /= (MAX_COLUMNS - bad_channels_tm);
+   }
+
+   fout_tm << fit_tm[0] << std::setw(15)
+           << fit_tm[1] << std::setw(15)
+           << fit_tm[2] << std::endl;
+
+#endif // !CORRECTIONS
+#endif // TAGM
+
+#if TAGH
+
+#if !CORRECTIONS
+   std::ofstream fout_th("Eparms-TAGH.out");
+#endif // !CORRECTIONS
+
+#if CORRECTIONS
+   std::fstream parmsFile("Eparms-TAGH.out", ios_base::in);
+   string parm0, parm1, parm2;
+   double p0, p1, p2;
+   int index = 0;
+   if (parmsFile) {
+      std::string line;
+      while (parmsFile >> std::ws && std::getline(parmsFile, line))
+         ;
+      std::istringstream input(line);
+      input >> parm0 >> parm1 >> parm2;
+      p0 = atof(parm0.c_str());
+      p1 = atof(parm1.c_str());
+      p2 = atof(parm2.c_str());
+   }
+   TProfile *psE_vs_psEl_th_corr[MAX_COUNTERS];
+#endif // CORRECTIONS
+
+   std::cout << "Getting original TAGH histograms" << std::endl;
+
+   TH2F *h_psE_vs_psEl_th[MAX_COUNTERS];
+   TH2F *h_psE_vs_psEl_th_side[MAX_COUNTERS];
+   TProfile *psE_vs_psEl_th[MAX_COUNTERS];
+   TH1D *h_tmp;
+   for (int hodo = 0; hodo < MAX_COUNTERS; ++hodo) {
+      h_psE_vs_psEl_th[hodo] = (TH2F*)infile->Get(Form("TAGH/psE_vs_psEl_th_%i",hodo+1));
+      h_tmp = h_psE_vs_psEl_th[hodo]->ProjectionY();
+      int bMax = h_tmp->GetMaximumBin();
+      int yMax = bMax;
+      int yMin = bMax;
+      for (int i = 0; i < (NEb_PS-bMax); ++i) {
+         if (h_tmp->GetBinContent(bMax+i) < 2) {
+            yMax = bMax + i;
+            break;
+         }
+      }
+      for (int i = 0; i < bMax; ++i) {
+         if (h_tmp->GetBinContent(bMax-i) < 2) {
+            yMin = bMax - i;
+            break;
+         }
+      }
+      psE_vs_psEl_th[hodo] = h_psE_vs_psEl_th[hodo]->ProfileX(Form("hodo_%i",hodo+1),yMin,yMax);
+      int bad_bins = 0;
+      for (int i = 1; i <= 50; ++i) {
+         double bEntries = psE_vs_psEl_th[hodo]->GetBinEntries(i);
+         if (bEntries < 5) {
+            psE_vs_psEl_th[hodo]->SetBinEntries(i,0);
+            bad_bins++;
+         }
+      }
+      int nentries = psE_vs_psEl_th[hodo]->GetEntries();
+      if (nentries == 0 || bad_bins > 45) {
+         for (int i = 0; i < 3; ++i) {
+            fitResults_th[hodo][i] = 0;
+            bad_channels_th++;
+         }
+      }
+      nentries = 0;
+      else {
+#if CORRECTIONS
+         psE_vs_psEl_th_corr[hodo] = new TProfile(Form("psE_vs_psEl_th_corr_%i",hodo+1),
+                                                  Form("PS E vs PS left, TAGH counter %i;\
+                                                  Energy asymmetry;PS energy (GeV)",hodo+1),
+                                                  50,0,1,2.3,4.9);
+         psE_vs_psEl_th_corr[hodo]->SetMaximum(4.9);
+         psE_vs_psEl_th_corr[hodo]->SetMinimum(2.3);
+         for (int i = 1; i <= 50; ++i) {
+            double content = psE_vs_psEl_th[hodo]->GetBinContent(i);
+            double asym = (i+0.5)*0.02;
+            content /= (p0 + p1*asym + p2*asym*asym);
+            psE_vs_psEl_th_corr[hodo]->Fill(asym,content);
+         }
+#endif // CORRECTIONS
+
+#if !CORRECTIONS
+         TFitResultPtr ptr = psE_vs_psEl_th[hodo]->Fit("pol2","sq");
+         fitResults_th[hodo][0] = ptr->Parameter(0);
+         fitResults_th[hodo][1] = ptr->Parameter(1);
+         fitResults_th[hodo][2] = ptr->Parameter(2);
+
+         // maximum of parabola at c0 - c1^2/(4c2)
+         max_E_th[hodo] = -1*fitResults_th[hodo][1]*fitResults_th[hodo][1];
+         max_E_th[hodo] /= 4*fitResults_th[hodo][2];
+         max_E_th[hodo] += fitResults_th[hodo][0];
+
+         for (int i = 0; i < 3; ++i) {
+            fitResults_th[hodo][i] /= max_E_th[hodo];
+            fit_th[i] += fitResults_th[hodo][i];
+         }
+#endif // !CORRECTIONS
+      }
+
+#if CORRECTIONS
+      outfile->cd("TAGH");
+      psE_vs_psEl_th_corr[hodo]->Write();
+#endif // CORRECTIONS
+
+#if !CORRECTIONS
+      outfile->cd("TAGH");
+      h_psE_vs_psEl_th[hodo]->Write();
+      psE_vs_psEl_th[hodo]->Write();
+#endif // !CORRECTIONS
+   }
+
+#if !CORRECTIONS
+   std::cout << "bad channels " << bad_channels_th << std::endl;
+
+   for (int i = 0; i < 3; ++i) {
+      fit_th[i] /= (MAX_COUNTERS - bad_channels_th);
+   }
+
+   fout_th << fit_th[0] << std::setw(15)
+           << fit_th[1] << std::setw(15) 
+           << fit_th[2] << std::endl;
+
+#endif // !CORRECTIONS
+#endif // TAGH
+}
+
