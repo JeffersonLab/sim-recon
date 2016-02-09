2f86f5a4b226bf20e22f34e5a7129a9db8ecf4df
Justin Stevens <jrsteven@jlab.org>
2016-02-09 09:52:12 -0500

    clean up p2pi macros and fit psi distribution to extract P*Sigma

--- src/plugins/Analysis/p2pi_hists/HistMacro_p2pi_preco1.C
+++ src/plugins/Analysis/p2pi_hists/HistMacro_p2pi_preco1.C
@@ -1,6 +1,4 @@
-// hnamepath: /p2pi_preco/Custom_p2pi_hists_NoKinFit_Measured/MM2_M2pi
-// hnamepath: /p2pi_preco/Custom_p2pi_hists_KinFitConverge_Measured/MM2_M2pi
-// hnamepath: /p2pi_preco/Custom_p2pi_hists_KinFitConverge_Measured/Egamma
+// hnamepath: /p2pi_preco/Custom_p2pi_hists_TimingCut_Measured/MM2_M2pi
 
 {
 	TDirectory *locTopDirectory = gDirectory;
@@ -10,15 +8,15 @@
 	else
 	  return;
 
-	//Go to NoKinFit directory
-	TDirectory *locDirectory = (TDirectory*)locReactionDirectory->FindObjectAny("Custom_p2pi_hists_NoKinFit_Measured");
+	//Go to TimingCut directory
+	TDirectory *locDirectory = (TDirectory*)locReactionDirectory->FindObjectAny("Custom_p2pi_hists_TimingCut_Measured");
 	if(!locDirectory)
 		return;
 	locDirectory->cd();
-	TH2I* locHist_NoKinFit_MM2_M2pi = (TH2I*)gDirectory->Get("MM2_M2pi");
-	TH2I* locHist_NoKinFit_Proton_dEdx_P = (TH2I*)gDirectory->Get("Proton_dEdx_P");
-	TH2I* locHist_NoKinFit_Egamma_M2pi = (TH2I*)gDirectory->Get("Egamma_M2pi");
-	TH2I* locHist_NoKinFit_PiPlusPsi_Egamma = (TH2I*)gDirectory->Get("PiPlusPsi_Egamma");
+	TH2I* locHist_TimingCut_MM2_M2pi = (TH2I*)gDirectory->Get("MM2_M2pi");
+	TH2I* locHist_TimingCut_Proton_dEdx_P = (TH2I*)gDirectory->Get("Proton_dEdx_P");
+	TH2I* locHist_TimingCut_Egamma_M2pi = (TH2I*)gDirectory->Get("Egamma_M2pi");
+	TH2I* locHist_TimingCut_PiPlusPsi_t = (TH2I*)gDirectory->Get("PiPlusPsi_t");
 
 	//Get/Make Canvas
 	TCanvas *locCanvas = NULL;
@@ -32,52 +30,74 @@
 	locCanvas->cd(1);
 	gPad->SetTicks();
 	gPad->SetGrid();
-	if(locHist_NoKinFit_MM2_M2pi != NULL) {	
-		locHist_NoKinFit_MM2_M2pi->Rebin2D();
-		locHist_NoKinFit_MM2_M2pi->GetXaxis()->SetTitleSize(0.05);
-		locHist_NoKinFit_MM2_M2pi->GetYaxis()->SetTitleSize(0.05);
-		locHist_NoKinFit_MM2_M2pi->GetXaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_MM2_M2pi->GetYaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_MM2_M2pi->Draw("colz");
+	if(locHist_TimingCut_MM2_M2pi != NULL) {	
+		locHist_TimingCut_MM2_M2pi->Rebin2D();
+		locHist_TimingCut_MM2_M2pi->GetXaxis()->SetTitleSize(0.05);
+		locHist_TimingCut_MM2_M2pi->GetYaxis()->SetTitleSize(0.05);
+		locHist_TimingCut_MM2_M2pi->GetXaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_MM2_M2pi->GetYaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_MM2_M2pi->Draw("colz");
 	}
 
 	locCanvas->cd(2);
 	gPad->SetTicks();
 	gPad->SetGrid();
-	if(locHist_NoKinFit_Proton_dEdx_P != NULL) {
-		locHist_NoKinFit_Proton_dEdx_P->Rebin2D();
-		locHist_NoKinFit_Proton_dEdx_P->SetTitle("Proton dE/dx vs momentum: |MM^{2}|<0.02 and |Missing Energy| < 0.2");
-		locHist_NoKinFit_Proton_dEdx_P->GetXaxis()->SetTitleSize(0.05);
-		locHist_NoKinFit_Proton_dEdx_P->GetYaxis()->SetTitleSize(0.05);
-		locHist_NoKinFit_Proton_dEdx_P->GetXaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_Proton_dEdx_P->GetYaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_Proton_dEdx_P->Draw("colz");
+	if(locHist_TimingCut_Proton_dEdx_P != NULL) {
+		locHist_TimingCut_Proton_dEdx_P->Rebin2D();
+		locHist_TimingCut_Proton_dEdx_P->SetTitle("Proton dE/dx vs momentum: |MM^{2}|<0.02 and |Missing Energy| < 0.2");
+		locHist_TimingCut_Proton_dEdx_P->GetXaxis()->SetTitleSize(0.05);
+		locHist_TimingCut_Proton_dEdx_P->GetYaxis()->SetTitleSize(0.05);
+		locHist_TimingCut_Proton_dEdx_P->GetXaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_Proton_dEdx_P->GetYaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_Proton_dEdx_P->Draw("colz");
 	}
 
 	locCanvas->cd(3);
 	gPad->SetTicks();
 	gPad->SetGrid();
-	if(locHist_NoKinFit_Egamma_M2pi != NULL) {
+	if(locHist_TimingCut_Egamma_M2pi != NULL) {
 		
-		locHist_NoKinFit_Egamma_M2pi->SetTitle("E_{#gamma} vs M_{#pi^{+}#pi^{-}}: Proton dE/dx > 2.2");
-		locHist_NoKinFit_Egamma_M2pi->GetXaxis()->SetTitleSize(0.05);
-		locHist_NoKinFit_Egamma_M2pi->GetXaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_Egamma_M2pi->GetYaxis()->SetTitleSize(0.05);
-                locHist_NoKinFit_Egamma_M2pi->GetYaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_Egamma_M2pi->Draw("colz");
+		locHist_TimingCut_Egamma_M2pi->SetTitle("E_{#gamma} vs M_{#pi^{+}#pi^{-}}: Proton dE/dx > 2.2");
+		locHist_TimingCut_Egamma_M2pi->GetXaxis()->SetTitleSize(0.05);
+		locHist_TimingCut_Egamma_M2pi->GetXaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_Egamma_M2pi->GetYaxis()->SetTitleSize(0.05);
+                locHist_TimingCut_Egamma_M2pi->GetYaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_Egamma_M2pi->Draw("colz");
 	}
 
 	locCanvas->cd(4);
 	gPad->SetTicks();
 	gPad->SetGrid();
-	if(locHist_NoKinFit_PiPlusPsi_Egamma != NULL) {
+	if(locHist_TimingCut_PiPlusPsi_t != NULL) {
 
-		locHist_NoKinFit_PiPlusPsi_Egamma->SetTitle("#psi_{#pi^{+}} vs E_{#gamma}: Proton dE/dx > 2.2; E_{#gamma}; #psi_{#pi^{+}}");
-		locHist_NoKinFit_PiPlusPsi_Egamma->GetXaxis()->SetTitleSize(0.05);
-		locHist_NoKinFit_PiPlusPsi_Egamma->GetXaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_PiPlusPsi_Egamma->GetYaxis()->SetTitleSize(0.05);
-                locHist_NoKinFit_PiPlusPsi_Egamma->GetYaxis()->SetLabelSize(0.05);
-		locHist_NoKinFit_PiPlusPsi_Egamma->Draw("colz");
+		locHist_TimingCut_PiPlusPsi_t->SetTitle("#psi_{#pi^{+}} vs E_{#gamma}: Proton dE/dx > 2.2; E_{#gamma}; #psi_{#pi^{+}}");
+		TH1D *locHist_TimingCut_PiPlusPsi = (TH1D*)locHist_TimingCut_PiPlusPsi_t->ProjectionY();
+		locHist_TimingCut_PiPlusPsi->Rebin(4);
+		locHist_TimingCut_PiPlusPsi->SetMinimum(0);
+		locHist_TimingCut_PiPlusPsi->GetXaxis()->SetTitleSize(0.05);
+		locHist_TimingCut_PiPlusPsi->GetXaxis()->SetLabelSize(0.05);
+		locHist_TimingCut_PiPlusPsi->Draw();
+
+		// fit 1+cos(2*phi) distribution
+		TF1* fit = new TF1("psiFit","[0]*(1.0 + [1]*cos(2*(x + [2])/180.*3.14159))");
+		locHist_TimingCut_PiPlusPsi->Fit(fit, "Q", "");
+		locHist_TimingCut_PiPlusPsi->Draw("e");
+		
+		// print fit parameters to canvas
+		Double_t PSigma = fit->GetParameter(1);
+		Double_t PSigmaErr = fit->GetParError(1);
+		Double_t Phi0 = fit->GetParameter(2);
+		Double_t Phi0Err = fit->GetParError(2);
+		TLatex tx;
+		tx.SetTextAlign(21);
+		tx.SetTextSize(0.05);
+		char text[100];
+		sprintf(text, "P#Sigma=%0.2f#pm%0.2f", PSigma, PSigmaErr);
+		tx.DrawLatex(0., locHist_TimingCut_PiPlusPsi->GetMaximum()*0.15, text);
+		sprintf(text, "#phi_{0}=%0.2f#pm%0.2f", Phi0, Phi0Err);
+		tx.DrawLatex(0., locHist_TimingCut_PiPlusPsi->GetMaximum()*0.06, text);
+		
+		fit->Delete();
 	}
 }
 
