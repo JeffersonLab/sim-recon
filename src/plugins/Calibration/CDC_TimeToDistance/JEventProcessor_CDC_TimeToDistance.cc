219758a4e7e1d56f19121aee16019dba82b409c7
Michael Staib <mstaib@jlab.org>
2016-05-31 11:22:34 -0400

    Change definition of pull.d to match Kalman tracking.

--- src/plugins/Calibration/CDC_TimeToDistance/JEventProcessor_CDC_TimeToDistance.cc
+++ src/plugins/Calibration/CDC_TimeToDistance/JEventProcessor_CDC_TimeToDistance.cc
@@ -125,8 +125,8 @@ jerror_t JEventProcessor_CDC_TimeToDistance::evnt(JEventLoop *loop, uint64_t eve
             double docaz = thisPull.z;
             if (docaz < 70.0 || docaz > 110.0) continue; // Only focus on the center of the chamber
             //if (docaz < 140.0) continue; // Only focus on downstream end of chamber
-            double distance = thisPull.d; // This is the distance from the lookup table
-            double predictedDistance = distance - residual; // This is the distance predicted by the fit
+            double predictedDistance = thisPull.d; // This is the DOCA of the track
+            double distance = residual + predictedDistance; // This is the distance from the T-D lookup
             const DCDCTrackHit* thisCDCHit = thisPull.cdc_hit;
 
             if (thisCDCHit == NULL) continue;
