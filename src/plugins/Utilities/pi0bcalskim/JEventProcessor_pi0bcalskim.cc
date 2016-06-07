4ab9105c57f5f427d3f76b8513c1424b2c7928a0
Sean Dobbs <seandobbs@gmail.com>
2016-06-07 17:56:45 -0400

    Example of writing out only certain objects in a skim.

--- src/plugins/Utilities/pi0bcalskim/JEventProcessor_pi0bcalskim.cc
+++ src/plugins/Utilities/pi0bcalskim/JEventProcessor_pi0bcalskim.cc
@@ -79,14 +79,15 @@ jerror_t JEventProcessor_pi0bcalskim::init(void)
 //------------------
 jerror_t JEventProcessor_pi0bcalskim::brun(JEventLoop *eventLoop, int32_t runnumber)
 {
+    /* Example
     // only write out BCAL data for these events
 	const DEventWriterEVIO* locEventWriterEVIO = NULL;
-	loop->GetSingle(locEventWriterEVIO);
-
+	eventLoop->GetSingle(locEventWriterEVIO);
+    
     if(locEventWriterEVIO) {
         locEventWriterEVIO->SetDetectorsToWriteOut("BCAL","pi0bcalskim");
     }
-
+    */
     return NOERROR;
 }
 
@@ -102,8 +103,10 @@ jerror_t JEventProcessor_pi0bcalskim::evnt(JEventLoop *loop, uint64_t eventnumbe
   vector<const DVertex*> kinfitVertex;
   loop->Get(kinfitVertex);
 
-	const DEventWriterEVIO* locEventWriterEVIO = NULL;
-	loop->GetSingle(locEventWriterEVIO);
+  const DEventWriterEVIO* locEventWriterEVIO = NULL;
+  loop->GetSingle(locEventWriterEVIO);
+
+  vector< const JObject* > locBCALShowersToSave;
 
   // always write out BOR events
   if(loop->GetJEvent().GetStatusBit(kSTATUS_BOR_EVENT)) {
@@ -184,13 +187,19 @@ jerror_t JEventProcessor_pi0bcalskim::evnt(JEventLoop *loop, uint64_t eventnumbe
 		TLorentzVector ptot = sh1_p+sh2_p;
 		inv_mass = ptot.M();
 		Candidate |= ( (sh2_E>0.67) && (inv_mass<0.30) );
+        if((sh2_E>0.67) && (inv_mass<0.30)) {
+            if(find(locBCALShowersToSave.begin(), locBCALShowersToSave.end(), locBCALShowers[i]) == locBCALShowersToSave.end())
+                locBCALShowersToSave.push_back(static_cast<const JObject *>(locBCALShowers[i]));
+            if(find(locBCALShowersToSave.begin(), locBCALShowersToSave.end(), locBCALShowers[j]) == locBCALShowersToSave.end())
+                locBCALShowersToSave.push_back(static_cast<const JObject *>(locBCALShowers[j]));
+        }
 	}
   }
   	if(Candidate){
 	        if( WRITE_EVIO ) {
                 //	cout << " inv mass = " << inv_mass << " sh1 E = " << sh1_E << " sh2 E = " << sh2_E << " event num = " << eventnumber << endl;
-                cout << eventnumber << endl;
-     			 locEventWriterEVIO->Write_EVIOEvent( loop, "pi0bcalskim" );
+                //cout << "writing out " << eventnumber << endl;
+                locEventWriterEVIO->Write_EVIOEvent( loop, "pi0bcalskim", locBCALShowersToSave );
   		  }
 			}
 
