84a954ed3ecba6e0ddd6909cc97b015bb4e2ccb0
Sean Dobbs <seandobbs@gmail.com>
2016-08-09 17:47:07 -0400

    Fix BCAL_point_time for ROOT6

--- src/plugins/Calibration/BCAL_point_time/macros/plot_BCAL_point_time.C
+++ src/plugins/Calibration/BCAL_point_time/macros/plot_BCAL_point_time.C
@@ -1,16 +1,17 @@
 
 // Plot the various histograms produced by the BCAL_point_time plugin
+#include <stdio.h>
 
 
-void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_tlog_smeared.root", char prefix[255] = "05GeVgamma")
+void plot_BCAL_point_time(string filename, string prefix = "05GeVgamma")
 {
-	//	char filename[255];
+	char plot_filename[255];
 
 	gStyle->SetOptStat(0);
 
 	TDirectory *main = gDirectory;
 
-	TFile *_file0 = TFile::Open(filename);
+	TFile *_file0 = new TFile(filename.c_str());
 	TDirectory *dir = (TDirectory*)gDirectory->FindObjectAny("bcalpointtime");
 	if(dir) dir->cd();
 
@@ -27,16 +28,16 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	point_NormVsZ_layer3->Draw("same");
 	point_NormVsZ_layer4->Draw("same");
 	//thrown_NVsZ->Draw("same");
-	leg = new TLegend(0.85,0.8,0.99,0.99);
+	TLegend *leg = new TLegend(0.85,0.8,0.99,0.99);
 	leg->AddEntry(point_NormVsZ_layer1,"Layer 1");
 	leg->AddEntry(point_NormVsZ_layer2,"Layer 2");
 	leg->AddEntry(point_NormVsZ_layer3,"Layer 3");
 	leg->AddEntry(point_NormVsZ_layer4,"Layer 4");
 	leg->Draw();
-	sprintf(filename,"plots/point_NormVsZ_%s.png",prefix);
-	canvas_point_NormVsZ->Print(filename);
-	sprintf(filename,"plots/point_NormVsZ_%s.pdf",prefix);
-	canvas_point_NormVsZ->Print(filename);
+	sprintf(plot_filename,"plots/point_NormVsZ_%s.png",prefix.c_str());
+	canvas_point_NormVsZ->Print(plot_filename);
+	sprintf(plot_filename,"plots/point_NormVsZ_%s.pdf",prefix.c_str());
+	canvas_point_NormVsZ->Print(plot_filename);
 
 	TCanvas *canvas_point_NormVsTheta = new TCanvas("canvas_point_NormVsTheta","canvas_point_NormVsTheta");
 	gPad->SetGridx();
@@ -57,10 +58,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	leg->AddEntry(point_NormVsTheta_layer3,"Layer 3");
 	leg->AddEntry(point_NormVsTheta_layer4,"Layer 4");
 	leg->Draw();
-	sprintf(filename,"plots/point_NormVsTheta_%s.png",prefix);
-	canvas_point_NormVsTheta->Print(filename);
-	sprintf(filename,"plots/point_NormVsTheta_%s.pdf",prefix);
-	canvas_point_NormVsTheta->Print(filename);
+	sprintf(plot_filename,"plots/point_NormVsTheta_%s.png",prefix.c_str());
+	canvas_point_NormVsTheta->Print(plot_filename);
+	sprintf(plot_filename,"plots/point_NormVsTheta_%s.pdf",prefix.c_str());
+	canvas_point_NormVsTheta->Print(plot_filename);
 
 	TCanvas *canvas_point_TimeVsZ = new TCanvas("canvas_point_TimeVsZ","canvas_point_TimeVsZ");
 	gPad->SetGridx();
@@ -79,10 +80,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	leg->AddEntry(point_TimeVsZ_layer3_prof,"Layer 3");
 	leg->AddEntry(point_TimeVsZ_layer4_prof,"Layer 4");
 	leg->Draw();
-	sprintf(filename,"plots/point_TimeVsZ_prof_%s.png",prefix);
-	canvas_point_TimeVsZ->Print(filename);
-	sprintf(filename,"plots/point_TimeVsZ_prof_%s.pdf",prefix);
-	canvas_point_TimeVsZ->Print(filename);
+	sprintf(plot_filename,"plots/point_TimeVsZ_prof_%s.png",prefix.c_str());
+	canvas_point_TimeVsZ->Print(plot_filename);
+	sprintf(plot_filename,"plots/point_TimeVsZ_prof_%s.pdf",prefix.c_str());
+	canvas_point_TimeVsZ->Print(plot_filename);
 
 	TCanvas *canvas_hit_TimeVsZ = new TCanvas("canvas_hit_TimeVsZ","canvas_hit_TimeVsZ");
 	gPad->SetGridx();
@@ -104,10 +105,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	hitds_TimeVsZ_layer3_prof->Draw("same");
 	hitds_TimeVsZ_layer4_prof->Draw("same");
 	leg->Draw();
-	sprintf(filename,"plots/hit_TimeVsZ_prof_%s.png",prefix);
-	canvas_hit_TimeVsZ->Print(filename);
-	sprintf(filename,"plots/hit_TimeVsZ_prof_%s.pdf",prefix);
-	canvas_hit_TimeVsZ->Print(filename);
+	sprintf(plot_filename,"plots/hit_TimeVsZ_prof_%s.png",prefix.c_str());
+	canvas_hit_TimeVsZ->Print(plot_filename);
+	sprintf(plot_filename,"plots/hit_TimeVsZ_prof_%s.pdf",prefix.c_str());
+	canvas_hit_TimeVsZ->Print(plot_filename);
 
 	TCanvas *canvas_hit_TimediffVsZ = new TCanvas("canvas_TimediffVsZ","canvas_TimediffVsZ");
 	gPad->SetGridx();
@@ -121,10 +122,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	hit_TimediffVsZ_layer3_prof->Draw("same");
 	hit_TimediffVsZ_layer4_prof->Draw("same");
 	leg->Draw();
-	sprintf(filename,"plots/hit_TimediffVsZ_prof_%s.png",prefix);
-	canvas_hit_TimediffVsZ->Print(filename);
-	sprintf(filename,"plots/hit_TimediffVsZ_prof_%s.pdf",prefix);
-	canvas_hit_TimediffVsZ->Print(filename);
+	sprintf(plot_filename,"plots/hit_TimediffVsZ_prof_%s.png",prefix.c_str());
+	canvas_hit_TimediffVsZ->Print(plot_filename);
+	sprintf(plot_filename,"plots/hit_TimediffVsZ_prof_%s.pdf",prefix.c_str());
+	canvas_hit_TimediffVsZ->Print(plot_filename);
 
 	TCanvas *canvas_hit_TimesumVsZ = new TCanvas("canvas_hit_TimesumVsZ","canvas_hit_TimesumVsZ");
 	gPad->SetGridx();
@@ -139,10 +140,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	hit_TimesumVsZ_layer2_prof->Draw("same");
 	hit_TimesumVsZ_layer3_prof->Draw("same");
 	leg->Draw();
-	sprintf(filename,"plots/hit_TimesumVsZ_prof_%s.png",prefix);
-	canvas_hit_TimesumVsZ->Print(filename);
-	sprintf(filename,"plots/hit_TimesumVsZ_prof_%s.pdf",prefix);
-	canvas_hit_TimesumVsZ->Print(filename);
+	sprintf(plot_filename,"plots/hit_TimesumVsZ_prof_%s.png",prefix.c_str());
+	canvas_hit_TimesumVsZ->Print(plot_filename);
+	sprintf(plot_filename,"plots/hit_TimesumVsZ_prof_%s.pdf",prefix.c_str());
+	canvas_hit_TimesumVsZ->Print(plot_filename);
 
 
 	TCanvas *canvashit_TimeVsZ_scat = new TCanvas("canvashit_TimeVsZ_scat","canvashit_TimeVsZ_scat",800,1000);
@@ -171,10 +172,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	hitds_TimeVsZ_layer3->Draw("colz");
 	canvashit_TimeVsZ_scat->cd(8);
 	hitds_TimeVsZ_layer4->Draw("colz");
-	sprintf(filename,"plots/hit_TimeVsZ_%s.png",prefix);
-	canvashit_TimeVsZ_scat->Print(filename);
-	sprintf(filename,"plots/hit_TimeVsZ_%s.pdf",prefix);
-	canvashit_TimeVsZ_scat->Print(filename);
+	sprintf(plot_filename,"plots/hit_TimeVsZ_%s.png",prefix.c_str());
+	canvashit_TimeVsZ_scat->Print(plot_filename);
+	sprintf(plot_filename,"plots/hit_TimeVsZ_%s.pdf",prefix.c_str());
+	canvashit_TimeVsZ_scat->Print(plot_filename);
 
 
 	TCanvas *canvas_hit_TimediffVsZ_scat = new TCanvas("canvas_hit_TimediffVsZ_scat","canvas_hit_TimediffVsZ_scat",800,600);
@@ -191,10 +192,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	hit_TimediffVsZ_layer3->Draw("colz");
 	canvas_hit_TimediffVsZ_scat->cd(4);
 	hit_TimediffVsZ_layer4->Draw("colz");
-	sprintf(filename,"plots/hit_TimediffVsZ_%s.png",prefix);
-	canvas_hit_TimediffVsZ_scat->Print(filename);
-	sprintf(filename,"plots/hit_TimediffVsZ_%s.pdf",prefix);
-	canvas_hit_TimediffVsZ_scat->Print(filename);
+	sprintf(plot_filename,"plots/hit_TimediffVsZ_%s.png",prefix.c_str());
+	canvas_hit_TimediffVsZ_scat->Print(plot_filename);
+	sprintf(plot_filename,"plots/hit_TimediffVsZ_%s.pdf",prefix.c_str());
+	canvas_hit_TimediffVsZ_scat->Print(plot_filename);
 
 	TCanvas *canvas_hit_TimesumVsZ_scat = new TCanvas("canvas_hit_TimesumVsZ_scat","canvas_hit_TimesumVsZ_scat",800,600);
 	TH1I *hit_TimesumVsZ_layer1 = (TH1I*)gDirectory->FindObjectAny("hit_TimesumVsZ_layer1");
@@ -210,10 +211,10 @@ void plot_BCAL_point_time(char filename[255] = "photons_mom_0.50_angle_011_127_t
 	hit_TimesumVsZ_layer3->Draw("colz");
 	canvas_hit_TimesumVsZ_scat->cd(4);
 	hit_TimesumVsZ_layer4->Draw("colz");
-	sprintf(filename,"plots/hit_TimesumVsZ_%s.png",prefix);
-	canvas_hit_TimesumVsZ_scat->Print(filename);
-	sprintf(filename,"plots/hit_TimesumVsZ_%s.pdf",prefix);
-	canvas_hit_TimesumVsZ_scat->Print(filename);
+	sprintf(plot_filename,"plots/hit_TimesumVsZ_%s.png",prefix.c_str());
+	canvas_hit_TimesumVsZ_scat->Print(plot_filename);
+	sprintf(plot_filename,"plots/hit_TimesumVsZ_%s.pdf",prefix.c_str());
+	canvas_hit_TimesumVsZ_scat->Print(plot_filename);
 
 
 	// back to main dir
