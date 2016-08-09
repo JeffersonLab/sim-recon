d47b258d529c694e21a3ebfbc6f32900f7af6d70
Sean Dobbs <seandobbs@gmail.com>
2016-08-09 17:50:21 -0400

    Fix FDC_Efficiency for ROOT6

--- src/plugins/monitoring/FDC_Efficiency/FDC_Efficiency.C
+++ src/plugins/monitoring/FDC_Efficiency/FDC_Efficiency.C
@@ -314,7 +314,7 @@ void FDC_Efficiency(bool save = 0){
   
   cAlignment->cd(1);
   TGraphErrors *galignV = new TGraphErrors(24, cell[0], meanV, cell_err[0], meanV_err);
-  galignV->SetTitle("Alignment along Wire (Error Bars: Sigma); Cell \# ; V_{Hit} - V_{Track} (cm)");
+  galignV->SetTitle("Alignment along Wire (Error Bars: Sigma); Cell # ; V_{Hit} - V_{Track} (cm)");
   galignV->SetMarkerColor(1);
   galignV->SetMarkerStyle(2);
   galignV->Draw("AP");
@@ -337,7 +337,7 @@ void FDC_Efficiency(bool save = 0){
 
   cAlignment->cd(2);
   TGraphErrors *galignU = new TGraphErrors(24, cell[0], meanU, cell_err[0], meanU_err);
-  galignU->SetTitle("Alignment perp. to Wire (Error Bars: Sigma); Cell \# ; U_{Hit} - U_{Track} (cm)");
+  galignU->SetTitle("Alignment perp. to Wire (Error Bars: Sigma); Cell # ; U_{Hit} - U_{Track} (cm)");
   galignU->SetMarkerColor(1);
   galignU->SetMarkerStyle(2);
   galignU->Draw("AP");
@@ -350,7 +350,7 @@ void FDC_Efficiency(bool save = 0){
   TGraphErrors *gmagnet[9];
   for (unsigned int r=0; r<9; r++){
     gmagnet[r] = new TGraphErrors(24, cell[r], slope[r], cell_err[r], slope_err[r]);
-    gmagnet[r]->SetTitle("Value for slope of magnetic deflection; Cell \# ; Slope (cm^{-1})");
+    gmagnet[r]->SetTitle("Value for slope of magnetic deflection; Cell # ; Slope (cm^{-1})");
     gmagnet[r]->SetMarkerColor(r+1);
     gmagnet[r]->SetMarkerStyle(2);
     if (r==0)
