ab75adc1c53c226cbcb85ab153b760f354ba5463
Alex Austregesilo <aaustreg@jlab.org>
2016-08-31 10:30:09 -0400

    reduce the number of histograms by making some of them optional

--- src/plugins/monitoring/FDC_Efficiency/FDC_Efficiency.C
+++ src/plugins/monitoring/FDC_Efficiency/FDC_Efficiency.C
@@ -1,6 +1,6 @@
 
 void FDC_Efficiency(bool save = 0){
-  
+
   //gDirectory->cd();
   //TDirectory *gDirectory = TFile::Open("hd_root.root");
   TDirectory *dir = (TDirectory*)gDirectory->FindObjectAny("FDC_Efficiency");
@@ -251,14 +251,16 @@ void FDC_Efficiency(bool save = 0){
     
   }
 
+  const unsigned int rad = 1;
+
   TCanvas *cResidual_Pseudo2 = new TCanvas("cResidual_Pseudo2", "Pseudo Hit Resolution 2D", 1000, 800);
   cResidual_Pseudo2->Divide(6,4);
 
   for(unsigned int icell=1; icell<=24; icell++){
     cResidual_Pseudo2->cd(icell);
-    for (unsigned int r=0; r<9; r++){
+    for (unsigned int r=0; r<rad; r++){
       char hname7[256];
-      sprintf(hname7, "hPseudoResUvsV_cell[%d]_radius[%d]", icell, (r+1)*5);
+      sprintf(hname7, "hPseudoResUvsV_cell[%d]_radius[%d]", icell, (r+1)*(45/rad));
       TH2 *h7 = (TH2*)(gDirectory->Get(hname7));
       
       h7->GetYaxis()->SetTitle("Position reconstructed along Wire (cm)");
@@ -271,20 +273,20 @@ void FDC_Efficiency(bool save = 0){
     }
   }
 
-  double slope[9][24];
-  double slope_err[9][24];
-  double cell[9][24];
-  double cell_err[9][24];
+  double slope[rad][24];
+  double slope_err[rad][24];
+  double cell[rad][24];
+  double cell_err[rad][24];
   TCanvas *cResidual_Profile = new TCanvas("cResidual_Profile", "Pseudo Resolution Profile", 1000, 800);
   cResidual_Profile->Divide(6,4);
   
   for(unsigned int icell=1; icell<=24; icell++){
     cResidual_Profile->cd(icell);
-    for (unsigned int r=0; r<9; r++){
+    for (unsigned int r=0; r<rad; r++){
       cell[r][icell] = icell + 0.1*r;
       cell_err[r][icell] = 0;
       char hname8[256];
-      sprintf(hname8, "hPseudoResUvsV_cell[%d]_radius[%d]_pfy", icell, (r+1)*5);
+      sprintf(hname8, "hPseudoResUvsV_cell[%d]_radius[%d]_pfy", icell, (r+1)*(45/rad));
       TH1 *h8 = (TH1*)(gDirectory->Get(hname8));
       
       h8->GetXaxis()->SetTitle("Position reconstructed along Wire (cm)");
@@ -314,7 +316,7 @@ void FDC_Efficiency(bool save = 0){
   
   cAlignment->cd(1);
   TGraphErrors *galignV = new TGraphErrors(24, cell[0], meanV, cell_err[0], meanV_err);
-  galignV->SetTitle("Alignment along Wire (Error Bars: Sigma); Cell # ; V_{Hit} - V_{Track} (cm)");
+  galignV->SetTitle("Alignment along Wire (Error Bars: Sigma); Cell \# ; V_{Hit} - V_{Track} (cm)");
   galignV->SetMarkerColor(1);
   galignV->SetMarkerStyle(2);
   galignV->Draw("AP");
@@ -337,7 +339,7 @@ void FDC_Efficiency(bool save = 0){
 
   cAlignment->cd(2);
   TGraphErrors *galignU = new TGraphErrors(24, cell[0], meanU, cell_err[0], meanU_err);
-  galignU->SetTitle("Alignment perp. to Wire (Error Bars: Sigma); Cell # ; U_{Hit} - U_{Track} (cm)");
+  galignU->SetTitle("Alignment perp. to Wire (Error Bars: Sigma); Cell \# ; U_{Hit} - U_{Track} (cm)");
   galignU->SetMarkerColor(1);
   galignU->SetMarkerStyle(2);
   galignU->Draw("AP");
@@ -347,10 +349,10 @@ void FDC_Efficiency(bool save = 0){
   lpack34->Draw();
 
   cAlignment->cd(3);
-  TGraphErrors *gmagnet[9];
-  for (unsigned int r=0; r<9; r++){
+  TGraphErrors *gmagnet[rad];
+  for (unsigned int r=0; r<rad; r++){
     gmagnet[r] = new TGraphErrors(24, cell[r], slope[r], cell_err[r], slope_err[r]);
-    gmagnet[r]->SetTitle("Value for slope of magnetic deflection; Cell # ; Slope (cm^{-1})");
+    gmagnet[r]->SetTitle("Value for slope of magnetic deflection; Cell \# ; Slope (cm^{-1})");
     gmagnet[r]->SetMarkerColor(r+1);
     gmagnet[r]->SetMarkerStyle(2);
     if (r==0)
