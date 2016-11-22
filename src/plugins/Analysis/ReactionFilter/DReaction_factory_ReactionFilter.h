8421653b43a9917204a8b6569c5754ae8f5080f0
pmattjlab <pmatt@jlab.org>
2016-11-21 22:51:34 -0500

    Plugin for creating trees for many final states.
    Tweaked from work by Ryan Mitchell.

---
+++ src/plugins/Analysis/ReactionFilter/DReaction_factory_ReactionFilter.h
@@ -0,0 +1,68 @@
+// $Id$
+//
+//    File: DReaction_factory_ReactionFilter.h
+// Created: Mon Nov 21 17:54:40 EST 2016
+// Creator: pmatt (on Darwin Pauls-MacBook-Pro-2.local 13.4.0 i386)
+//
+
+#ifndef _DReaction_factory_ReactionFilter_
+#define _DReaction_factory_ReactionFilter_
+
+#include <iostream>
+#include <iomanip>
+
+#include <JANA/JFactory.h>
+#include <ANALYSIS/DReaction.h>
+#include <ANALYSIS/DHistogramActions.h>
+#include <ANALYSIS/DCutActions.h>
+
+#include "DCustomAction_dEdxCut.h"
+
+using namespace std;
+using namespace jana;
+
+class DReaction_factory_ReactionFilter : public jana::JFactory<DReaction>
+{
+	public:
+		DReaction_factory_ReactionFilter()
+		{
+			// This is so that the created DReaction objects persist throughout the life of the program instead of being cleared each event. 
+			SetFactoryFlag(PERSISTANT);
+		}
+		const char* Tag(void){return "ReactionFilter";}
+
+	private:
+		jerror_t init(void);
+		jerror_t brun(JEventLoop* locEventLoop, int32_t locRunNumber);
+		jerror_t evnt(JEventLoop* locEventLoop, uint64_t locEventNumber);
+		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+
+		// Create Reaction Steps
+		void Create_FirstStep(DReaction* locReaction, FSInfo* locFSInfo);
+		void Create_DecaySteps(DReaction* locReaction, FSInfo* locFSInfo);
+		void Create_DecayStep(DReaction* locReaction, FSInfo* locFSInfo, Particle_t locPID);
+
+		// Actions & cuts
+		void Define_LooseCuts(void);
+		void Add_PreComboCuts(DReaction* locReaction, FSInfo* locFSInfo);
+		void Add_PIDActions(DReaction* locReaction);
+		void Add_MassHistograms(DReaction* locReaction, FSInfo* locFSInfo, bool locUseKinFitResultsFlag, string locBaseUniqueName = "");
+
+		// User-input channels
+		deque<FSInfo*> dFSInfos;
+
+		// Keep track of DReactionSteps that have been created for decaying particles
+		// These can be re-used between DReactions, allowing the analysis library to save memory for combo steps
+		map<pair<Particle_t, bool>, set<DReactionStep*> > dDecayStepMap_All; //bool: true for mass constrained by kinfit, false if not
+
+		// Cuts
+		map<Particle_t, pair<double, double> > dMissingMassCuts; //Unknown = none missing //if negative, uses missing mass squared instead
+		map<Particle_t, pair<double, double> > dInvariantMassCuts;
+		map<Particle_t, map<DetectorSystem_t, double> > dPIDTimingCuts;
+
+		double dBeamBunchPeriod;
+		deque<DReactionStep*> dReactionStepPool; //to prevent memory leaks
+};
+
+#endif // _DReaction_factory_ReactionFilter_
+
---
+++ src/plugins/Analysis/ReactionFilter/FSInfo.h
@@ -0,0 +1,124 @@
+#ifndef FSINFO_H
+#define FSINFO_H
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <cstdlib>
+#include <ctype.h>
+
+#include "particleType.h"
+
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+//
+//   The FSInfo class:  this class holds information about a
+//                      given final state (FS)
+//
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+
+class FSInfo
+{
+	public:
+
+		FSInfo(string FSName);
+
+		// the name of the final state
+		//  (this encodes a list of the final state particles
+		//   and directions about whether to reconstruct them
+		//   inclusively or exclusively)
+		string FSName(void) { return m_FSName; }
+
+		// compact string name of the reaction
+		string ReactionName(void) { return m_ReactionName; }
+
+		// is this an inclusive or exclusive final state?
+		bool exclusive(void) const{ return (m_FSName.find("EXC") != string::npos); }
+		bool inclusive(void) const{ return (m_FSName.find("INC") != string::npos); }
+		bool missingN(void)  const{ return (m_FSName.find("MIN") != string::npos); }
+
+		// use intermediate mass fits (true, unless NIMF is in the FS name)
+		bool intermediateMassFits(void) const{ return !(m_FSName.find("NIMF") != string::npos); }
+
+		// a list of particles associated with this final state
+		vector<Particle_t>& PIDs(void) { return m_PIDs; }
+
+		// total electric charge of all final state particles
+		int totalCharge(void) const{ return m_totalCharge; };
+
+	private:
+
+		// private member data
+		string m_FSName;
+		vector<Particle_t> m_PIDs;
+		int m_totalCharge;
+		string m_ReactionName;
+
+		// get short name (for building reaction/tree names)
+		string Get_ShortName(Particle_t locPID) const;
+
+		// functions to unpack the final state name and parse strings
+		vector<Particle_t> getPIDsFromFSName( const string& FSName );
+		vector<Particle_t> getPIDsFromFSName( const string& FSName, string& locReactionName);
+		int getTotalChargeFromPIDs const( const vector<Particle_t>& locPIDs );
+};
+
+#endif
+
+inline int FSInfo::getTotalChargeFromPIDs( const vector<Particle_t>& locPIDs ) const
+{
+	int totalCharge = 0;
+	for(auto locPID : locPIDs)
+		totalCharge += ParticleCharge(locPID);
+	return totalCharge;
+}
+
+inline vector<Particle_t> FSInfo::getPIDsFromFSName( const string& FSName)
+{
+	string locReactionName;
+	return getPIDsFromFSName(FSName, locReactionName);
+}
+
+inline string FSInfo::Get_ShortName(Particle_t locPID) const
+{
+	switch (locPID) {
+	case Gamma:
+		return "g";
+	case Positron:
+		return "ep";
+	case Electron:
+		return "em";
+	case MuonPlus:
+		return "mup";
+	case MuonMinus:
+		return "mum";
+	case Pi0:
+		return "pi0";
+	case PiPlus:
+		return "pip";
+	case PiMinus:
+		return "pim";
+	case KPlus:
+		return "kp";
+	case KMinus:
+		return "km";
+	case Proton:
+		return ""; //is understood
+	case AntiProton:
+		return "pbar";
+	case KShort:
+		return "ks";
+	case Eta:
+		return "eta";
+	case Lambda:
+		return "lambda";
+	case AntiLambda:
+		return "lbar";
+}
+
