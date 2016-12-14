4ec46a8c059afa54552eb07dd6697277d6405900
Michael Staib <mstaib@cmu.edu>
2016-12-14 16:31:50 +0000

    Add macros for plotting FDC residuals in local coordinates.

---
+++ src/plugins/monitoring/TrackingPulls/PlotWireResidualsLocal.C
@@ -0,0 +1,19 @@
+{
+   TCanvas *cWireRes = new TCanvas("FDCWireResidualsLocal","FDCWireResidualsLocal",1200,1000);
+   cWireRes->Divide(6,4);
+
+   TProfile2D *thisWireProfile;
+   char histoWName[256];
+   char histoWTitle[256];
+
+   for(auto i=1 ; i <=24; i++){
+      cWireRes->cd(i);
+      sprintf(histoWName, "/TrackingPulls/FDCPulls_Plane%.2i/2D Wire Hit Residuals Local", i);
+      sprintf(histoWTitle, "2D Wire Hit Residuals Local Plane %.2i", i);
+      gDirectory->GetObject(histoWName, thisWireProfile);
+      thisWireProfile->SetMinimum(-0.025);
+      thisWireProfile->SetMaximum(0.025);
+      thisWireProfile->SetTitle(histoWTitle);
+      thisWireProfile->Draw("colz");
+   }
+}
