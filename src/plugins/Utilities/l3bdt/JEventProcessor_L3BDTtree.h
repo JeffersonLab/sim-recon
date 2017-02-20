7e24171f8ab46dcb18e1516982085b65482d920c
faustus123 <davidl@jlab.org>
2017-02-19 21:01:23 -0500

    Refactored to include many more input variables. Removed is_good variable and will specify via training macro instead.

--- src/plugins/Utilities/l3bdt/JEventProcessor_L3BDTtree.h
+++ src/plugins/Utilities/l3bdt/JEventProcessor_L3BDTtree.h
@@ -12,6 +12,156 @@
 
 #include <JANA/JEventProcessor.h>
 
+#include <DAQ/Df250TriggerTime.h>
+#include <DAQ/Df250PulseData.h>
+#include <DAQ/Df125TriggerTime.h>
+#include <DAQ/Df125PulseIntegral.h>
+#include <DAQ/Df125PulseTime.h>
+#include <DAQ/Df125PulsePedestal.h>
+#include <DAQ/Df125CDCPulse.h>
+#include <DAQ/Df125FDCPulse.h>
+#include <DAQ/DF1TDCHit.h>
+#include <DAQ/DF1TDCTriggerTime.h>
+#include <DAQ/DCAEN1290TDCHit.h>
+#include <DAQ/DL1Info.h>
+#include <DAQ/DEventTag.h>
+#include <PID/DVertex.h>
+
+#include <BCAL/DBCALDigiHit.h>
+#include <BCAL/DBCALTDCDigiHit.h>
+#include <CDC/DCDCDigiHit.h>
+#include <FCAL/DFCALDigiHit.h>
+#include <FDC/DFDCWireDigiHit.h>
+#include <FDC/DFDCCathodeDigiHit.h>
+#include <PAIR_SPECTROMETER/DPSCTDCDigiHit.h>
+#include <PAIR_SPECTROMETER/DPSCDigiHit.h>
+#include <PAIR_SPECTROMETER/DPSDigiHit.h>
+#include <START_COUNTER/DSCDigiHit.h>
+#include <START_COUNTER/DSCTDCDigiHit.h>
+#include <START_COUNTER/DSCHit.h>
+#include <TAGGER/DTAGHDigiHit.h>
+#include <TAGGER/DTAGHTDCDigiHit.h>
+#include <TAGGER/DTAGMDigiHit.h>
+#include <TAGGER/DTAGMTDCDigiHit.h>
+#include <TOF/DTOFTDCDigiHit.h>
+#include <TOF/DTOFDigiHit.h>
+#include <TOF/DTOFHit.h>
+#include <TOF/DTOFPoint.h>
+#include <TPOL/DTPOLRingDigiHit.h>
+#include <TPOL/DTPOLSectorDigiHit.h>
+
+#include <BCAL/DBCALPoint.h>
+#include <BCAL/DBCALCluster.h>
+#include <FCAL/DFCALCluster.h>
+#include <PID/DNeutralParticle.h>
+#include <PID/DNeutralShower.h>
+#include <PID/DChargedTrack.h>
+#include <PID/DBeamPhoton.h>
+#include <PID/DEventRFBunch.h>
+#include <TRACKING/DTrackCandidate.h>
+#include <TRACKING/DTrackWireBased.h>
+#include <TRACKING/DTrackTimeBased.h>
+
+#define MyTypes(X) \
+	X(Df250TriggerTime) \
+	X(Df250PulseData) \
+	X(Df125TriggerTime) \
+	X(Df125PulseIntegral) \
+	X(Df125PulseTime) \
+	X(Df125PulsePedestal) \
+	X(Df125CDCPulse) \
+	X(Df125FDCPulse) \
+	X(DF1TDCHit) \
+	X(DF1TDCTriggerTime) \
+	X(DCAEN1290TDCHit) \
+	X(DL1Info) \
+	X(DEventTag) \
+	X(DVertex) \
+	\
+	X(DBCALDigiHit) \
+	X(DBCALTDCDigiHit) \
+	X(DCDCDigiHit) \
+	X(DFCALDigiHit) \
+	X(DFDCWireDigiHit) \
+	X(DFDCCathodeDigiHit) \
+	X(DPSCTDCDigiHit) \
+	X(DPSCDigiHit) \
+	X(DPSDigiHit) \
+	X(DSCDigiHit) \
+	X(DSCTDCDigiHit) \
+	X(DTAGHDigiHit) \
+	X(DTAGHTDCDigiHit) \
+	X(DTAGMDigiHit) \
+	X(DTAGMTDCDigiHit) \
+	X(DTOFTDCDigiHit) \
+	X(DTOFDigiHit) \
+	X(DTPOLRingDigiHit) \
+	X(DTPOLSectorDigiHit) \
+	\
+	X(DBCALPoint) \
+	X(DBCALCluster) \
+	X(DFCALCluster) \
+	X(DNeutralParticle) \
+	X(DNeutralShower) \
+	X(DChargedTrack) \
+	X(DBeamPhoton) \
+	X(DEventRFBunch) \
+	X(DSCHit) \
+	X(DTOFHit) \
+	X(DTOFPoint) \
+	X(DTrackCandidate) \
+	X(DTrackWireBased) \
+	X(DTrackTimeBased)
+
+#define MyIntBranchTypes(X) \
+	X(trig_mask) \
+	X(fp_trig_mask) \
+	\
+	X(NCDC_superlayer1) \
+	X(NCDC_superlayer2) \
+	X(NCDC_superlayer3) \
+	X(NCDC_superlayer4) \
+	X(NCDC_superlayer5) \
+	X(NFDCwires_package1) \
+	X(NFDCwires_package2) \
+	X(NFDCwires_package3) \
+	X(NFDCwires_package4) \
+	X(NFDCCathodes_package1) \
+	X(NFDCCathodes_package2) \
+	X(NFDCCathodes_package3) \
+	X(NFDCCathodes_package4) \
+	X(NTOF_half_length) \
+	X(NTOF_half_width) \
+	\
+	X(Nbeam_photons_coherent) \
+	X(Nbeam_photons_3_4) \
+	X(Nbeam_photons_4_5) \
+	X(Nbeam_photons_5_6) \
+	X(Nbeam_photons_6_7) \
+	X(Nbeam_photons_7_8) \
+	X(Nbeam_photons_8_9) \
+	X(Nbeam_photons_9_10) \
+	X(Nbeam_photons_10_11) \
+	X(Nbeam_photons_11_12)
+
+#define MyFloatBranchTypes(X) \
+	X(Esc_tot) \
+	X(Etof_tot) \
+	X(Ebcal_points) \
+	X(Ebcal_clusters) \
+	X(Efcal_clusters) \
+	X(Rfcal_max) \
+	X(Rfcal_min) \
+	\
+	X(Ptot_candidates) \
+	\
+	X(Evisible_neutrals) \
+	X(Evisible_tracks) \
+	X(Evisible_charged_Kaons) \
+	X(Evisible_charged_pions) \
+	X(Evisible_protons) \
+	X(Evisible)
+
 class JEventProcessor_L3BDTtree:public jana::JEventProcessor{
 	public:
 		JEventProcessor_L3BDTtree();
@@ -20,30 +170,21 @@ class JEventProcessor_L3BDTtree:public jana::JEventProcessor{
 		
 		class bdt_params_t{
 			public:
-				Int_t   Nstart_counter;
-				Int_t   Ntof;
-				Int_t   Nbcal_points;
-				Int_t   Nbcal_clusters;
-				Float_t Ebcal_points;
-				Float_t Ebcal_clusters;
-				Int_t   Nfcal_clusters;
-				Float_t Efcal_clusters;
-				Int_t   Ntrack_candidates;
-				Float_t Ptot_candidates;
-				Int_t   Npshits;
-				Int_t   Npschits;
-				
-				Int_t   is_good;
-				
-				Float_t Evisible;
-				Float_t FCAL_rmax;
-				Float_t FCAL_rmin;
-				
+				// Include number of objects for all JANA types
+				#define Nobjs(A) Int_t N##A;
+				MyTypes(Nobjs)
+
+				// Include sorted int types
+				#define Intobjs(A) Int_t A;
+				MyIntBranchTypes(Intobjs)
+
+				// Include sorted float types
+				#define Floatobjs(A) Int_t A;
+				MyFloatBranchTypes(Floatobjs)
 		};
 		
 		TTree *l3tree;
 		bdt_params_t bdt;
-		double min_visible_GeV;
 
 	private:
 		jerror_t init(void);						///< Called once at program start.
