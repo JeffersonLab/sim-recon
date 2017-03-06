4d8a4a5c085cfb63beaab9b82b7cbd897784b5f5
David Lawrence <davidl@jlab.org>
2017-03-06 14:36:54 -0500

    Parse the EventTag bank and create DEventTag objects. Also include new mva_response member.

--- src/libraries/DAQ/DEventTag.h
+++ src/libraries/DAQ/DEventTag.h
@@ -16,13 +16,18 @@ class DEventTag:public jana::JObject{
 	public:
 		JOBJECT_PUBLIC(DEventTag);
 
-		DEventTag(uint64_t es=0L, DL3Trigger::L3_decision_t d=DL3Trigger::kNO_DECISION, uint64_t l3s=0, uint32_t l3a=0)
-			:event_status(es),L3_decision(d),L3_status(l3s),L3_algorithm(l3a){}
+		DEventTag(uint64_t es=0L, DL3Trigger::L3_decision_t d=DL3Trigger::kNO_DECISION, uint64_t l3s=0, uint32_t l3a=0, double mva=3E-6)
+			:event_status(es),L3_decision(d),L3_status(l3s),L3_algorithm(l3a),mva_encoded(mva){
+				mva_response = ((double)(mva_encoded & 0x7FFFFFFF))/1000.0;
+				if( mva_encoded & 0x80000000 ) mva_response = -mva_response;
+			}
 		
 		uint64_t event_status;                   ///< JANA event status word when event was written
 		DL3Trigger::L3_decision_t L3_decision;   ///< L3 decision when event was written
 		uint64_t L3_status;                      ///< L3 status word when event was written
 		uint32_t L3_algorithm;                   ///< L3 algorithm identifier when event was written
+		double   mva_response;                   ///< L3 MVA response (if any)
+		uint32_t mva_encoded;                    ///< same as above but encoded as unsigned int (for debugging)
 		
 		// This method is used primarily for pretty printing
 		// the second argument to AddString is printf style format
@@ -31,6 +36,7 @@ class DEventTag:public jana::JObject{
 			AddString(items, "L3_decision" , "%d"     , L3_decision);
 			AddString(items, "L3_status"   , "0x%016x" , L3_status);
 			AddString(items, "L3_algorithm", "0x%08x"  , L3_algorithm);
+			AddString(items, "L3_mva_response", "0x%5.3f"  , mva_response);
 		}
 
 };
