1d16096ada4a73cec7c173c3a040a6cf56b7aa9a
Elton Smith <elton@jlab.org>
2017-07-03 12:04:51 -0400

    replace original TwoPiPlotGenerator files

---
+++ src/libraries/AMPTOOLS_DATAIO/TwoPiPlotGenerator.cc
@@ -0,0 +1,79 @@
+#include "TLorentzVector.h"
+#include "TLorentzRotation.h"
+#include "TLorentzVector.h"
+#include "TLorentzRotation.h"
+
+#include "AMPTOOLS_DATAIO/TwoPiPlotGenerator.h"
+#include "IUAmpTools/Histogram1D.h"
+#include "IUAmpTools/Kinematics.h"
+
+TwoPiPlotGenerator::TwoPiPlotGenerator( const FitResults& results ) :
+PlotGenerator( results )
+{
+  // calls to bookHistogram go here
+  
+  // bookHistogram( k2PiMass, new Histogram1D( 200, 0., 2.0, "M2pi", "Invariant Mass of #pi^{+} #pi^{-}") );
+  bookHistogram( k2PiMass, new Histogram1D( 200, 0.2, 0.8, "M2pi", "Invariant Mass of #pi^{+} #pi^{-}") );
+  bookHistogram( kPiPCosTheta, new Histogram1D( 50, -1., 1., "cosTheta", "cos( #theta ) of Resonance Production") );
+
+  bookHistogram( kPhiPiPlus,  new Histogram1D( 50, -1*PI, PI, "PhiPiPlus",  "#Phi_{#pi_{+}}" ) );
+  bookHistogram( kPhiPiMinus, new Histogram1D( 50, -1*PI, PI, "PhiPiMinus", "#Phi_{#pi_{-}}" ) );
+  bookHistogram( kPhi, new Histogram1D( 50, -1*PI, PI, "Phi", "#Phi" ) );
+  bookHistogram( kphi, new Histogram1D( 50, -1*PI, PI, "phi", "#phi" ) );
+  bookHistogram( kPsi, new Histogram1D( 50, -1*PI, PI, "psi", "#psi" ) );
+  // bookHistogram( kt, new Histogram1D( 100, 0, 5, "t", "-t" ) );
+  bookHistogram( kt, new Histogram1D( 100, 0, 0.05, "t", "-t" ) );
+}
+
+void
+TwoPiPlotGenerator::projectEvent( Kinematics* kin ){
+  
+  TLorentzVector beam   = kin->particle( 0 );
+  TLorentzVector recoil = kin->particle( 1 );
+  TLorentzVector p1 = kin->particle( 2 );
+  TLorentzVector p2 = kin->particle( 3 );
+
+  TLorentzVector resonance = p1 + p2; 
+  TLorentzRotation resonanceBoost( -resonance.BoostVector() );
+
+  TLorentzVector recoil_res = resonanceBoost * recoil;
+  TLorentzVector p1_res = resonanceBoost * p1;
+
+  // normal to the production plane
+  TVector3 y = (beam.Vect().Unit().Cross(-recoil.Vect().Unit())).Unit();
+  
+  // choose helicity frame: z-axis opposite recoil proton in rho rest frame
+  TVector3 z = -1. * recoil_res.Vect().Unit();
+  TVector3 x = y.Cross(z).Unit();
+  TVector3 angles(   (p1_res.Vect()).Dot(x),
+                     (p1_res.Vect()).Dot(y),
+                     (p1_res.Vect()).Dot(z) );
+
+  GDouble cosTheta = angles.CosTheta();
+  
+  GDouble phi = angles.Phi();
+  
+  TVector3 eps(1.0, 0.0, 0.0); // beam polarization vector
+  GDouble Phi = atan2(y.Dot(eps), beam.Vect().Unit().Dot(eps.Cross(y)));
+
+  GDouble psi = phi - Phi;
+  if(psi < -1*PI) psi += 2*PI;
+  if(psi > PI) psi -= 2*PI;
+
+  // compute invariant t
+  GDouble t = - 2* recoil.M() * (recoil.E()-recoil.M());
+
+  // calls to fillHistogram go here
+  
+  fillHistogram( k2PiMass, ( resonance ).M() );
+  
+  fillHistogram( kPiPCosTheta, cosTheta );
+
+  fillHistogram( kPhiPiPlus,  p1.Phi() );
+  fillHistogram( kPhiPiMinus, p2.Phi() );
+  fillHistogram( kPhi, Phi );
+  fillHistogram( kphi, phi );
+
+  fillHistogram( kPsi, psi );
+  fillHistogram( kt, -t );      // fill with -t to make positive
+}
---
+++ src/libraries/AMPTOOLS_DATAIO/TwoPiPlotGenerator.h
@@ -0,0 +1,30 @@
+#if !(defined TWOPIPLOTGENERATOR)
+#define TWOPIPLOTGENERATOR
+
+#include <vector>
+#include <string>
+
+#include "IUAmpTools/PlotGenerator.h"
+
+using namespace std;
+
+class FitResults;
+class Kinematics;
+
+class TwoPiPlotGenerator : public PlotGenerator
+{
+    
+public:
+  
+  // create an index for different histograms
+  enum { k2PiMass = 0, kPiPCosTheta, kPhiPiPlus, kPhiPiMinus, kPhi, kphi, kPsi, kt, kNumHists};
+  
+  TwoPiPlotGenerator( const FitResults& results );
+    
+private:
+        
+  void projectEvent( Kinematics* kin );
+  
+};
+
+#endif
