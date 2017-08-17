9018e55c70694b0af143c610a786543c43ce83ff
faustus123 <davidl@jlab.org>
2017-08-17 16:31:35 -0400

    Switch to logz scale and fix labels for density plot.

--- src/programs/Utilities/material2root/density_vs_r_vs_z.C
+++ src/programs/Utilities/material2root/density_vs_r_vs_z.C
@@ -12,17 +12,23 @@ void density_vs_r_vs_z(void)
 	c1->Divide(1,2);
 	
 	c1->cd(1);
+	gPad->SetLogz();
 	gPad->SetRightMargin(0.15);
-	density_LL->GetZaxis()->SetRangeUser(0.0, 1.25);
+	density_LL->GetZaxis()->SetRangeUser(1.0E-3, 1.25);
 	density_LL->SetStats(0);
 	density_LL->SetTitle("Material Map from FindMatLL(...)");
+	density_LL->GetZaxis()->SetTitle("density (g/cm^{3})");
+	density_table->GetZaxis()->SetRangeUser(0.001, 1.5);
 	density_LL->Draw("colz");
 
 	c1->cd(2);
+	gPad->SetLogz();
 	gPad->SetRightMargin(0.15);
-	density_table->GetZaxis()->SetRangeUser(0.0, 1.25);
+	density_table->GetZaxis()->SetRangeUser(1.0E-3, 1.25);
 	density_table->SetStats(0);
 	density_table->SetTitle("Material Map from FindMatTable(...)");
+	density_table->GetZaxis()->SetTitle("density (g/cm^{3})");
+	density_table->GetZaxis()->SetRangeUser(0.001, 1.5);
 	density_table->Draw("colz");
 	
 	c1->SaveAs("density_vs_r_vs_z.png");
