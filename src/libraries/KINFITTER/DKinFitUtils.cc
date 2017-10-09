ca3c281bbaf388e313d404b13c07e75663fdbc10
pmattjlab <pmatt@jlab.org>
2017-10-09 11:21:13 -0400

    Update trackeff_missing, fix kinfit missing p4 bug.

--- src/libraries/KINFITTER/DKinFitUtils.cc
+++ src/libraries/KINFITTER/DKinFitUtils.cc
@@ -86,6 +86,7 @@ shared_ptr<DKinFitParticle> DKinFitUtils::Make_TargetParticle(int locPID, int lo
 	locKinFitParticle->Set_Charge(locCharge);
 	locKinFitParticle->Set_Mass(locMass);
 	locKinFitParticle->Set_KinFitParticleType(d_TargetParticle);
+	locKinFitParticle->Set_VertexP4AtProductionVertex(true);
 
 	if(dDebugLevel > 5)
 	{
@@ -154,6 +155,7 @@ shared_ptr<DKinFitParticle> DKinFitUtils::Make_MissingParticle(int locPID, int l
 	locKinFitParticle->Set_Charge(locCharge);
 	locKinFitParticle->Set_Mass(locMass);
 	locKinFitParticle->Set_KinFitParticleType(d_MissingParticle);
+	locKinFitParticle->Set_VertexP4AtProductionVertex(true);
 
 	if(dDebugLevel > 5)
 	{
