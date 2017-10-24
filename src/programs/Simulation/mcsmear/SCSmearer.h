6dc64d4c8f2104555d50358e5968e51f516c6e1a
Sean Dobbs <seandobbs@gmail.com>
2017-10-24 15:30:43 -0400

    Add z-dependent timing resolutions to mcsmear along with a factor to remove double-counting of tracking resolutions

--- src/programs/Simulation/mcsmear/SCSmearer.h
+++ src/programs/Simulation/mcsmear/SCSmearer.h
@@ -11,9 +11,30 @@ class sc_config_t
   public:
 	sc_config_t(JEventLoop *loop);
 
-	inline double GetPaddleTimeResolution(int sector)  { 
-		return START_TIME_RESOLUTIONS.at(sector-1); 
+    double GetPaddleTimeResolution(int sector, double sc_local_z)  { 
+        double time_resolution = 0.;
+
+        if(sc_local_z < SC_BOUNDARY1[sector]) {
+            time_resolution = SC_SECTION1_P0[sector] + SC_SECTION1_P1[sector]*sc_local_z;
+        } else if(sc_local_z < SC_BOUNDARY2[sector]) {
+            time_resolution = SC_SECTION2_P0[sector] + SC_SECTION2_P1[sector]*sc_local_z;
+        } else {
+            time_resolution = SC_SECTION3_P0[sector] + SC_SECTION3_P1[sector]*sc_local_z;
+        }
+        
+        // max sure that we aren't getting some ridiculously large resolution
+        if(time_resolution > SC_MAX_RESOLUTION[sector])
+            time_resolution = SC_MAX_RESOLUTION[sector];
+    
+        // If these resolutions come from data, apply correction factors to remove any other contributions
+        time_resolution = (time_resolution - SC_MC_CORRECTION_P0) / SC_MC_CORRECTION_P1;
+
+        // convert ps to ns
+        time_resolution /= 1000.;
+        cout << " time resolution = " << time_resolution << endl;
+        return time_resolution;
 	}
+
 	double GetEfficiencyCorrectionFactor(int sector) {
 		return paddle_efficiencies.at(sector-1);
 	}
@@ -22,8 +43,20 @@ class sc_config_t
 	double START_PHOTONS_PERMEV;
 	double START_PADDLE_THRESHOLD;
 
-	vector<double> START_TIME_RESOLUTIONS;
 	vector<double> paddle_efficiencies;
+
+    // Start counter geometry parameters
+    vector<double> SC_START_Z;
+
+    // Start counter resolution parameters
+    vector<double> SC_MAX_RESOLUTION;
+    vector<double> SC_BOUNDARY1, SC_BOUNDARY2;
+    vector<double> SC_SECTION1_P0, SC_SECTION1_P1;
+    vector<double> SC_SECTION2_P0, SC_SECTION2_P1;
+    vector<double> SC_SECTION3_P0, SC_SECTION3_P1;
+
+    double SC_MC_CORRECTION_P0, SC_MC_CORRECTION_P1;
+
 };
 
 
@@ -38,6 +71,9 @@ class SCSmearer : public Smearer
 	}
 	
 	void SmearEvent(hddm_s::HDDM *record);
+
+  protected:
+    hddm_s::StcTruthPointList::iterator FindMatchingTruthPoint(hddm_s::StcTruthHitList::iterator hiter, hddm_s::StcTruthPointList &truthPoints);
 	
   private:
   	sc_config_t  *sc_config;
