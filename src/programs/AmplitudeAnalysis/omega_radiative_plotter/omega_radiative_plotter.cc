4ffe1465aa998e2db169afc7c104c7294a4139a9
William McGinley <wmcginle@maria.phys.cmu.edu>
2018-01-05 21:28:16 +0000

    remove obsolete BCAL nonlinear energy corrections

--- src/programs/AmplitudeAnalysis/omega_radiative_plotter/omega_radiative_plotter.cc
+++ src/programs/AmplitudeAnalysis/omega_radiative_plotter/omega_radiative_plotter.cc
@@ -146,7 +146,7 @@ int main( int argc, char* argv[] ){
         else continue;
 
         if (iplot == PlotGenerator::kData) histname += "dat";
-//        if (iplot == PlotGenerator::kBkgnd) histname += "bkgnd";
+        if (iplot == PlotGenerator::kBkgnd) histname += "bkgnd";
         if (iplot == PlotGenerator::kAccMC) histname += "acc";
         if (iplot == PlotGenerator::kGenMC) histname += "gen";
 
