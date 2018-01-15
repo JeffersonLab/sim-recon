3a784d3ceeff92691cc0fbab64fe4b0a6ce24f65
Sean Dobbs <seandobbs@gmail.com>
2018-01-15 16:33:32 -0500

    tweak comments and binning

--- src/plugins/Calibration/HLDetectorTiming/JEventProcessor_HLDetectorTiming.cc
+++ src/plugins/Calibration/HLDetectorTiming/JEventProcessor_HLDetectorTiming.cc
@@ -235,14 +235,15 @@ jerror_t JEventProcessor_HLDetectorTiming::evnt(JEventLoop *loop, uint64_t event
         if(fdcHitVector[i]->type == 0 ) {
             Fill1DHistogram ("HLDetectorTiming", "FDC", "FDCHit Wire time", fdcHitVector[i]->t,
                     "FDCHit Wire time;t [ns];", nBins, xMin, xMax);
-	    // two F1TDC modules per layer
+	    // Keep track of module/crate level shifts
+	    // two F1TDC modules per wire layer
 	    int module = 2 * fdcHitVector[i]->gLayer;
 	    if(fdcHitVector[i]->element > 48)
 		    module++;
 	    Fill2DHistogram ("HLDetectorTiming", "FDC", "FDCHit Wire time vs. module",
 			     module, fdcHitVector[i]->t,
 			     "FDCHit Wire time; module number; t [ns];", 
-			     48, 0.5, 48.5, 700, -200, 1200);
+			     48, 0.5, 48.5, 400, -200, 600);
 
         }
         else{
