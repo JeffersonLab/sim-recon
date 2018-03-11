24c1f571353ae2493f82f34555b05cc93b63f564
Sean Dobbs <seandobbs@gmail.com>
2018-03-11 15:24:55 -0400

    Add latest version of RCDB compatibility

--- src/SBMS/sbms.py
+++ src/SBMS/sbms.py
@@ -657,18 +657,19 @@ def AddSQLite(env):
 # RCDB
 ##################################
 def AddRCDB(env):
-	rcdb_home = os.getenv('RCDB_HOME')
-	env.Append(CPPDEFINES='RCDB_MYSQL')
-#	env.Append(CPPDEFINES='RCDB_SQLITE')
-	if(rcdb_home != None) :
-		env.AppendUnique(CXXFLAGS = ['-DHAVE_RCDB'])
-		RCDB_CPPPATH = ["%s/cpp/include" % (rcdb_home), "%s/cpp/include/SQLite" % (rcdb_home)]
-		RCDB_LIBPATH = "%s/cpp/lib" % (rcdb_home)
-		RCDB_LIBS = ["rcdb"]
-		env.AppendUnique(CPPPATH = RCDB_CPPPATH)
-		env.AppendUnique(LIBPATH = RCDB_LIBPATH)
-		env.AppendUnique(LIBS    = RCDB_LIBS)
-		AddMySQL(env)
+        rcdb_home = os.getenv('RCDB_HOME')
+        if(rcdb_home != None) :
+                env.AppendUnique(CXXFLAGS = ['-DHAVE_RCDB'])
+                RCDB_CPPPATH = ["%s/cpp/include" % (rcdb_home)]
+                env.AppendUnique(CPPPATH = RCDB_CPPPATH)
+ 
+                # add MySQL
+                env.Append(CPPDEFINES='RCDB_MYSQL')
+                AddMySQL(env)
+
+                # add SQlite
+                env.Append(CPPDEFINES='RCDB_SQLITE')
+                AddSQLite(env)
 
 
 ##################################
