669eeeae917f96196c195e6059899cd8106478d3
Sean Dobbs <seandobbs@gmail.com>
2018-03-12 19:54:47 -0400

    Change CDC hit factory to use variable length arrays to save on memory

--- src/libraries/CDC/DCDCHit_factory.h
+++ src/libraries/CDC/DCDCHit_factory.h
@@ -21,8 +21,25 @@ class DCDCHit_factory: public jana::JFactory<DCDCHit>{
   DCDCHit_factory(){};
   ~DCDCHit_factory(){};
 
+  // we need to store information on the hits with respect to their readout channels in order to look for correlated hits
+  struct cdchit_info_t{
+      uint32_t rocid;
+      uint32_t slot;
+      uint32_t connector;
+
+      double time;
+      double max;
+
+      inline bool operator==(const struct cdchit_info_t &rhs) const {
+          return (rocid==rhs.rocid) && (slot==rhs.slot) && (connector==rhs.connector);
+      }
+  };
+
+
   int RemoveCorrelationHits;
   double RemoveCorrelationHitsCut;
+  double CorrelatedHitPeak;
+
   // timing cut limits
   double LowTCut;
   double HighTCut;
