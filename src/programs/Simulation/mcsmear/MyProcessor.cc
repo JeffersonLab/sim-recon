f029b1bd2bbc5efc96e98a871d09e9ad7edb320e
Justin Stevens <jrsteven@jlab.org>
2018-07-06 11:56:50 -0400

    for mcsmear: if no RCDB table exists, skip over obtaining parameters for hit merging

--- src/programs/Simulation/mcsmear/MyProcessor.cc
+++ src/programs/Simulation/mcsmear/MyProcessor.cc
@@ -202,96 +202,99 @@ jerror_t MyProcessor::brun(JEventLoop *loop, int locRunNumber)
 
 #ifdef HAVE_RCDB
 	// Pull configuration parameters from RCDB
-	config->ParseRCDBConfigFile(locRunNumber);
+	bool haveRCDBConfigFile = config->ParseRCDBConfigFile(locRunNumber);
+	if(haveRCDBConfigFile) {
 
-	const double fadc250_period_ns(4.);
-	const double fadc125_period_ns(8.);
+		const double fadc250_period_ns(4.);
+		const double fadc125_period_ns(8.);
+		
+		// hits merging / truncation parameters for the CDC
+		hddm_s_merger::set_cdc_max_hits(config->readout["CDC"].at("NPEAK"));
+		double cdc_ie = config->readout["CDC"].at("IE");
+		double cdc_pg = config->readout["CDC"].at("PG");
+		double cdc_gate = (cdc_ie + cdc_pg) * fadc125_period_ns;
+		hddm_s_merger::set_cdc_integration_window_ns(cdc_gate);
+		
+		// hits merging / truncation parameters for the FDC
+		hddm_s_merger::set_fdc_wires_max_hits(config->readout["FDC"].at("NHITS"));
+		double fdc_width = config->readout["FDC"].at("WIDTH");
+		hddm_s_merger::set_fdc_wires_min_delta_t_ns(fdc_width + 5.);
+		hddm_s_merger::set_fdc_strips_max_hits(config->readout["FDC"].at("NPEAK"));
+		double fdc_ie = config->readout["FDC"].at("IE");
+		double fdc_pg = config->readout["FDC"].at("PG");
+		double fdc_gate = (fdc_ie + fdc_pg) * fadc125_period_ns;
+		hddm_s_merger::set_fdc_strips_integration_window_ns(fdc_gate);
+		
+		// hits merging / truncation parameters for the STC
+		hddm_s_merger::set_stc_adc_max_hits(config->readout["ST"].at("NPEAK"));
+		hddm_s_merger::set_stc_tdc_max_hits(config->readout["ST"].at("NHITS"));
+		double stc_width = config->readout["ST"].at("WIDTH");
+		hddm_s_merger::set_stc_min_delta_t_ns(stc_width + 5.);
+		double stc_nsa = config->readout["ST"].at("NSA");
+		double stc_nsb = config->readout["ST"].at("NSB");
+		double stc_gate = (stc_nsa + stc_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_stc_integration_window_ns(stc_gate);
+		
+		// hits merging / truncation parameters for the BCAL
+		hddm_s_merger::set_bcal_adc_max_hits(config->readout["BCAL"].at("NPEAK"));
+		hddm_s_merger::set_bcal_tdc_max_hits(config->readout["BCAL"].at("NHITS"));
+		double bcal_width = config->readout["BCAL"].at("WIDTH");
+		hddm_s_merger::set_bcal_min_delta_t_ns(bcal_width + 5.);
+		double bcal_nsa = config->readout["BCAL"].at("NSA");
+		double bcal_nsb = config->readout["BCAL"].at("NSB");
+		double bcal_gate = (bcal_nsa + bcal_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_bcal_integration_window_ns(bcal_gate);
+		
+		// hits merging / truncation parameters for the TOF
+		hddm_s_merger::set_ftof_adc_max_hits(config->readout["TOF"].at("NPEAK"));
+		hddm_s_merger::set_ftof_tdc_max_hits(config->readout["TOF"].at("NHITS"));
+		double ftof_width = config->readout["TOF"].at("WIDTH");
+		hddm_s_merger::set_ftof_min_delta_t_ns(ftof_width + 5.);
+		double tof_nsa = config->readout["TOF"].at("NSA");
+		double tof_nsb = config->readout["TOF"].at("NSB");
+		double tof_gate = (tof_nsa + tof_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_ftof_integration_window_ns(tof_gate);
+		
+		// hits merging / truncation parameters for the FCAL
+		hddm_s_merger::set_fcal_max_hits(config->readout["FCAL"].at("NPEAK"));
+		double fcal_nsa = config->readout["FCAL"].at("NSA");
+		double fcal_nsb = config->readout["FCAL"].at("NSB");
+		double fcal_gate = (fcal_nsa + fcal_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_fcal_integration_window_ns(fcal_gate);
+		
+		// hits merging / truncation parameters for the CCAL
+		hddm_s_merger::set_ccal_max_hits(config->readout["FCAL"].at("NPEAK"));
+		hddm_s_merger::set_ccal_integration_window_ns(fcal_gate);
+		
+		// hits merging / truncation parameters for the PS
+		hddm_s_merger::set_ps_max_hits(config->readout["PS"].at("NPEAK"));
+		double ps_nsa = config->readout["PS"].at("NSA");
+		double ps_nsb = config->readout["PS"].at("NSB");
+		double ps_gate = (ps_nsa + ps_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_ps_integration_window_ns(ps_gate);
+		hddm_s_merger::set_psc_adc_max_hits(config->readout["PSC"].at("NPEAK"));
+		hddm_s_merger::set_psc_tdc_max_hits(config->readout["PSC"].at("NHITS"));
+		double psc_width = config->readout["PSC"].at("WIDTH");
+		hddm_s_merger::set_psc_min_delta_t_ns(psc_width + 5.);
+		double psc_nsa = config->readout["PSC"].at("NSA");
+		double psc_nsb = config->readout["PSC"].at("NSB");
+		double psc_gate = (psc_nsa + psc_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_psc_integration_window_ns(psc_gate);
+		
+		// hits merging / truncation parameters for the TAGM/TAGH
+		hddm_s_merger::set_tag_adc_max_hits(config->readout["TAGM"].at("NPEAK"));
+		hddm_s_merger::set_tag_tdc_max_hits(config->readout["TAGM"].at("NHITS"));
+		double tag_width = config->readout["TAGM"].at("WIDTH");
+		hddm_s_merger::set_tag_min_delta_t_ns(tag_width + 5.);
+		double tag_nsa = config->readout["TAGM"].at("NSA");
+		double tag_nsb = config->readout["TAGM"].at("NSB");
+		double tag_gate = (tag_nsa + tag_nsb) * fadc250_period_ns;
+		hddm_s_merger::set_tag_integration_window_ns(tag_gate);
+		
+		// hits merging / truncation parameters for the TPOL
+		hddm_s_merger::set_tpol_max_hits(config->readout["TPOL"].at("NPEAK"));
+	}
 
-	// hits merging / truncation parameters for the CDC
-	hddm_s_merger::set_cdc_max_hits(config->readout["CDC"].at("NPEAK"));
-	double cdc_ie = config->readout["CDC"].at("IE");
-	double cdc_pg = config->readout["CDC"].at("PG");
-	double cdc_gate = (cdc_ie + cdc_pg) * fadc125_period_ns;
-	hddm_s_merger::set_cdc_integration_window_ns(cdc_gate);
-
-	// hits merging / truncation parameters for the FDC
-	hddm_s_merger::set_fdc_wires_max_hits(config->readout["FDC"].at("NHITS"));
-	double fdc_width = config->readout["FDC"].at("WIDTH");
-	hddm_s_merger::set_fdc_wires_min_delta_t_ns(fdc_width + 5.);
-	hddm_s_merger::set_fdc_strips_max_hits(config->readout["FDC"].at("NPEAK"));
-	double fdc_ie = config->readout["FDC"].at("IE");
-	double fdc_pg = config->readout["FDC"].at("PG");
-	double fdc_gate = (fdc_ie + fdc_pg) * fadc125_period_ns;
-	hddm_s_merger::set_fdc_strips_integration_window_ns(fdc_gate);
-
-	// hits merging / truncation parameters for the STC
-	hddm_s_merger::set_stc_adc_max_hits(config->readout["ST"].at("NPEAK"));
-	hddm_s_merger::set_stc_tdc_max_hits(config->readout["ST"].at("NHITS"));
-	double stc_width = config->readout["ST"].at("WIDTH");
-	hddm_s_merger::set_stc_min_delta_t_ns(stc_width + 5.);
-	double stc_nsa = config->readout["ST"].at("NSA");
-	double stc_nsb = config->readout["ST"].at("NSB");
-	double stc_gate = (stc_nsa + stc_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_stc_integration_window_ns(stc_gate);
-
-	// hits merging / truncation parameters for the BCAL
-	hddm_s_merger::set_bcal_adc_max_hits(config->readout["BCAL"].at("NPEAK"));
-	hddm_s_merger::set_bcal_tdc_max_hits(config->readout["BCAL"].at("NHITS"));
-	double bcal_width = config->readout["BCAL"].at("WIDTH");
-	hddm_s_merger::set_bcal_min_delta_t_ns(bcal_width + 5.);
-	double bcal_nsa = config->readout["BCAL"].at("NSA");
-	double bcal_nsb = config->readout["BCAL"].at("NSB");
-	double bcal_gate = (bcal_nsa + bcal_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_bcal_integration_window_ns(bcal_gate);
-
-	// hits merging / truncation parameters for the TOF
-	hddm_s_merger::set_ftof_adc_max_hits(config->readout["TOF"].at("NPEAK"));
-	hddm_s_merger::set_ftof_tdc_max_hits(config->readout["TOF"].at("NHITS"));
-	double ftof_width = config->readout["TOF"].at("WIDTH");
-	hddm_s_merger::set_ftof_min_delta_t_ns(ftof_width + 5.);
-	double tof_nsa = config->readout["TOF"].at("NSA");
-	double tof_nsb = config->readout["TOF"].at("NSB");
-	double tof_gate = (tof_nsa + tof_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_ftof_integration_window_ns(tof_gate);
-
-	// hits merging / truncation parameters for the FCAL
-	hddm_s_merger::set_fcal_max_hits(config->readout["FCAL"].at("NPEAK"));
-	double fcal_nsa = config->readout["FCAL"].at("NSA");
-	double fcal_nsb = config->readout["FCAL"].at("NSB");
-	double fcal_gate = (fcal_nsa + fcal_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_fcal_integration_window_ns(fcal_gate);
-
-	// hits merging / truncation parameters for the CCAL
-	hddm_s_merger::set_ccal_max_hits(config->readout["FCAL"].at("NPEAK"));
-	hddm_s_merger::set_ccal_integration_window_ns(fcal_gate);
-
-	// hits merging / truncation parameters for the PS
-	hddm_s_merger::set_ps_max_hits(config->readout["PS"].at("NPEAK"));
-	double ps_nsa = config->readout["PS"].at("NSA");
-	double ps_nsb = config->readout["PS"].at("NSB");
-	double ps_gate = (ps_nsa + ps_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_ps_integration_window_ns(ps_gate);
-	hddm_s_merger::set_psc_adc_max_hits(config->readout["PSC"].at("NPEAK"));
-	hddm_s_merger::set_psc_tdc_max_hits(config->readout["PSC"].at("NHITS"));
-	double psc_width = config->readout["PSC"].at("WIDTH");
-	hddm_s_merger::set_psc_min_delta_t_ns(psc_width + 5.);
-	double psc_nsa = config->readout["PSC"].at("NSA");
-	double psc_nsb = config->readout["PSC"].at("NSB");
-	double psc_gate = (psc_nsa + psc_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_psc_integration_window_ns(psc_gate);
-
-	// hits merging / truncation parameters for the TAGM/TAGH
-	hddm_s_merger::set_tag_adc_max_hits(config->readout["TAGM"].at("NPEAK"));
-	hddm_s_merger::set_tag_tdc_max_hits(config->readout["TAGM"].at("NHITS"));
-	double tag_width = config->readout["TAGM"].at("WIDTH");
-	hddm_s_merger::set_tag_min_delta_t_ns(tag_width + 5.);
-	double tag_nsa = config->readout["TAGM"].at("NSA");
-	double tag_nsb = config->readout["TAGM"].at("NSB");
-	double tag_gate = (tag_nsa + tag_nsb) * fadc250_period_ns;
-	hddm_s_merger::set_tag_integration_window_ns(tag_gate);
-
-	// hits merging / truncation parameters for the TPOL
-	hddm_s_merger::set_tpol_max_hits(config->readout["TPOL"].at("NPEAK"));
 #endif  // HAVE_RCDB
 
     // fast forward any merger input files over skipped events
